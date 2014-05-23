171a588b66b75ca05a62379689d0c4d1cfbb1985
Stephen Wooten <hello@stephenwooten.com>
2014-05-23 13:00:19 -0500

    Updating Gemfile.lock and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521190721) do
+ActiveRecord::Schema.define(version: 20140523042541) do
 
   create_table "admin_login_attempts", force: true do |t|
     t.string   "email"
