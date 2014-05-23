3803199dcea611d1c1dc06bd9353d66078f6432b
Stephen Wooten <hello@stephenwooten.com>
2014-05-22 22:20:41 -0500

    Nope. Dont need these yet so getting rid of them.

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,12 +1,8 @@
 class Bar < ActiveRecord::Base
-  include EmbeddableContent
-
   has_many :rules
   has_many :bars, through: :rules
 
   has_one :bar_setting
-
-  content_name = 'bar'
 
   def settings
     bar_setting || BarSetting.new
