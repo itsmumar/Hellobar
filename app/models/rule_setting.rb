796a914f6613940ebf44b855e2729ef9aa531370
Stephen Wooten <hello@stephenwooten.com>
2014-06-04 12:03:48 -0500

    Forgot to git rm rule setting after consolidating

--- app/models/rule_setting.rb
+++
@@ -1,8 +0,0 @@
-class RuleSetting < ActiveRecord::Base
-  serialize :exclude_urls
-  serialize :include_urls
-
-  def public_attributes
-    as_json(except: [:id, :rule_id, :created_at, :updated_at])
-  end
-end
