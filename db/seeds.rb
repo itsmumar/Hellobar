5caff56d4218c1b6a5a24f9c87c2b490aeab496b
Stephen Wooten <hello@stephenwooten.com>
2014-06-10 12:28:12 -0500

    Dont need seed file anymore. Also, unsure why it is used in CI

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-site = Site.create url: 'wearepolymathic.com'
-user = User.create email: 'test@email.com',
-                   password: 'password'
-
-user.sites << site
-
-rule_set = site.rule_sets.create
-rule = rule_set.rules.create
-bar = rule_set.bars.create goal: "CollectEmail"
+# seed
