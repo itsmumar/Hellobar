e2dfd31c1cc3aa327db35e51d15af6eb87b7d5d9
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-06-10 13:21:58 -0500

    restart monit during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,13 @@ set :application, "hellobar"
 set :repo_url, "git@github.com:PolymathicMedia/hellobar_new.git"
 set :deploy_to, "/mnt/deploy"
 set :linked_files, %w{config/database.yml config/secrets.yml config/settings.yml}
+set :linked_dirs, %w{log tmp/pids}
 set :rails_env, "production"
+set :ssh_options, { :forward_agent => true }
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
   desc "Restart application"
@@ -18,6 +18,7 @@ namespace :deploy do
     on roles(:web) do
       invoke "deploy:reload_nginx_config"
       invoke "deploy:restart_thin"
+      invoke "deploy:restart_monit"
     end
   end
 
@@ -39,6 +40,17 @@ namespace :deploy do
   task :start_thin do
     on roles(:web) do
       execute "cd #{release_path} && sudo bundle exec thin start -C config/thin/www.yml"
+    end
+  end
+
+  task :restart_monit do
+    on roles(:web) do
+      execute "sudo service monit stop"
+      execute "sudo rm /etc/monit/monitrc"
+      execute "sudo ln -s /mnt/deploy/current/config/deploy/monitrc /etc/monit/monitrc"
+      execute "sudo chown root /etc/monit/monitrc"
+      execute "sudo service monit start"
+      execute "sudo monit restart all"
     end
   end
 end
