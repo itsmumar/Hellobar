f61d5e62d2004246d8902479f6505fafebd47ace
Stephen Wooten <hello@stephenwooten.com>
2014-06-11 16:24:24 -0500

    [#72736586] LegacyGoal#bars association for ETL

--- lib/legacy_migrator/legacy_goal.rb
+++ lib/legacy_migrator/legacy_goal.rb
@@ -2,5 +2,7 @@ class LegacyMigrator
   class LegacyGoal < LegacyModel
     self.table_name = 'goals'
     self.inheritance_column = nil
+
+    has_many :bars, class_name: 'LegacyMigrator::LegacyBar', foreign_key: 'goal_id'
   end
 end
