9bee5619306ee1568a0ee332fb0155dff3e33027
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-06-17 15:36:40 -0500

    added stats harvesting / processing

--- config/initializers/hello.rb
+++ config/initializers/hello.rb
@@ -4,6 +4,7 @@ require "./lib/hello/asset_storage"
 require "./lib/hello/bar_data"
 require "./lib/hello/email_digest"
 require "./lib/hello/tracking"
+require "./lib/hello/tracking/internal_stats_harvester"
 require "./lib/hello/tracking_param"
 require "./lib/hello/wordpress_user"
 
