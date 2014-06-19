b32b815e50d20e870bc12bf37c33abc14e88277a
Stephen Wooten <hello@stephenwooten.com>
2014-06-19 13:38:47 -0500

    Update the gitignore and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618201227) do
+ActiveRecord::Schema.define(version: 20140619161755) do
 
   create_table "admin_login_attempts", force: true do |t|
     t.string   "email"
