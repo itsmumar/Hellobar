79179483b52d491ec88849e460c585f83570aa19
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-07-11 11:51:40 -0500

    service provider load order

--- config/initializers/service_providers.rb
+++ config/initializers/service_providers.rb
@@ -1,4 +1,5 @@
 require "./config/initializers/settings"
 
 require "./lib/service_provider"
+require "./lib/service_providers/embed_code_provider"
 Dir["./lib/service_providers/**/*.rb"].each { |f| require f }
