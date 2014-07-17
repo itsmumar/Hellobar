e598aa004fc4dd7a7c1b6b4a7694a3080826d1cd
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-07-17 15:19:55 -0500

    add email data library

--- config/initializers/hello.rb
+++ config/initializers/hello.rb
@@ -3,6 +3,7 @@ require "./config/initializers/settings"
 require "./lib/hello/ab_testing"
 require "./lib/hello/asset_storage"
 require "./lib/hello/bar_data"
+require "./lib/hello/email_data"
 require "./lib/hello/email_digest"
 require "./lib/hello/internal_analytics"
 require "./lib/hello/tracking"
@@ -11,3 +12,4 @@ require "./lib/hello/tracking_param"
 require "./lib/hello/wordpress_user"
 
 Hello::BarData.connect!
+Hello::EmailData.connect!
