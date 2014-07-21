a65ddf991b0d1a3fa37a60f39f466ef92c5d2c7f
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-07-21 11:13:41 -0500

    Revert "inject CSS into URL2PNG that hides the hellobar in preview image"
    This reverts commit 7391d4dd720bea7eb06cb94f3b181bdc1c3cc9f2.

--- app/serializers/site_element_serializer.rb
+++ app/serializers/site_element_serializer.rb
@@ -28,8 +28,7 @@ class SiteElementSerializer < ActiveModel::Serializer
   end
 
   def site_preview_image
-    css_url = "#{root_url.gsub(/\/$/, "")}/stylesheets/hide_bar.css"
-    params = "?url=#{object.site.url}&custom_css=#{css_url}"
+    params = "?url=#{object.site.url}"
     token = Digest::MD5.hexdigest("#{params}SC10DF8C7E0FE8")
     "https://api.url2png.com/v6/P52EBC321291EF/#{token}/png/#{params}"
   end
