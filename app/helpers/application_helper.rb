6359a01d0fc0adbdb7a2e0b168701fd8da612afe
polymatt <matt@polymathic.me>
2014-07-23 14:30:42 -0500

    Install Page & Reveal Blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ require "avatar/view/action_view_support"
 
 module ApplicationHelper
   include Avatar::View::ActionViewSupport
+
+  def page_id
+    if controller_name == 'pages' && params[:page]
+      [controller_name, params[:page]].join('-')
+    else      
+      [controller_name, action_name].join('-')
+    end
+  end  
 end
