c1f96a959f40895ca7302c1c5e8a5aa40ab2cf00
polymatt <matt@polymathic.me>
2014-08-01 16:06:20 -0500

    Static Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+
+  layout 'static'
+
+  def use_cases
+  end
+
+  def terms_of_use
+  end
+
+  def privacy_policy
+  end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+
+  layout 'static'
+
 end
