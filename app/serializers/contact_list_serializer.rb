d4042b4090182fbf8cb8d0af39ca66390d127be0
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-08-04 07:47:03 -0500

    include provider in contact list serializer; form class

--- app/serializers/contact_list_serializer.rb
+++ app/serializers/contact_list_serializer.rb
@@ -1,7 +1,11 @@
 class ContactListSerializer < ActiveModel::Serializer
-  attributes :id, :site_id, :name, :errors
+  attributes :id, :site_id, :name, :errors, :provider
 
   def errors
     object.errors.full_messages
+  end
+
+  def provider
+    object.identity.try(:provider) || 0
   end
 end
