346223380bb60a18c915416daf08cb48c44cecdb
polymatt <matt@polymathic.me>
2014-08-04 16:44:51 -0500

    Settings Styling First-Pass

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,6 @@ class UserController < ApplicationController
   before_filter :authenticate_user!
   before_filter :load_user, :only => [:edit, :update]
 
-  layout :determine_layout
-
   def update
     if @user.update_attributes(user_params)
       sign_in @user, :bypass => true
@@ -20,10 +18,6 @@ class UserController < ApplicationController
 
   def load_user
     @user = current_user
-  end
-
-  def determine_layout
-    %w(new edit).include?(action_name) ? "ember" : "application"
   end
 
   def user_params
