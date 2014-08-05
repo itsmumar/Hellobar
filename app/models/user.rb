f4a89d50c6a1862af15658f6efcd248df7b80e73
Stephen Wooten <hello@stephenwooten.com>
2014-08-05 13:22:24 -0500

    Validation around uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   delegate :url_helpers, to: "Rails.application.routes"
 
-  validate :email_does_not_exist_in_wordpress
+  validate :uniqueness_of_email
 
   def send_devise_notification(notification, *args)
     host = ActionMailer::Base.default_url_options[:host]
@@ -26,6 +26,12 @@ class User < ActiveRecord::Base
   end
 
   private
+
+  def uniqueness_of_email
+    email_does_not_exist_in_wordpress
+
+    errors.add(:email, 'has already been taken') if User.exists?(email: email)
+  end
 
   def email_does_not_exist_in_wordpress
     errors.add(:email, "has already been taken") if Hello::WordpressUser.email_exists?(email)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
-describe User do
+describe User, 'as a valid object' do
   it "cannot have the same email as someone in the wordpress database" do
     Hello::WordpressUser.should_receive(:email_exists?).with("foo@bar.com").and_return(true)
 
     user = User.create(:email => "foo@bar.com")
 
     user.errors.messages[:email].should include("has already been taken")
+  end
+
+  it 'cannot have the same email as someone in the Rails database' do
+    email = 'hoogaboo@gmail.com'
+    User.create email: email, password: 'supers3cr37'
+
+    user = User.create email: email
+
+    user.errors.messages[:email].should include('has already been taken')
   end
 end
