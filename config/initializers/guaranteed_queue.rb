d85e443d0122dcb67baff79d3f217ccbbe2a5fcc
Colin Young <me@colinyoung.com>
2014-08-26 17:29:07 -0500

    Poll less frequently for edge GQ

--- config/initializers/guaranteed_queue.rb
+++ config/initializers/guaranteed_queue.rb
@@ -3,5 +3,6 @@ require "./config/initializers/settings"
 GuaranteedQueue.config(
   :access_key_id => Hellobar::Settings[:aws_access_key_id],
   :secret_access_key => Hellobar::Settings[:aws_secret_access_key],
-  :queue_env => Hellobar::Settings[:env_name]
+  :queue_env => Hellobar::Settings[:env_name],
+  :poll_interval_seconds => (Hellobar::Settings[:env_name] == "edge") ? 6 : 2
 )
