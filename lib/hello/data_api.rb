415e4e5edc7b95e419e994aea6aa8184c21535f4
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-08-26 10:04:42 -0500

    comment and rewrite parts of Hello::DataAPI

--- lib/hello/data_api.rb
+++ lib/hello/data_api.rb
@@ -3,6 +3,14 @@ require "./lib/hello/data_api_helper"
 
 module Hello::DataAPI
   class << self
+
+    # Returns total views and conversions for each site_element, by day for n days.
+    # For example, if site element with id 123 had a total of 10 views and 3 conversions yesterday,
+    # and received 5 more views and 1 more conversion today, the response would look like:
+    #
+    # lifetime_totals(site, [site_element], 2)
+    # => {"123" => [[10, 3], [15, 4]]}
+    #
     def lifetime_totals(site, site_elements, num_days = 1, cache_options = {})
       if Hellobar::Settings[:fake_data_api]
         return {}.tap do |hash|
@@ -28,13 +36,25 @@ module Hello::DataAPI
       end
     end
 
-    def lifetime_totals_by_type(site, site_elements, num_days = 30)
-      data = Hello::DataAPI.lifetime_totals(site, site.site_elements, num_days) || {}
+    # Returns total views and conversions for each site element, by day for n days, grouped by truncated
+    # element subtype (ie: all social goals will get grouped together).
+    #
+    # lifetime_totals_by_type(site, site.site_elements, 2)
+    # => {
+    #      :total =>   [[9, 3], [12, 6]],
+    #      :email =>   [[3, 1], [4, 2]],
+    #      :traffic => [[3, 1], [4, 2]],
+    #      :social =>  [[3, 1], [4, 2]]
+    #    }
+    #
+    def lifetime_totals_by_type(site, site_elements, num_days = 30, cache_options = {})
+      data = Hello::DataAPI.lifetime_totals(site, site.site_elements, num_days, cache_options) || {}
       totals = {:total => [], :email => [], :social => [], :traffic => []}
       elements = site.site_elements.where(:id => data.keys)
 
       return totals if data == {}
 
+      # collect the ids of each group
       ids = {
         :total => data.keys,
         :email => elements.select{|e| e.element_subtype == "email"}.map{|e| e.id.to_s},
@@ -42,18 +62,32 @@ module Hello::DataAPI
         :social => elements.select{|e| e.element_subtype =~ /social\//}.map{|e| e.id.to_s}
       }
 
+      # loop for the number of days of data, based on what came back from the API
       data.values.first.count.times do |i|
-        totals[:total] << data.inject([0, 0]){|m, d| [m[0] + d[1][i][0], m[1] + d[1][i][1]]}
+        # sum the values for day i in each row of data and shovel that array into totals
+        totals[:total] << data.inject([0, 0]) do |sum, data_row|
+          day_i_data = data_row[1][i]
+          [sum[0] + day_i_data[0], sum[1] + day_i_data[1]]
+        end
 
+        # do the same for each subset of data, grouped by element subtype
         [:email, :traffic, :social].each do |key|
           type_data = data.select{|k, v| ids[key].include?(k)}
-          totals[key] << type_data.inject([0, 0]){|m, d| [m[0] + d[1][i][0], m[1] + d[1][i][1]]}
+          totals[key] << type_data.inject([0, 0]) do |sum, data_row|
+            day_i_data = data_row[1][i]
+            [sum[0] + day_i_data[0], sum[1] + day_i_data[1]]
+          end
         end
       end
 
       totals
     end
 
+    # Returns the total subscribers for each contact list
+    #
+    # contact_list_totals(site, site.contact_lists)
+    # => {"1" => 141, "2" => 951}
+    #
     def contact_list_totals(site, contact_lists, cache_options = {})
       contact_list_ids = contact_lists.map(&:id).sort
 
@@ -66,6 +100,15 @@ module Hello::DataAPI
       end
     end
 
+    # Returns groups of segments and their view/conversion data, grouped by opportunity
+    #
+    # suggested_opportunities(site, site.site_elements)
+    # => {
+    #      "high traffic, low conversion" =>  [["co:USA", 100, 10], ["dv:Mobile", 200, 20]],
+    #      "low traffic, high conversion" =>  [["co:USA", 100, 10], ["dv:Mobile", 200, 20]],
+    #      "high traffic, high conversion" => [["co:USA", 100, 10], ["dv:Mobile", 200, 20]]
+    #    }
+    #
     def suggested_opportunities(site, site_elements, cache_options = {})
       site_element_ids = site_elements.map(&:id).sort
 
@@ -78,6 +121,11 @@ module Hello::DataAPI
       end
     end
 
+    # Return name, email and timestamp of subscribers for a contact list
+    #
+    # get_contacts(contact_list)
+    # => [["person100@gmail.com", "person name", 1388534400], ["person99@gmail.com", "person name", 1388534399]]
+    #
     def get_contacts(contact_list, from_timestamp = nil, cache_options = {})
       cache_key = "hello:data-api:#{contact_list.site_id}:contact_list-#{contact_list.id}:from#{from_timestamp}"
       cache_options[:expires_in] = 10.minutes
--- spec/lib/hello/data_api_spec.rb
+++ spec/lib/hello/data_api_spec.rb
@@ -7,18 +7,19 @@ describe Hello::DataAPI, '.lifetime_totals_by_type' do
     site = sites(:zombo)
 
     Hello::DataAPI.should_receive(:lifetime_totals).and_return({
-      site_elements(:zombo_traffic).id.to_s => [[2, 1]],
-      site_elements(:zombo_email).id.to_s =>   [[4, 3]],
-      site_elements(:zombo_twitter).id.to_s => [[6, 5]]
+      site_elements(:zombo_traffic).id.to_s =>  [[2, 1]],
+      site_elements(:zombo_email).id.to_s =>    [[4, 3]],
+      site_elements(:zombo_twitter).id.to_s =>  [[6, 5]],
+      site_elements(:zombo_facebook).id.to_s => [[8, 7]]
     })
 
     result = Hello::DataAPI.lifetime_totals_by_type(site, site.site_elements)
 
     result.should == {
-      :total =>   [[12, 9]],
+      :total =>   [[20, 16]],
       :traffic => [[2, 1]],
       :email =>   [[4, 3]],
-      :social =>  [[6, 5]]
+      :social =>  [[14, 12]]
     }
   end
 end
