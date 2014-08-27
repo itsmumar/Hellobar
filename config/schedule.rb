683faf2cbe993edcc51459858a83625d504db676
Colin Young <me@colinyoung.com>
2014-08-27 16:28:07 -0500

    Use whenever's script syntax

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,3 +6,7 @@ env :PATH, ENV['PATH']
 every 20.minutes, :roles => [:web] do
   runner "Hello::Tracking::InternalStatsHarvester.process_internal_stats"
 end
+
+every 15.minutes, :roles => [:web] do
+  script "send_sqs_message #{Hellobar::Settings[:env_name]} 'contact_list:sync_all!'" 
+end
