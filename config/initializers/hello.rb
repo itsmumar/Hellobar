82dc73a6e4354c588a90ae6eb990bd9271c1623b
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-08-28 15:54:14 -0500

    remove EmailData

--- config/initializers/hello.rb
+++ config/initializers/hello.rb
@@ -4,7 +4,6 @@ require "./lib/hello/ab_testing"
 require "./lib/hello/asset_storage"
 require "./lib/hello/data_api"
 require "./lib/hello/data_api_helper"
-require "./lib/hello/email_data"
 require "./lib/hello/email_digest"
 require "./lib/hello/internal_analytics"
 require "./lib/hello/segments"
@@ -12,5 +11,3 @@ require "./lib/hello/tracking"
 require "./lib/hello/tracking/internal_stats_harvester"
 require "./lib/hello/tracking_param"
 require "./lib/hello/wordpress_user"
-
-Hello::EmailData.connect!
