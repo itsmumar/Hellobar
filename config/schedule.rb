f7e1da681159e2375dd9ae48a606de8f8906439b
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-08-29 15:43:32 -0500

    load settings yaml in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,10 +3,13 @@ set :output, { standard: PREFIX + "cron.log", error: PREFIX + "cron.error.log" }
 
 env :PATH, ENV['PATH']
 
+settings_file = "config/settings.yml"
+settings_yaml = File.exists?(settings_file) ? YAML.load_file(settings_file) : {}
+
 every 20.minutes, :roles => [:web] do
   runner "Hello::Tracking::InternalStatsHarvester.process_internal_stats"
 end
 
 every 15.minutes, :roles => [:web] do
-  script "send_sqs_message #{Hellobar::Settings[:env_name]} 'contact_list:sync_all!'" 
+  script "send_sqs_message #{settings_yaml["env_name"] || "edge"} 'contact_list:sync_all!'" 
 end
