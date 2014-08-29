3353d5c23f2a5224a676d2dfce8e7bb6a9d2475e
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-08-29 15:39:59 -0500

    commenting out broken whenever def for now

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,10 +3,7 @@ set :output, { standard: PREFIX + "cron.log", error: PREFIX + "cron.error.log" }
 
 env :PATH, ENV['PATH']
 
-every 20.minutes, :roles => [:web] do
-  runner "Hello::Tracking::InternalStatsHarvester.process_internal_stats"
-end
-
-every 15.minutes, :roles => [:web] do
-  script "send_sqs_message #{Hellobar::Settings[:env_name]} 'contact_list:sync_all!'" 
-end
+# TODO: Rails environment is not loaded in this file, so need a different way to access env_name
+# every 20.minutes, :roles => [:web] do
+#   runner "Hello::Tracking::InternalStatsHarvester.process_internal_stats"
+# end
