50769e3c9041ec7e9e0dfdf2cffaf90e55f80f57
Colin Young <me@colinyoung.com>
2014-09-02 15:50:45 -0500

    We will not need to sync at intervals per JB.

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,7 +11,3 @@ settings_yaml = File.exists?(settings_file) ? YAML.load_file(settings_file) : {}
 every 20.minutes, :roles => [:web] do
   runner "Hello::Tracking::InternalStatsHarvester.process_internal_stats"
 end
-
-every 15.minutes, :roles => [:web] do
-  script "send_sqs_message #{settings_yaml["env_name"] || "edge"} 'contact_list:sync_all!'" 
-end
