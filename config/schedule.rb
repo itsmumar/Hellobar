975ab3df92879dffc98112f8c30b4b6a8d83d52e
Colin Young <me@colinyoung.com>
2014-09-02 16:21:12 -0500

    Move Stats Harvester processing into GQ.

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,12 @@ env :PATH, ENV['PATH']
 
 settings_file = "config/settings.yml"
 settings_yaml = File.exists?(settings_file) ? YAML.load_file(settings_file) : {}
+env = settings_yaml["env_name"] || "edge"
 
 every 20.minutes, :roles => [:web] do
   runner "Hello::Tracking::InternalStatsHarvester.process_internal_stats"
+end
+
+every 20.minutes, :roles => [:web] do
+  script "send_sqs_message #{env} 'hello::tracking::internal_stats_harvester:process_internal_stats'"
 end
