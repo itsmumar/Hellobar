9e33a5236da578fc48ce30353242fca9c9b2374d
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-09-16 15:18:01 -0500

    make gravatar helper work with blank emails

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -10,4 +10,9 @@ module UserHelper
 
     truncate(display_name, :length => 30)
   end
+
+  def gravatar_url_for(user, opts = {})
+    email = OpenStruct.new(:email => user.email.blank? ? "none" : user.email)
+    avatar_url_for(email, opts)
+  end
 end
