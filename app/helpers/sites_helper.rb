8fece87bb9e1d1231390bc251e9e43d3ee1f9992
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-09-19 11:07:51 -0500

    construct links to creating targeted content in improve section

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -5,9 +5,38 @@ module SitesHelper
     site.url
   end
 
-  def segment_description(short)
-    segment, value = short.split(":", 2)
+  def segment_description(segment_and_value)
+    segment, value = segment_and_value.split(":", 2)
     user_segment = Hello::Segments::User.find{ |d| d[:key] == segment }
     "#{user_segment[:name]} is #{value}"
+  end
+
+  def create_targeted_content_link(site, segment)
+    segment, value = segment.split(":", 2)
+    segment = Condition::SEGMENTS.find { |s| s[1] == segment }
+
+    return "" unless segment.present? # the API returned a segment key which we don't implement
+
+    segment = segment[1]
+    existing_rule = rule_for_segment_and_value(site, segment, value)
+
+    path = if existing_rule
+      new_site_site_element_path(site, anchor: "/settings?rule_id=#{existing_rule.id}")
+    else
+      new_site_site_element_path(site, anchor: "/targeting?segment=#{segment}&value=#{value}")
+    end
+
+    link_to "Create targeted content", path, class: "button"
+  end
+
+  def rule_for_segment_and_value(site, segment, value)
+    site.rules.includes(:conditions).find do |rule|
+      condition = rule.conditions.first
+      segment_data = condition.try(:segment_data)
+
+      rule.conditions.count == 1 &&
+        segment_data[:key] == segment &&
+        condition.value == value
+    end
   end
 end
--- spec/helpers/sites_helper_spec.rb
+++ spec/helpers/sites_helper_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe SitesHelper do
+  fixtures :all
+
   describe "display_url_for_site" do
     it "returns shorter URLs for different sites" do
       site = Site.new(:url => "http://asdf.com")
@@ -19,6 +21,26 @@ describe SitesHelper do
 
     it "correctly expands short segment strings when value contains a colin" do
       segment_description("rf:http://zombo.com").should == "Referrer URL is http://zombo.com"
+    end
+  end
+
+  describe "create_targeted_content_link" do
+    it "uses an existing rule if one already matches" do
+      link = create_targeted_content_link(sites(:zombo), "dv:mobile")
+      link.should =~ /rule_id=#{rules(:zombo_mobile).id}/
+    end
+
+    it "links to editor with segment and value in params if no matching rule exists" do
+      link = create_targeted_content_link(sites(:zombo), "co:Russia")
+      link.should =~ /\?.*segment=co/
+      link.should =~ /\?.*value=Russia/
+    end
+  end
+
+  describe "rule_for_segment_and_value" do
+    it "returns a rule if it has a single condition of 'segment is value'" do
+      rule = rule_for_segment_and_value(sites(:zombo), "dv", "mobile")
+      rule.should == rules(:zombo_mobile)
     end
   end
 end
