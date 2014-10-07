22dce130966286981caeb6e16ce9231912186bad
Stephen Wooten <hello@stephenwooten.com>
2014-10-07 16:05:59 -0500

    Handle for nil subscriptions

--- app/helpers/upgrade_modal_helper.rb
+++ app/helpers/upgrade_modal_helper.rb
@@ -1,6 +1,7 @@
 module UpgradeModalHelper
   # returns a button based on the subscription
   def choose_plan_button(subscription, plan)
+    subscription ||= Subscription::Free.new
     subscription_type = subscription.type.split('::').last.downcase
 
     if subscription_type == plan
