5af7b8eed66b9715a6f5efe2d1c47684f5cbb24d
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-10-26 20:20:19 +0800

    consolidate prompt partials and styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,14 +16,4 @@ module ApplicationHelper
       !(params[:controller] == "user" && params[:action] == "edit") &&
       !(params[:controller] == "user" && params[:action] == "update")
   end
-
-  # sets a flash when there are errors with their payment method
-  def render_payment_errors
-    if @site.present? && @site.bills_with_payment_issues.present?
-      total_bill_amount = @site.bills_with_payment_issues.inject(0){|sum,b| sum+=b.amount}
-      first_bill_date = @site.bills_with_payment_issues.sort{|a,b| a.bill_at <=> b.bill_at}.first.bill_at
-
-      flash.now[:error] = "You have outstanding bills in the amount of #{number_to_currency(total_bill_amount)} since #{first_bill_date.strftime("%-m-%-d-%Y")}"
-    end
-  end
 end
--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -4,4 +4,16 @@ module SitesHelper
   rescue URI::InvalidURIError
     site.url
   end
+
+  def site_has_payment_issue?(site)
+    site.present? && site.bills_with_payment_issues.present?
+  end
+
+  def payment_issue_amount(site)
+    site_has_payment_issue?(site) ? site.bills_with_payment_issues.inject(0){|sum,b| sum+=b.amount} : 0
+  end
+
+  def payment_issue_date(site)
+    site_has_payment_issue?(site) ? site.bills_with_payment_issues.sort{|a,b| a.bill_at <=> b.bill_at}.first.bill_at : nil
+  end
 end
