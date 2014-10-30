a2652cf0ab3d9550f20f0123a249f27b1bd85691
Stephen Wooten <hello@stephenwooten.com>
2014-10-30 14:00:04 -0500

    dont need web for beefy box

--- config/deploy/beefy.rb
+++ config/deploy/beefy.rb
@@ -15,7 +15,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'ec2-54-227-93-122.compute-1.amazonaws.com', user: 'hellobar', roles: %w{web app db}
+server 'ec2-54-227-93-122.compute-1.amazonaws.com', user: 'hellobar', roles: %w{app db}
 
 
 # Custom SSH Options
