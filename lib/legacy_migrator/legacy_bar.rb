32b2043b8fbb7dd78238f7c5c02867c230f2769b
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-11-05 13:49:17 -0600

    additional associations for legacy models

--- lib/legacy_migrator/legacy_bar.rb
+++ lib/legacy_migrator/legacy_bar.rb
@@ -2,6 +2,8 @@ class LegacyMigrator
   class LegacyBar < LegacyModel
     self.table_name = 'bars'
 
+    belongs_to :goal, class_name: "LegacyGoal"
+
     serialize :settings_json, JSON
   end
 end
--- lib/legacy_migrator/legacy_membership.rb
+++ lib/legacy_migrator/legacy_membership.rb
@@ -3,5 +3,6 @@ class LegacyMigrator
     self.table_name = 'memberships'
 
     belongs_to :user, class_name: 'LegacyUser'
+    belongs_to :account, class_name: 'LegacyAccount'
   end
 end
