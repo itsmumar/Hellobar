3a16c035f8a2edc0f416de0bb3e0a4df6bf14a9f
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-11-07 09:27:45 -0600

    don't allow empty values in url conditions

--- app/models/condition/url_condition.rb
+++ app/models/condition/url_condition.rb
@@ -1,4 +1,6 @@
 class UrlCondition < Condition
+  before_validation :clear_blank_values
+
   validates :operand, presence: true,
                       inclusion: {
                         in: %w{ is is_not includes does_not_include }
@@ -12,5 +14,11 @@ class UrlCondition < Condition
     else
       "#{segment_data[:name]} #{OPERANDS[operand]} #{value.first}"
     end
+  end
+
+  private
+
+  def clear_blank_values
+    self.value = value.select{|v| !v.blank?}
   end
 end
--- spec/models/condition/url_condition_spec.rb
+++ spec/models/condition/url_condition_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe UrlCondition do
+  fixtures :all
+
+  it "clears empty values during validation" do
+    condition = UrlCondition.new(
+      rule: rules(:zombo),
+      operand: "is",
+      value: ["/foo", "/bar", ""]
+    )
+
+    condition.should be_valid
+    condition.value.should == ["/foo", "/bar"]
+  end
 end
