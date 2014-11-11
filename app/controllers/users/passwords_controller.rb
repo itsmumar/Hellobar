1f538c160d8924e96b484bbd87817c8d75e9d333
Stephen Wooten <hello@stephenwooten.com>
2014-11-11 15:31:04 -0600

    No need to manually set flash. Somehow devise sets it properly

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,7 +5,6 @@ class Users::PasswordsController < Devise::PasswordsController
     if Hello::WordpressUser.email_exists?(params[:user].try(:[], :email))
       render "pages/redirect_forgot"
     else
-      flash[:success] = 'You will receive an email with instructions about how to confirm your account in a few minutes.'
       super
     end
   end
