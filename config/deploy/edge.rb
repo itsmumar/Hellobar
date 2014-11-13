f64a16d7b2ce39d830d689d124ce273cf1650c77
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-11-13 15:58:35 -0600

    add db roles back to cap

--- config/deploy/edge.rb
+++ config/deploy/edge.rb
@@ -15,7 +15,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'edge.hellobar.com', user: 'hellobar', roles: %w{web cron}
+server 'edge.hellobar.com', user: 'hellobar', roles: %w{web db cron}
 
 
 # Custom SSH Options
