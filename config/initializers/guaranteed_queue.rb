b8def8e214e1e2c88db53694904d85668d108fc8
Colin Young <me@colinyoung.com>
2014-11-17 14:14:49 -0600

    Stub requests for both test and dev.

--- config/initializers/guaranteed_queue.rb
+++ config/initializers/guaranteed_queue.rb
@@ -5,5 +5,5 @@ GuaranteedQueue.config(
   :secret_access_key => Hellobar::Settings[:aws_secret_access_key],
   :queue_name => Hellobar::Settings[:main_queue],
   :poll_interval_seconds => ENV['GUARANTEED_QUEUE_INTERVAL'] || 5,
-  :stub_requests => Rails.env.development?
+  :stub_requests => Rails.env.development? || Rails.env.test?
 )
