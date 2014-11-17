4a9dc1b096c660836a4fb54b26b655ef867567dd
Colin Young <me@colinyoung.com>
2014-11-17 13:51:25 -0600

    Stub GQ message queues on Development.

--- config/initializers/guaranteed_queue.rb
+++ config/initializers/guaranteed_queue.rb
@@ -4,5 +4,6 @@ GuaranteedQueue.config(
   :access_key_id => Hellobar::Settings[:aws_access_key_id],
   :secret_access_key => Hellobar::Settings[:aws_secret_access_key],
   :queue_name => Hellobar::Settings[:main_queue],
-  :poll_interval_seconds => ENV['GUARANTEED_QUEUE_INTERVAL'] || 5
+  :poll_interval_seconds => ENV['GUARANTEED_QUEUE_INTERVAL'] || 5,
+  :stub_requests => Rails.env.development?
 )
