612020128318f4939daa464f440bc68f34bebd09
Colin Young <me@colinyoung.com>
2014-11-18 11:44:35 -0600

    Add all cron environments to whenever list.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :linked_dirs, %w{log tmp/pids}
 set :rails_env, "production"
 set :ssh_options, { :forward_agent => true }
 set :branch, ENV["REVISION"] || ENV["BRANCH"] || "master"
+set :whenever_roles, %w(app db web)
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
