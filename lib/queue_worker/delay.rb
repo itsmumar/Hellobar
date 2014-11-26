f543641a1b450e5ab6615b7bfae786405b4c74a2
Colin Young <me@colinyoung.com>
2014-11-26 10:52:45 -0600

    Modify send_sqs_message to support QueueWorker

--- lib/queue_worker/delay.rb
+++ lib/queue_worker/delay.rb
@@ -1,24 +1,27 @@
-module QueueWorker
+class QueueWorker
 end
 
-module QueueWorker::Delay
-  def delay task_name, options={}
-    namespace = options.delete(:namespace) || self.class.name
-    body = "#{namespace.underscore.downcase}:#{task_name}[#{id}]".gsub(/^:/,'')
-    if Rails.env.development? || Rails.env.test?
-      begin
-        # Ensure private methods are called.
-        self.method(task_name).()
-      rescue NoMethodError => e
-        if e.message.include?("undefined method `#{task_name}'")
-          raise "Not sure how to queue task '#{body}' because there is no method #{self.class}##{task_name}: #{$!}"
-        else
-          raise e
+class QueueWorker
+  module Delay
+    def delay task_name, options={}
+      namespace = options.delete(:namespace) || self.class.name
+      body = "#{namespace.underscore.downcase}:#{task_name}[#{id}]".gsub(/^:/,'')
+      if Rails.env.development? || Rails.env.test?
+        begin
+          # Ensure private methods are called.
+          self.method(task_name).()
+        rescue NoMethodError => e
+          if e.message.include?("undefined method `#{task_name}'")
+            raise "Not sure how to queue task '#{body}' because there is no method #{self.class}##{task_name}: #{$!}"
+          else
+            raise e
+          end
         end
+      else
+        stage = Hellobar::Settings[:env_name]
+        raise 'Stage not set -- you must add :env_name to settings.yml' unless stage.present?
+        `bundle exec script/send_sqs_message #{stage} '#{body}'`
       end
-    else
-      queue = Hellobar::Settings[:main_queue]
-      `bundle exec script/send_sqs_message #{queue} '#{body}'`
     end
   end
 end
