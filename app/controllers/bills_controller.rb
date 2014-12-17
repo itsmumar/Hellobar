854188067518124783605c5fc5ae5c1114d5d5ca
tjacobs3 <thmsjcbs@gmail.com>
2014-12-17 10:27:53 -0600

    Add billing info to reciepts

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,6 +3,8 @@ class BillsController < ApplicationController
 
   def show
     @bill = Bill.find(params[:id])
+    @details = @bill.successful_billing_attempt.try(:payment_method_details)
     raise(ActiveRecord::RecordNotFound) unless @bill.site.owner == current_user
+    render layout: false
   end
 end
