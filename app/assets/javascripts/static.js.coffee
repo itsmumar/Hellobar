b8a05b674f0531d0cb5ac55b3774fdf1bfca6562
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-12-18 15:00:20 -0600

    fixes for upgrade modal with only one bar

--- app/assets/javascripts/static.js.coffee
+++ app/assets/javascripts/static.js.coffee
@@ -3,6 +3,14 @@
 #= require jquery-ui.min
 #= require handlebars
 #= require serialize-json
+#= require jstz-1.0.4.min
 
 #= require user_migration
 #= require flash_message_initializer
+
+$ ->
+  # Detect User Timezone
+  if $('#detect_timezone').length
+    $timezone = $('#site_timezone, #user_timezone')
+    userTimezone = jstz.determine().name()
+    $timezone.val(userTimezone)
--- app/controllers/user_migration_controller.rb
+++ app/controllers/user_migration_controller.rb
@@ -3,34 +3,45 @@ class UserMigrationController < ApplicationController
   before_filter :verify_wordpress_user
 
   def new
-    @bars = current_wordpress_user.bars
+    @bars = [current_wordpress_user.bars.first]
   end
 
   def create
-    user = current_wordpress_user.convert_to_user
-
-    params[:bar_ids].each do |url, ids|
-      Site.new(url: url).tap do |site|
-        site.save!
-        site.create_default_rule
-
-        SiteMembership.create!(site: site, user: user)
-
-        ids.each do |id|
-          if bar = load_wordpress_bar(id)
-            bar.convert_to_site_element!(site.rules.first)
-          end
-        end
-
-        sub = site.site_elements.count >= 10 ? Subscription::FreePlus.new(schedule: "monthly") : Subscription::Free.new(schedule: "monthly")
-        site.change_subscription(sub)
-      end
+    if params[:bar_ids].blank? && params[:bar_id].present? && params[:site][:url].present?
+      params[:bar_ids] = {params[:site][:url] => [params[:bar_id]]}
     end
 
-    sign_in(user)
-    session[:wordpress_user_id] = nil
+    ActiveRecord::Base.transaction do
+      user = current_wordpress_user.convert_to_user
 
-    render json: { url: user.sites.any? ? site_site_elements_path(user.sites.first, anchor: "upgrade-modal") : new_site_path }
+      params[:bar_ids].each do |url, ids|
+        Site.new(url: url).tap do |site|
+          site.save!
+          site.create_default_rule
+
+          SiteMembership.create!(site: site, user: user)
+
+          ids.each do |id|
+            if bar = load_wordpress_bar(id)
+              bar.convert_to_site_element!(site.rules.first)
+            end
+          end
+
+          sub = site.site_elements.count >= 10 ? Subscription::FreePlus.new(schedule: "monthly") : Subscription::Free.new(schedule: "monthly")
+          site.change_subscription(sub)
+        end
+      end
+
+      sign_in(user)
+    end
+
+    session[:wordpress_user_id] = nil
+    url = current_user.sites.any? ? site_site_elements_path(current_user.sites.first, anchor: "upgrade-modal") : new_site_path
+
+    respond_to do |format|
+      format.json { render json: { url: url } }
+      format.html { redirect_to url }
+    end
   end
 
   private
