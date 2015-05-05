a8e0a7424efd80c697afbcc83c3e9d394461a76d
tjacobs3 <thmsjcbs@gmail.com>
2015-05-05 15:39:33 -0500

    Update email digest number rounding

--- app/helpers/email_digest_helper.rb
+++ app/helpers/email_digest_helper.rb
@@ -8,6 +8,13 @@ module EmailDigestHelper
     end
   end
 
+  def format_number(num)
+    precision = 3
+    precision = 2 if num >= 1_000 && num < 100_000
+
+    number_to_human(num, :units => {:thousand => "k", :million => "m", :billion => "b"}, format: "%n%u", precision: precision)
+  end
+
   def self.template_name(site)
     if site.script_installed_at.nil?
       "New Email Digest (Not Installed)"
--- spec/helpers/email_digest_helper_spec.rb
+++ spec/helpers/email_digest_helper_spec.rb
@@ -1,4 +1,21 @@
 require 'spec_helper'
 
-describe EmailDigestHelper do
+describe EmailDigestHelper, type: :helper do
+  context "format_number" do
+    it "should format 1,580 as 1.6k" do
+      helper.format_number(1_580).should == "1.6k"
+    end
+
+    it "should format 12,800 as 13k" do
+      helper.format_number(12_800).should == "13k"
+    end
+
+    it "should format 874 as 874" do
+      helper.format_number(874).should == "874"
+    end
+
+    it "should format 112,500 as 112k" do
+      helper.format_number(112_300).should == "112k"
+    end
+  end
 end
