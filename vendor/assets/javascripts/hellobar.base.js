59dc3428fc02340a829cebb4781a2bfc8581ccbc
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-05-07 09:44:34 -0500

    creates at-runtime computation of "scrollTarget" concept
    this handles changing page sizes better

--- vendor/assets/javascripts/hellobar.base.js
+++ vendor/assets/javascripts/hellobar.base.js
@@ -1636,6 +1636,23 @@ var _HB = {
 
 };
 
+  // TEMP NOTE for 
+  // triggerOptions: [
+  //   {value: 'immidiately', label: 'Immediately'}
+  //   {value: 'wait-5', label: '5 second delay'}
+  //   {value: 'wait-10', label: '10 second delay'}
+  //   {value: 'wait-60', label: '60 second delay'}
+  //   {value: 'scroll-some', label: 'After scrolling a little'}
+  //   {value: 'scroll-middle', label: 'After scrolling to middle'}
+  //   {value: 'scroll-to-bottom', label: 'After scrolling to bottom'}
+  //   {value: 'exit-intent', label: 'User intends to leave'} 
+  //   {value: 'tab-inactive', label: 'User switches tabs'}
+  // ]
+
+
+
+
+
 /*
 CryptoJS v3.1
 code.google.com/p/crypto-js
