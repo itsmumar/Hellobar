e87cf5819e0feda41bf4f1d210e01ace8af83434
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-05-11 11:40:26 -0500

    remove crufty display_condition_attribute

--- db/migrate/20150423145614_view_triggers.rb
+++ db/migrate/20150423145614_view_triggers.rb
@@ -2,7 +2,6 @@ class ViewTriggers < ActiveRecord::Migration
   def change
 
     add_column :site_elements, :view_condition, :string
-    add_column :site_elements, :view_condition_attribute, :integer
 
   end
 end
