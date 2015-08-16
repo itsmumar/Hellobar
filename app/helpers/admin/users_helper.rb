6580ffaef7a2edcdee7cfdfcd5e5c15fada333b2
tjacobs3 <thmsjcbs@gmail.com>
2015-08-16 12:25:41 -0500

    Add additional billing detail to the admin users page [#100828302]

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,7 +1,7 @@
 module Admin::UsersHelper
   def bills_for(site)
     bills = Hash.new { |h, k| h[k] = [] } # Bill => [Refunds]
-    site.bills.select { |b| b.amount != 0 }.sort_by { |x| x.bill_at }.reverse.each do |bill|
+    site.bills.select { |b| b.subscription.nil? || !b.subscription.instance_of?(Subscription::Free) }.sort_by { |x| x.bill_at }.reverse.each do |bill|
       if bill.instance_of?(Bill::Refund)
         bills[bill.refunded_billing_attempt.bill] << bill
       else
