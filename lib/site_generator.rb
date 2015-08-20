215b34f446f74099a2a7ee53928a50da153942f2
Rebecca Miller-Webster <rebecca@miller-webster.com>
2015-08-20 13:12:46 -0500

    Add class to generate a site for a bar
    * change rake task to use the new SiteGenerator
    * only load the SiteGenerator file in dev & test

---
+++ lib/site_generator.rb
@@ -0,0 +1,46 @@
+class SiteGenerator
+  attr_reader :full_path, :site
+
+  def initialize(site_id, opts = {})
+    @site = Site.find(site_id)
+    @full_path = opts[:full_path] || generate_full_path(opts)
+  end
+
+  def generate_file
+    File.open(@full_path, 'w') do |file|
+      file.write(generate_html)
+    end
+  end
+
+
+  def generate_html
+    str = <<-EOS
+      <html>
+      <head>
+      <style></style>
+      </head>
+      <body style="background-color: #FFFFFF;">
+      <a onclick="console.log(‘BUTTON PUSHED')">HERE</a>
+      <div style="height:500px;">Content</div>
+      <div style="height:500px;">Content</div>
+      Content
+      <script>
+    EOS
+
+    str += @site.script_content
+    str += "</script><p>Generated on #{Time.now}</p></body></html>"
+    str
+  end
+
+  private
+
+  def generate_full_path(opts)
+    directory = opts[:directory]
+
+    return nil if directory.nil?
+
+    directory = Pathname.new(directory) unless directory.respond_to?(:join)
+    @full_path = directory.join("#{SecureRandom.hex}.html")
+  end
+
+end
---
+++ spec/lib/site_generator_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe SiteGenerator do
+  fixtures :all
+
+  let(:site) { sites(:zombo) }
+
+  before do
+    allow_any_instance_of(Site).to receive(:lifetime_totals).and_return({"1" => [[1,0]]})
+  end
+
+  describe "#initialize" do
+    it "finds the site" do
+      generator = described_class.new(site.id)
+
+      expect(generator.site).to eq(site)
+    end
+
+    it "takes a full path" do
+      full_path = "/Users/hello/bar.html"
+
+      generator = described_class.new(site.id, full_path: full_path)
+
+      expect(generator.full_path).to eq(full_path)
+    end
+
+    context "when directory is set" do
+      it "generates a full path in that directory" do
+        directory = Rails.root.join("spec/tmp")
+
+        generator = described_class.new(site.id, directory: directory)
+
+        expect(generator.full_path.to_s).to be_starts_with(directory.to_s)
+      end
+
+      it "generates an html file" do
+        directory = Rails.root.join("spec/tmp")
+
+        generator = described_class.new(site.id, directory: directory)
+
+        expect(File.extname(generator.full_path.to_s)).to eq(".html")
+      end
+    end
+  end
+
+  describe "#generate_html" do
+    it "includes the site's script content" do
+      allow_any_instance_of(ScriptGenerator).to receive(:pro_secret).and_return("asdf")
+      generator = described_class.new(site.id)
+
+      html = generator.generate_html
+
+      expect(html).to include(site.script_content)
+    end
+  end
+
+  describe "#generate_file" do
+    it "creates a file at full path" do
+      path = generate_path
+      generator = described_class.new(site.id, full_path: path)
+
+      generator.generate_file
+
+      expect(File.exists?(path)).to be_true
+
+      File.delete(path)
+    end
+
+    it "creates a file with the generated html" do
+      path = generate_path
+      generator = described_class.new(site.id, full_path: path)
+      allow(generator).to receive(:generate_html)
+
+      generator.generate_file
+
+      expect(generator).to have_received(:generate_html)
+
+      File.delete(path)
+    end
+
+    def generate_path
+      dir = Rails.root.join("spec/tmp")
+      Dir.mkdir(dir) unless File.directory?(dir)
+      dir.join("#{SecureRandom.hex}.html")
+    end
+  end
+end
