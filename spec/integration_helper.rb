ea5a34854b944097d8ebe81f81e09ff86f0255ac
tjacobs3 <thmsjcbs@gmail.com>
2015-09-23 11:32:29 -0500

    Add sign in / sign out tests [#104006072]

--- spec/integration_helper.rb
+++ spec/integration_helper.rb
@@ -4,15 +4,13 @@ require 'spec_helper'
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.include SiteGeneratorHelper
+  config.include FeatureHelper
 
   Capybara.javascript_driver = :webkit
+  OmniAuth.config.test_mode = true
 
   config.before(:all) do
     setup_site_generator
-  end
-
-  config.after(:all) do
-    teardown_site_generator
   end
 
   config.before(:suite) do
