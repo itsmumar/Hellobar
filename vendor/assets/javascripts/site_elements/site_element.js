a75677bbbdf49f92ee83869510b560540f5b1c42
John Butler <imtall@gmail.com>
2015-12-14 19:55:56 -0800

    Re-organized and separated JS

--- vendor/assets/javascripts/site_elements/site_element.js
+++ vendor/assets/javascripts/site_elements/site_element.js
@@ -1,42 +1,4 @@
-// IE 8 Support for initializers
-if (!Object.create) {
-    Object.create = function(o, properties) {
-        if (typeof o !== 'object' && typeof o !== 'function') throw new TypeError('Object prototype may only be an Object: ' + o);
-        else if (o === null) throw new Error("This browser's implementation of Object.create is a shim and doesn't support 'null' as the first argument.");
-        if (typeof properties != 'undefined') throw new Error("This browser's implementation of Object.create is a shim and doesn't support a second argument.");
-        function F() {};
-        F.prototype = o;
-        return new F();
-    };
-}
-// IE 8 support for bind
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function(oThis) {
-    if (typeof this !== 'function') {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-    }
 
-    var aArgs   = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        fNOP    = function() {},
-        fBound  = function() {
-          return fToBind.apply(this instanceof fNOP
-                 ? this
-                 : oThis,
-                 aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    if (this.prototype) {
-      // native functions don't have a prototype
-      fNOP.prototype = this.prototype; 
-    }
-    fBound.prototype = new fNOP();
-
-    return fBound;
-  };
-}
 
 var SiteElement = function(props) {
   for (var key in props) {
