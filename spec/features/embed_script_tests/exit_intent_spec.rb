1fd5fcb03303b28a5dfc2a6c07337721ecc2359f
tjacobs3 <thmsjcbs@gmail.com>
2016-01-06 12:54:25 -0600

    Exit intent uses document mouseleave [#111163708]

---
+++ spec/features/embed_script_tests/exit_intent_spec.rb
@@ -0,0 +1,36 @@
+require 'integration_helper'
+
+feature "element with exit intent", js: true do
+  let(:element) { FactoryGirl.create(:site_element, view_condition: "exit-intent") }
+  before { allow_any_instance_of(ScriptGenerator).to receive(:pro_secret).and_return('random') }
+
+  scenario "shows when document is blurred" do
+    path = generate_file_and_return_path(element.site.id)
+
+    visit "#{site_path_to_url(path)}"
+
+    # iframe should not be showing yet
+    page.has_selector?("#random-container")
+    expect(page).to have_selector("#random-container", visible: false)
+
+    # trigger blur event
+    page.find('body').trigger("blur")
+    expect(page).to have_selector("#random-container", visible: true)
+  end
+
+  scenario "shows when mouseenter and mouseleave have been triggered" do
+    path = generate_file_and_return_path(element.site.id)
+
+    visit "#{site_path_to_url(path)}"
+
+    # iframe should not be showing yet
+    page.has_selector?("#random-container")
+    expect(page).to have_selector("#random-container", visible: false)
+
+    # mouse enter then wait a sufficient amount of time then mouse exit
+    page.find('body').trigger("mouseenter")
+    sleep(2.5)
+    page.find('body').trigger("mouseleave")
+    expect(page).to have_selector("#random-container", visible: true)
+  end
+end
