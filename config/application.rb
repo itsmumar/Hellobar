f8e339cee2636ab0627ca89fc8a034c262d52ac1
tjacobs3 <thmsjcbs@gmail.com>
2016-02-22 11:02:43 -0600

    Remove references to in bar ads [#114119761]

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,13 @@ module Hellobar
     # code (config, etc.)
     # config.autoload_paths += %W(#{config.root}/lib)
 
+    config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
+
     config.sass.preferred_syntax = :sass
     config.action_mailer.default_url_options = { host: "www.hellobar.com" }
 
     config.assets.precompile += ['editor.css', 'static.css', 'admin.css', 'editor/vendor.js', 'editor/application.js', 'ember.js', '*.css.erb', '*.css.sass.erb']
     config.assets.paths << Rails.root.join('vendor', 'assets')
-
 
     config.handlebars.precompile = false
     config.handlebars.templates_root = 'editor/templates'
