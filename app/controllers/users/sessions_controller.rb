f36a0ca6e835073301e35518dd142575b67bbb75
Stephen Wooten <hello@stephenwooten.com>
2016-02-26 15:39:47 -0600

    [Finishes #114528821] Change the redirect flow for a user entering a failed password

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -52,7 +52,7 @@ class Users::SessionsController < Devise::SessionsController
           @user = User.new
 
           flash.now[:alert] = "Invalid email or password."
-          render action: :new
+          render action: :find_email
         end
       else
         render "pages/redirect_login"
@@ -61,11 +61,28 @@ class Users::SessionsController < Devise::SessionsController
       # The user used oauth to sign in so redirect them to that
       redirect_to "/auth/#{Authentication.joins(:user).where(users: {email: email}).first.provider}"
     else
-      return_val = super
-      cookies.permanent[:login_email] = email
-      # Record log in
-      Analytics.track(*current_person_type_and_id, "Logged In", {ip: request.remote_ip})
-      return return_val
+      @user = User.find_by(email: user_params[:email])
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+
+      if @user.valid_password?(user_params[:password])
+        sign_in(@user)
+
+        cookies.permanent[:login_email] = email
+        # Record log in
+        Analytics.track(*current_person_type_and_id, "Logged In", {ip: request.remote_ip})
+
+        redirect_to after_sign_in_path_for(@user)
+      else
+        flash.now[:alert] = "Invalid password."
+
+        render :find_email
+      end
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
