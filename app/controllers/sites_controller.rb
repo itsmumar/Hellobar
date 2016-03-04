9db5d9d9f63babfa2d7a7e7af4b3bbcd10cb3f3e
Stephen Wooten <hello@stephenwooten.com>
2016-03-04 13:28:25 -0600

    [Finishes #114824011] Add a cancellation button and flash notice when the params have a URL

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -14,6 +14,8 @@ class SitesController < ApplicationController
 
   def new
     @site = Site.new(url: params[:url])
+
+    flash.now[:notice] = "Are you sure you want to add the site #{@site.url}?" if params[:url]
   end
 
   def create
