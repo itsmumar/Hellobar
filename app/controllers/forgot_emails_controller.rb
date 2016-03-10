74b1f3b55fcc0c12ad7be133467b681ecfa01666
Stephen Wooten <hello@stephenwooten.com>
2016-03-10 12:37:20 -0600

    [#114239489] Forgot email flow DOESNT SEND EMAILS

---
+++ app/controllers/forgot_emails_controller.rb
@@ -0,0 +1,9 @@
+class ForgotEmailsController < ApplicationController
+  layout 'static'
+
+  def new
+  end
+
+  def create
+  end
+end
--- app/controllers/users/forgot_emails_controller.rb
+++ app/controllers/users/forgot_emails_controller.rb
@@ -1,3 +1,12 @@
 class Users::ForgotEmailsController < ApplicationController
   layout 'static'
+
+  def new
+  end
+
+  def create
+    MailerGateway.send_email("User Forgot Email", "support@hellobar.com", params)
+
+    redirect_to new_forgot_email_path, notice: "We'll get in touch with you shortly!"
+  end
 end
