4af405de5966ed0aebd8201bf6be79749e07c5bd
Stephen Wooten <hello@stephenwooten.com>
2016-03-11 11:03:24 -0600

    [Finishes #114239489] Update param names to match Grand Central

--- app/controllers/users/forgot_emails_controller.rb
+++ app/controllers/users/forgot_emails_controller.rb
@@ -5,8 +5,14 @@ class Users::ForgotEmailsController < ApplicationController
   end
 
   def create
-    MailerGateway.send_email("User Forgot Email", "support@hellobar.com", params)
+    MailerGateway.send_email("Forgot Email", "support@hellobar.com", forgot_email_params)
 
     redirect_to new_forgot_email_path, notice: "We'll get in touch with you shortly!"
+  end
+
+private
+
+  def forgot_email_params
+    params.permit(:site_url, :first_name, :last_name)
   end
 end
