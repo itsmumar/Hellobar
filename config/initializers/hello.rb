6b99b6ee96a2b81c00f7e30e900d5ac9f5973b7f
Pete Jabbour <petejabbour1@gmail.com>
2016-05-19 15:28:50 -0500

    [#118123205] removed initializer include call to lib files now deleted

--- config/initializers/hello.rb
+++ config/initializers/hello.rb
@@ -13,10 +13,6 @@ require "./lib/hello/wordpress_model"
 require "./lib/hello/wordpress_user"
 require "./lib/analytics"
 
-require "./lib/hello/messages/activity_message"
-require "./lib/hello/messages/announcement_activity_message"
-require "./lib/hello/messages/conversion_activity_message"
-
 require "./lib/hello/user_onboarding_campaigns/user_onboarding_campaign"
 require "./lib/hello/user_onboarding_campaigns/create_a_bar_campaign"
 require "./lib/hello/user_onboarding_campaigns/configure_your_bar_campaign"
