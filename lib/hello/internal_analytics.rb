8389f3041a0eafeb2a5ab47762a25e0625415f1c
Pete Jabbour <petejabbour1@gmail.com>
2016-06-14 11:55:24 -0500

    [#120866587] allow ab tests load in testing env

--- lib/hello/internal_analytics.rb
+++ lib/hello/internal_analytics.rb
@@ -48,17 +48,17 @@ module Hello
     # ================================================================
     # ==      REGISTER YOUR TESTS AT: lib/hello/ab_tests.yml        ==
     # ================================================================
-    unless Rails.env.test?
-      hash = YAML.load(File.read("lib/hello/ab_tests.yml"))
-      hash.each do |registered_test|
-        name            = registered_test["name"]
-        values          = registered_test["values"]
-        index           = registered_test["index"]
-        weights         = registered_test["weights"].present? ? registered_test["weights"] : []
-        user_start_date = registered_test["user_start_date"]
-        register_test(name, values, index, weights, user_start_date)
-      end
+
+    hash = YAML.load(File.read("lib/hello/ab_tests.yml"))
+    hash.each do |registered_test|
+      name            = registered_test["name"]
+      values          = registered_test["values"]
+      index           = registered_test["index"]
+      weights         = registered_test["weights"].present? ? registered_test["weights"] : []
+      user_start_date = registered_test["user_start_date"]
+      register_test(name, values, index, weights, user_start_date)
     end
+
 
     def ab_test_cookie_name
       AB_TEST_COOKIE
