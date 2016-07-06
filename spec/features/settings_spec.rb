83cd6727bfcede438ede16ae992c4db5bc79d516
Sean Corbett <stcorbett@gmail.com>
2016-07-06 17:29:46 -0500

    [#122003897]: use specifics on handling test variants (#984)
    Remove handling for ab-tests in cases where we don’t need it anymore
    due to tests having been concluded some time in the past

--- spec/features/settings_spec.rb
+++ spec/features/settings_spec.rb
@@ -2,8 +2,6 @@ require "integration_helper"
 
 feature "Manage Settings", js: true do
   before do
-    allow_any_instance_of(ApplicationController).
-      to receive(:get_ab_variation).and_return('original')
     @user = login
     @site = @user.sites.first
     @rule = @site.create_default_rules
@@ -15,12 +13,8 @@ feature "Manage Settings", js: true do
   after { devise_reset }
 
   context "add custom invoice address" do
-    scenario "is available" do
-      expect(page).to have_content("Add custom invoice address")
-    end
-
     scenario "updates when input is given" do
-      page.find("a.toggle-site-invoice-information").click
+      page.find('a', text: "Add custom invoice address").click
       fill_in "site_invoice_information", :with => "my cool address"
       click_button("Save & Update")
       visit edit_site_path(@site)
