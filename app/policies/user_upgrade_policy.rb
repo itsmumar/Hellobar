9f7930fcd16fcefa10dc34b0bcc457ff2bcd5369
An Pham <an.pham@crossover.com>
2016-08-09 15:16:55 +0700

    - Added new rule: user have scripts installed and active bar with views > 1000.

--- app/policies/user_upgrade_policy.rb
+++ app/policies/user_upgrade_policy.rb
@@ -8,7 +8,8 @@ class UserUpgradePolicy
   end
 
   def should_show_upgrade_suggest_modal?
-    return false if @has_paying_subscription
+    # Freemium user that have HB installed and active.
+    return false if @has_paying_subscription || no_script_installed?
     return false unless any_max_total_views_in_month?
 
     already_viewed_before? @user.upgrade_suggest_modal_last_shown_at
@@ -21,9 +22,14 @@ class UserUpgradePolicy
 
   private
 
+  def no_script_installed?
+    @user.sites.select{ |site| site.has_script_installed? }.empty?
+  end
+
   def any_max_total_views_in_month?
-    # At least one HB bar for that user should have received 1000 views in the last 30 days
-    @user.site_elements.any? { |site_element| site_element.total_views(days: 30) >= MAX_VIEW_IN_MONTH }
+    # At least one active HB bar for that user should have received
+    # 1000 views in the last 30 days
+    @user.site_elements.active.any? { |site_element| site_element.total_views(days: 30) >= MAX_VIEW_IN_MONTH }
   end
 
   def already_viewed_before?(last_shown_at)
