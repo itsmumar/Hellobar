964315b0651116efad1a223102cab2ff49e257e0
John Butler <imtall@gmail.com>
2016-08-14 11:58:13 -0700

    Fixed wordpress migration and minor updates to copy

--- app/models/font.rb
+++ app/models/font.rb
@@ -1,3 +1,24 @@
 class Font < ActiveHash::Base
   include ActiveModel::Serialization
+
+  class << self
+    def guess(input)
+      if input and input != ""
+        font_names = input.to_s
+        all_fonts = Font.all
+        # The font_name might be "Helvetica,sans-serif"
+        font_names.split(",").each do |font_name|
+          font_name = font_name.gsub(/^\s+/,"").gsub(/\s+$/,"")
+          # Try to find the font
+          possible_fonts = all_fonts.each do |font|
+            if font.value.downcase.include?(font_name.downcase)
+              # Return the first found font
+              return font
+            end
+          end
+        end
+      end
+      return nil
+    end
+  end
 end
