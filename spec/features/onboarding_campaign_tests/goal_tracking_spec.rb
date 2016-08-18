80a7a0163db9e4823c327dbdcb203c9b4ede2db6
An Pham <an.pham@crossover.com>
2016-08-18 15:40:17 +0700

    [WIP] Add debugger to check on CircleCI, because test failed on CI only.

--- spec/features/onboarding_campaign_tests/goal_tracking_spec.rb
+++ spec/features/onboarding_campaign_tests/goal_tracking_spec.rb
@@ -24,16 +24,14 @@ feature "User onboarding statuses get updated as they select a goal for their fi
     expect(goals.size > 1).to be_true
     expect(onboarding_status_setter).to receive(:selected_goal!).exactly(goals.size).times
     click_through_all_goal_interstitial_options
-
-    # wait for last ajax request complete
-    visit new_site_site_element_path(user.sites.first)
   end
 
   def click_through_all_goal_interstitial_options
     goals.each do |goal|
-      wait_for_ajax
-      visit new_site_site_element_path(user.sites.first)
       page.find(:xpath, "//h6[contains(text(),'#{goal}')]/following-sibling::a").click
+      wait_for_ajax
+      sleep 0.5
+      visit new_site_site_element_path(user.sites.first)
     end
   end
 end
