c780599dda1901243369f67822f1da07f5f2272e
Raj <raj_abc38@yahoo.com>
2016-10-14 01:00:20 +0530

    Schema migration update

--- db/migrate/20161003132619_update_storage_structure_of_collect_names_in_site_elements.rb
+++ db/migrate/20161003132619_update_storage_structure_of_collect_names_in_site_elements.rb
@@ -5,19 +5,33 @@ class UpdateStorageStructureOfCollectNamesInSiteElements < ActiveRecord::Migrati
     SiteElement.find_each do |site_element|
       collect_name = (site_element.settings[:collect_names] == 1)
 
-      id = (0...10).map { ('a'..'z').to_a[rand(6)] }.join
       fields_to_collect = site_element.settings[:fields_to_collect]
+      email_exists = false
+      name_exists = false
+      phone_exists = false
 
       if fields_to_collect.is_a?(Array)
-        exists = false
-        fields_to_collect.each { |ftc| exists = true if ftc["type"] == "builtin-name" }
+        fields_to_collect.each do |ftc|
+          name_exists = true if ftc["type"] == "builtin-name"
+          email_exists = true if ftc["type"] == "builtin-email"
+          phone_exists = true if ftc["type"] == "builtin-phone"
+        end
 
         site_element.settings.delete(:collect_names) if exists
       else
         site_element.settings.delete(:collect_names)
-        site_element.settings[:fields_to_collect] = [{ id: id, type: "builtin-name",
-                                                        is_enabled: collect_name }]
+        site_element.settings[:fields_to_collect] = [{ id: ((0...10).map { ('a'..'z').to_a[rand(6)] }.join),
+                                                       type: "builtin-name",
+                                                       is_enabled: collect_name }]
       end
+
+      site_element.settings[:fields_to_collect] << { id: ((0...10).map { ('a'..'z').to_a[rand(6)] }.join),
+                                                     type: "builtin-email",
+                                                     is_enabled: true } unless email_exists
+
+      site_element.settings[:fields_to_collect] << { id: ((0...10).map { ('a'..'z').to_a[rand(6)] }.join),
+                                                    type: "builtin-phone",
+                                                    is_enabled: true }  unless phone_exists
 
       site_element.save
     end
@@ -26,6 +40,7 @@ class UpdateStorageStructureOfCollectNamesInSiteElements < ActiveRecord::Migrati
   # This is a script to revert `fields_to_collect` to `collect_names`.
   # `self.down` can be run when `fields_to_collect` only have `name` realted element only.
   def self.down
+
     begin
       SiteElement.find_each do |site_element|
         collect_name = site_element.settings[:fields_to_collect][0][:is_enabled]
