f8df672c4a085150f550ce34b1a432805a43d962
Sergey Shakhov <sergey.shakhov@crossover.com>
2016-10-24 02:03:00 +0300

    Conversion: after-convert-options-mixin converted to ES6

---
+++ app/assets/javascripts/editor/controllers/mixins/after-convert-options-mixin.js
@@ -0,0 +1,9 @@
+HelloBar.AfterConvertOptions = Ember.Mixin.create({
+
+  showAfterConvertOptions: [
+    {value: true, label: 'Continue showing even after the visitor responds'},
+    {value: false, label: 'Stop showing after the visitor provides a response'}
+  ],
+
+  hideShowAfterConvertOptions: Ember.computed.equal('model.element_subtype', 'announcement')
+});
