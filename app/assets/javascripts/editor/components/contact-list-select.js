e9243f4c62b6bc3abf94e7c7f09c961f12bffec1
Sergey Shakhov <sergey.shakhov@crossover.com>
2016-10-24 03:00:26 +0300

    Conversion: components/contact-list-select, components/range-slider converted to ES6

---
+++ app/assets/javascripts/editor/components/contact-list-select.js
@@ -0,0 +1,59 @@
+HelloBar.ContactListSelectComponent = Ember.Component.extend({
+
+  classNames        : ['contact-list-wrapper'],
+  classNameBindings : ['isOpen:is-open', 'hasContactList:has-list'],
+  attributeBindings : ['tabindex'], // to make component focusable
+
+  tabindex: -1,
+  isOpen         : false,
+  hasContactList : Ember.computed.gt('options.length', 0),
+
+  init() {
+    if (this.get('hasContactList') && Ember.isEmpty(this.get('value'))) {
+      this.sendAction('setList', this.get('options.firstObject.id'));
+    }
+
+    return this._super();
+  },
+
+  _setSelectedList: ( function() {
+    let value = this.get('value') || 0;
+    let list = this.get('options').findBy('id', value);
+    return this.set('selectedList', list || this.get('options.firstObject'));
+  }).observes('value').on('init'),
+
+  focusOut() {
+    return this.set('isOpen', false);
+  },
+
+  actions: {
+
+    toggleOpen() {
+      return this.toggleProperty('isOpen');
+    },
+
+    newList() {
+      return this.sendAction('editList');
+    },
+
+    editList() {
+      return this.sendAction('editList', this.get('selectedList.id'));
+    },
+
+    listSelected(value) {
+      this.sendAction('setList', value);
+      return this.set('value', value);
+    }
+  }
+});
+
+//-----------  Contact List Child Views  -----------#
+
+HelloBar.ContactListOption = Ember.View.extend({
+
+  classNames: ['contact-list-option'],
+
+  click() {
+    return this.get('parentView').send('listSelected', this.get('option.id'));
+  }
+});
