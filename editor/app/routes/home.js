b84f9163ece627fe8f721d136d01933b83d53ce0
Sergey Shakhov <sergey.shakhov@crossover.com>
2016-11-02 12:43:27 +0300

    First route

--- editor/app/routes/home.js
+++ editor/app/routes/home.js
@@ -1,8 +1,10 @@
-HelloBar.HomeRoute = Ember.Route.extend({
+import Ember from 'ember';
+
+export default Ember.Route.extend({
 
   // Auto-redirects the index/home route to the first step
   redirect() {
-    if (HB_DATA.skipInterstitial || localStorage["stashedEditorModel"]) {
+    if ((HB_DATA && HB_DATA.skipInterstitial) || localStorage["stashedEditorModel"]) {
       // skip interstitial if it's explisitly set in global variable or if editor model was already stored in localStorage
       return this.replaceWith("settings");
     } else {
