a4ab77b934ecbd05d7caa2a2e6dde35ef55426ae
Raj <raj_abc38@yahoo.com>
2016-11-06 14:45:18 +0530

    Enclose existing headlines within `<p><strong>` tag

--- db/migrate/20161102031039_add_blocks_to_site_element.rb
+++ db/migrate/20161102031039_add_blocks_to_site_element.rb
@@ -1,9 +1,32 @@
 class AddBlocksToSiteElement < ActiveRecord::Migration
   def up
     add_column :site_elements, :blocks, :text
+
+    SiteElement.find_each do |site_element|
+      headline = site_element.headline
+
+      unless headline.include?("<p><strong>")
+        headline = "<p><strong>#{site_element.headline}</strong></p>"
+
+        # Skip callbacks & script regeneration here.
+        site_element.update_column(:headline, headline)
+      end
+    end
   end
 
   def down
     remove_column :site_elements, :blocks, :text
+
+    SiteElement.find_each do |site_element|
+      headline = site_element.headline
+
+      if headline.include?("<p><strong>")
+        headline.slice!("<p><strong>")
+        headline.slice!("</strong></p>")
+
+        # Skip callbacks & script regeneration here.
+        site_element.update_column(:headline, headline)
+      end
+    end
   end
 end
