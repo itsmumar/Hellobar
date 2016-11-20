17b5c0e34217287a82400ea60cf5153b7f48de47
Sergey Shakhov <sergey.shakhov@crossover.com>
2016-11-20 20:20:41 +0300

    Inputs, social-select component refactoring

--- editor/app/components/social-select.js
+++ editor/app/components/social-select.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  init() {
+    this._super(...arguments);
+    console.log('social-select-option', this);
+  },
+
   tagName: 'ul',
 
   classNames: ['social-select'],
@@ -9,27 +14,3 @@ export default Ember.Component.extend({
 
   isSelected: Ember.computed.notEmpty('selection')
 });
-
-
-//-----------  Social Option Child Views  -----------#
-
-HelloBar.SocialOption = Ember.View.extend({
-
-  tagName: 'li',
-
-  classNameBindings: ['content.service', 'isSelected'],
-
-  isSelected: ( function () {
-    return Ember.isEqual(this.get('content.value'), this.get('parentView.selection'));
-  }).property('parentView.selection'),
-
-  click(event) {
-    if (!this.get('isSelected')) {
-      this.set('parentView.selection', this.get('content.value'));
-    } else if (event.target.className === 'icon-close') {
-      this.set('parentView.selection', null);
-    }
-    return false;
-  }
-});
-
