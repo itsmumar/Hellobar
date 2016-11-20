2c141ec6b0d536e8da6137ce5ee883ef898b78ca
Sergey Shakhov <sergey.shakhov@crossover.com>
2016-11-21 01:05:28 +0300

    Targeting refactoring

--- editor/app/controllers/targeting/saved.js
+++ editor/app/controllers/targeting/saved.js
@@ -1,12 +1,26 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Controller.extend({
-  needs: "targeting",
+
+  targetingController: Ember.inject.controller('targeting'),
+
+  ruleOptions: Ember.computed('targetingController.ruleOptions'),
+
+  selectedRuleOption: (function() {
+    const ruleId = this.get('model.rule_id');
+    const options = this.get('ruleOptions');
+    return _.find(options, (option) => option.id === ruleId);
+  }).property('model.rule_id'),
 
   actions: {
 
     openRuleModal(ruleData) {
-      return this.get('controllers.targeting').send('openRuleModal', ruleData);
+      return this.get('targetingController').send('openRuleModal', ruleData);
+    },
+
+    selectTargetingRule(rule) {
+      this.set('model.rule_id', rule.id);
     }
   }
 });
