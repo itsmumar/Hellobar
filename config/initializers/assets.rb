7c08c632a704053b6a310985d0ff6c94c0ec97cd
Raj <raj_abc38@yahoo.com>
2016-11-22 22:57:50 +0530

    Add paths for new & edit explicitely for ember

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@ Rails.application.config.assets.precompile += %w( admin.js static.js dropper_tri
 Rails.application.config.assets.precompile += %w(hellobar.eot hellobar.woff hellobar.ttf hellobar.svg)
 Rails.application.config.assets.precompile += %w(hellobar-icons.eot hellobar-icons.woff hellobar-icons.ttf hellobar-icons.svg)
 Rails.application.config.assets.precompile += %w(eyedropper.svg receipt.css site_elements_controller.js team.js)
-Rails.application.config.assets.precompile += %w(editor/editor.js editor/editor.css editor/vendor.js editor/vendor.css)
+Rails.application.config.assets.precompile += %w(editor/editor-require.js editor/editor.js editor/editor.css editor/vendor.js editor/vendor.css editor/vendor/fonts/*)
