6f7a97b0ab91c37803257bbc9f55cf1e484fc488
Sergey Shakhov <sergey.shakhov@crossover.com>
2016-11-23 04:32:51 +0300

    locationType changed to hash (to prevent misc URL issues)

--- editor/config/environment.js
+++ editor/config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'editor',
     environment: environment,
-    rootURL: '/sites/2/site_elements/8/edit/',
-    locationType: 'auto',
+    rootURL: '/editor',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
