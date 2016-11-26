a959b35c94e7e12b9a1912aaf2936e681c40a633
Sergey Shakhov <sergey.shakhov@crossover.com>
2016-11-26 23:20:04 +0300

    Template support in grid theme selector

--- editor/app/components/theme-tile-grid.js
+++ editor/app/components/theme-tile-grid.js
@@ -1,11 +1,18 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Component.extend({
 
-  themes: availableThemes,
+  allThemes: availableThemes,
+
+  genericThemes: function () {
+    return _.filter(this.get('allThemes'), (theme) => theme.type === 'generic');
+  }.property('allThemes'),
+
+  templateThemes: function () {
+    return _.filter(this.get('allThemes'), (theme) => theme.type === 'template')
+  }.property('allThemes'),
 
   classNames: ['theme-tile-grid']
 });
-
-
 
