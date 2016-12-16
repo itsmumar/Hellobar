9e9e23671dddf456a8ae78c7984c7c3a6320a94c
Paweł Gościcki <pawel.goscicki@gmail.com>
2016-12-16 16:31:55 +0100

    Bring back Slack deploy notifications

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,3 @@
 server '54.235.76.197', user: 'hellobar', roles: %w{web db cron} # web1
 server '23.21.249.113', user: 'hellobar', roles: %w{web}         # web2
 server '54.211.192.176', user: 'hellobar', roles: %w{web worker} # worker1
-
-set :ssh_options, {
-  forward_agent: true
-}
-
-# slack notification integration
-set :slack_webhook, "https://hooks.slack.com/services/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"
-set :slack_channel, ['#hello-bar', '#hb-deployments', '#public-hb']
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,1 @@
 server 'staging.hellobar.com', user: 'hellobar', roles: %w{web db cron worker}
-
-set :ssh_options, {
-  forward_agent: true
-}
-
-# slack notification integration
-set :slack_webhook, "https://hooks.slack.com/services/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"
-set :slack_channel, ['#hello-bar']
