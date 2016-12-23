1e85130ca899c8e97ac2e35a4ae1ffc6215abd18
Paweł Gościcki <pawel.goscicki@gmail.com>
2016-12-23 14:07:27 +0100

    Treat symbols as metadata keys in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -123,6 +123,9 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  # Allow tagging js specs using just symbols
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
