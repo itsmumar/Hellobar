f63732a3ab70f61604bf4c685cda9e5b5908e55e
Paweł Gościcki <pawel.goscicki@gmail.com>
2016-12-27 12:52:39 +0100

    Fix Settings feature spec

--- spec/features/settings_spec.rb
+++ spec/features/settings_spec.rb
@@ -1,25 +1,32 @@
 require "integration_helper"
 
-feature "Manage Settings", js: true do
+feature "Manage Settings", :js do
   before do
     @user = login
     @site = @user.sites.first
     @rule = @site.create_default_rules
+
     allow_any_instance_of(Site).to receive(:lifetime_totals).and_return({"1" => [[1,0]]})
+
     payment_method = create(:payment_method, user: @user)
     @site.change_subscription(Subscription::Pro.new(schedule: "monthly"), payment_method)
+
     visit edit_site_path(@site)
   end
-  after { devise_reset }
 
-  context "add custom invoice address" do
-    scenario "updates when input is given" do
-      page.find("a", text: "Settings").click
-      page.find('a', text: "Add custom invoice address").click
-      fill_in "site_invoice_information", :with => "my cool address"
-      click_button("Save & Update")
-      visit edit_site_path(@site)
-      expect(page).to have_content("my cool address")
-    end
+  after do
+    devise_reset
+  end
+
+  scenario "it allows adding new custom invoice address" do
+    page.find('a', text: "Add custom invoice address").click
+
+    fill_in "site_invoice_information", :with => "my cool address"
+
+    click_button("Save & Update")
+
+    visit edit_site_path(@site)
+
+    expect(page).to have_content("my cool address")
   end
 end
