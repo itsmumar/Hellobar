447ae6f0a209fb006337fbd297a2e9518fc58dbb
Sergey Shakhov <sergey.shakhov@crossover.com>
2017-01-04 15:58:54 +0300

    Component references updated

--- editor/app/components/selection/social-select.js
+++ editor/app/components/selection/social-select.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  init() {
-    this._super(...arguments);
-    console.log('social-select-option', this);
-  },
-
   tagName: 'ul',
 
   classNames: ['social-select'],
