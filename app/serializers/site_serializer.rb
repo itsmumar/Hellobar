48505a2d92628fe86b125501e284ec2af068478d
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-01-26 20:45:09 +0100

    Serialize closable attribute in Site serializer

--- app/serializers/site_serializer.rb
+++ app/serializers/site_serializer.rb
@@ -46,12 +46,13 @@ class SiteSerializer < ActiveModel::Serializer
 
   def capabilities
     {
-      remove_branding:        object.capabilities.remove_branding?,
-      custom_targeted_bars:   object.capabilities.custom_targeted_bars?,
-      at_site_element_limit:  object.capabilities.at_site_element_limit?,
-      custom_thank_you_text:  object.capabilities.custom_thank_you_text?,
-      after_submit_redirect:  object.capabilities.after_submit_redirect?,
-      custom_html:  object.capabilities.custom_html?
+      remove_branding: object.capabilities.remove_branding?,
+      closable: object.capabilities.closable?,
+      custom_targeted_bars: object.capabilities.custom_targeted_bars?,
+      at_site_element_limit: object.capabilities.at_site_element_limit?,
+      custom_thank_you_text: object.capabilities.custom_thank_you_text?,
+      after_submit_redirect: object.capabilities.after_submit_redirect?,
+      custom_html: object.capabilities.custom_html?
     }
   end
 
