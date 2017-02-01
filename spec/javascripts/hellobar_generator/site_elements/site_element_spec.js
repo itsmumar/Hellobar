53c8c0cbf623eed0f62f97b6d93c61a082e42d8d
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-02-01 18:30:06 +0100

    Move SiteElement js specsc into appropriate place

--- spec/javascripts/hellobar_generator/site_elements/site_element_spec.js
+++ spec/javascripts/hellobar_generator/site_elements/site_element_spec.js
@@ -4,6 +4,44 @@
 var context = describe;
 
 describe("SiteElement", function() {
+  describe("#checkForDisplaySettings", function() {
+    beforeEach(function() {
+      iframe_window = {className: "my-cool-class"};
+      iframe_window.style = jasmine.createSpy('style');
+      siteElement = new HB.SiteElement({w: iframe_window, view_condition: ""});
+      spyOn(HB, "viewed")
+    });
+
+    it("records a view", function () {
+      siteElement.checkForDisplaySetting();
+      expect(HB.viewed).toHaveBeenCalled();
+    });
+
+    it("does not record a view when don't record view is set", function () {
+      siteElement.dontRecordView = true;
+      siteElement.checkForDisplaySetting();
+      expect(HB.viewed).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("#onClosed", function() {
+    context("callbacks", function() {
+      beforeEach(function() {
+        spyOn(HB, 'trigger');
+        siteElement.onClosed();
+      });
+
+      it("triggers the elementDismissed callback", function() {
+        expect(HB.trigger).toHaveBeenCalledWith('elementDismissed');
+      });
+
+      it("triggers the converted callback", function() {
+        expect(HB.trigger).toHaveBeenCalledWith('closed', siteElement);
+      });
+    });
+
+  });
+
   describe("#imageFor", function() {
     siteElement = null;
     beforeEach(function() {
