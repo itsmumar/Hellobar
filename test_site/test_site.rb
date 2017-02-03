9ce5fede79c9719fe9dee42f2038541d3ac60b43
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-02-03 17:09:48 +0100

    Expose test Sinatra app to the outside internet (on demand)

--- test_site/test_site.rb
+++ test_site/test_site.rb
@@ -3,6 +3,12 @@ require 'sinatra'
 # Run `rake test_site:generate` to create test.html for last site
 # Alternately, pass the site id: `rake test_site:generate[SITE_ID]`
 # Run `rake test_site:run` to run this file & navigate to localhost:4567
+
+# Bind Sinatra app to an actual network interface
+# This allows you to expose your IP on the router and access your local Sinatra
+# app from the internet
+set :bind, '0.0.0.0'
+
 get '*' do
   File.read('test_site/public/test.html')
 end
