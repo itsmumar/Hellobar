9290df1592f18e7cd6c1e6bff61d35ee434dcaf5
Sergey Shakhov <sergey.shakhov@crossover.com>
2017-02-20 19:57:56 +0300

    Application controller decomposed to multiple mixins

---
+++ editor/app/controllers/application.mixin.validation.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+// GLOBALS: isValidNumber functions
+
+export default Ember.Mixin.create({
+
+  validation: Ember.inject.service(),
+
+  initializeValidation() {
+    const validationRules = [
+      {
+        fieldName: 'phone_number',
+        validator: (model) => {
+          if (Ember.get(model, 'element_subtype') === 'call') {
+            const phoneNumber = Ember.get(model, 'phone_number');
+            if (!phoneNumber) {
+              return 'Phone number is required';
+            }
+            const countryCode = Ember.get(model, 'phone_country_code');
+            if (!isValidNumber(phoneNumber, countryCode)) {
+              return 'Wrong phone number for specified country';
+            }
+          }
+          return null;
+        }
+      }
+    ];
+    this.get('validation').add('main', validationRules);
+  }
+
+
+});
