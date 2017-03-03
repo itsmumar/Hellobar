bce610ed29459b86fc98030d0eb78ee8aaf28198
Anton Sozontov <a.sozontov@gmail.com>
2017-03-04 04:47:55 +0700

    Fix specs

--- app/models/font.rb
+++ app/models/font.rb
@@ -5,11 +5,15 @@ class Font < ActiveHash::Base
     # @param [String] font_names; e.g. "Helvetica,sans-serif"
     def guess(font_names)
       return unless font_names.present?
-
+      font_names = font_names.to_s.split(',')
       all_fonts = Font.all
-      font_names.to_s.split(',').find do |font_name|
-        all_fonts.find { |font| font.same?(font_name.strip) }
+
+      font_names.each do |font_name|
+        font = all_fonts.find { |font| font.same?(font_name.strip) }
+        return font if font.present?
       end
+
+      nil
     end
   end
 
--- spec/models/font_spec.rb
+++ spec/models/font_spec.rb
@@ -2,24 +2,28 @@ require 'spec_helper'
 
 describe Font do
   describe '#guess' do
-    it 'should guess correctly with the first value' do
-      expect(Font.guess('Arial ')).to eq(Font.find('arial'))
+    it 'guesses correctly with the first value' do
+      expect(Font.guess('Arial,Helvetica')).to eq(Font.find('arial'))
     end
 
-    it 'should guess correctly using the second value' do
-      expect(Font.guess('monkey , Arial')).to eq(Font.find('arial'))
+    it 'guesses correctly with the first value' do
+      expect(Font.guess('Arial')).to eq(Font.find('arial'))
     end
 
-    it 'should be case-insensitive' do
-      expect(Font.guess(' ARIAL ')).to eq(Font.find('arial'))
+    it 'guesses correctly using the second value' do
+      expect(Font.guess('monkey,Arial')).to eq(Font.find('arial'))
+    end
+
+    it 'is case-insensitive' do
+      expect(Font.guess('ARIAL')).to eq(Font.find('arial'))
       expect(Font.guess('arial')).to eq(Font.find('arial'))
     end
 
-    it 'should ignore extra spaces' do
+    it 'ignores extra spaces' do
       expect(Font.guess(' monkey, Arial')).to eq(Font.find('arial'))
     end
 
-    it "should return nil when it can't find a match" do
+    it 'returns nil when it can\'t find a match' do
       expect(Font.guess('monkey')).to be_nil
     end
   end
