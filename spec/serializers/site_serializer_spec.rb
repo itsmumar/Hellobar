da3dec71d029eea3fdd3e1be7090160d9a023b5a
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-03-08 15:36:54 +0100

    Serialize ProManaged capabilities for Site
    (so that they are available in frontend)

--- spec/serializers/site_serializer_spec.rb
+++ spec/serializers/site_serializer_spec.rb
@@ -9,7 +9,8 @@ describe SiteSerializer do
     expect(serializable_hash[:capabilities]).to be_a Hash
 
     %i(remove_branding closable custom_targeted_bars at_site_element_limit
-       custom_thank_you_text after_submit_redirect custom_html).each do |key|
+       custom_thank_you_text after_submit_redirect custom_html content_upgrades
+       autofills geolocation_injection).each do |key|
       expect(serializable_hash[:capabilities]).to have_key key
     end
   end
