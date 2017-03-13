9deb20dae0316e37e7c0c5cea96a739d1cddcab4
Anton Sozontov <a.sozontov@gmail.com>
2017-03-14 01:42:47 +0700

    Move AlwaysSuccessfulPaymentMethodDetails and AlwaysFailsPaymentMethodDetails to factory

--- spec/factories/payment_method_details.rb
+++ spec/factories/payment_method_details.rb
@@ -1,7 +1,33 @@
-FactoryGirl.define do
-  factory :payment_method_details do
+class AlwaysSuccessfulPaymentMethodDetails < PaymentMethodDetails
+  def charge(_amount_in_dollars)
+    [true, "fake-txn-id-#{ Time.now.to_i }"]
   end
 
-  factory :always_successful_billing_details, parent: :payment_method_details, class: 'AlwaysSuccessfulPaymentMethodDetails' do
+  def refund(_amount_in_dollars, original_transaction_id)
+    [true, "fake-refund-id-#{ Time.now.to_i } (original: #{ original_transaction_id }"]
+  end
+
+  def data
+    {}
+  end
+end
+
+class AlwaysFailsPaymentMethodDetails < PaymentMethodDetails
+  def charge(_amount_in_dollars)
+    [false, 'There was some issue with your payment (fake)']
+  end
+
+  def refund(_amount_in_dollars, _original_transaction_id)
+    [false, 'There was some issue with your refund (fake)']
+  end
+end
+
+FactoryGirl.define do
+  factory :payment_method_details do
+    factory :always_successful_billing_details, class: 'AlwaysSuccessfulPaymentMethodDetails' do
+    end
+
+    factory :always_fails_payment_method_details, class: 'AlwaysFailsPaymentMethodDetails' do
+    end
   end
 end
