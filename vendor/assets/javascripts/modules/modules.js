b559aa576d1f4ea374335372e2d7f1febdc80842
Anton Sozontov <a.sozontov@gmail.com>
2017-03-14 04:41:05 +0700

    Add index.js to modules

--- vendor/assets/javascripts/modules/modules.js
+++ vendor/assets/javascripts/modules/modules.js
@@ -1,9 +1,3 @@
-//= require ./base/base.storage
-//= require ./base/base.ajax
-//= require ./base/base.dom
-//= require ./base/base.serialization
-//= require ./base/base.capabilities
-//= require ./base/base.site
-//= require ./geolocation/geolocation
-//= require ./geolocation/geolocation.injection
-//= require ./autofills/autofills
+//= require ./base
+//= require ./geolocation
+//= require ./autofills
