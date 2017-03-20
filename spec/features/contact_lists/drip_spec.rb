2e118db7574831c62e1e8f0e505415454ca07185
Anton Sozontov <a.sozontov@gmail.com>
2017-03-21 03:19:26 +0700

    Cover with feature spec

---
+++ spec/features/contact_lists/drip_spec.rb
@@ -0,0 +1,25 @@
+require 'integration_helper'
+require 'service_provider_integration_helper'
+
+feature 'Drip Integration', js: true do
+  let(:provider) { 'drip' }
+
+  before do
+    @fake_data_api_original = Hellobar::Settings[:fake_data_api]
+    Hellobar::Settings[:fake_data_api] = true
+    @user = login
+  end
+
+  after do
+    Hellobar::Settings[:fake_data_api] = @fake_data_api_original
+  end
+
+  scenario 'displays campaigns and tags' do
+    open_provider_form(@user, provider)
+    page.find('.button.ready').click
+
+    expect(page).to have_content('Choose a Drip campaign to sync with')
+    expect(page).to have_content('Apply Tags (Optional)')
+    expect(page).to have_link('+ Add tag')
+  end
+end
