ab58b99c4a356c860ba89dbce77efc8da129d555
Sergey Shakhov <sergey.shakhov@crossover.com>
2017-03-22 23:53:15 +0300

    A couple of improvements

--- app/controllers/site_elements_controller.rb
+++ app/controllers/site_elements_controller.rb
@@ -26,7 +26,6 @@ class SiteElementsController < ApplicationController
     @site_element = SiteElement.new(
       font_id: SiteElement.columns_hash['font_id'].default,
       rule: @site.rules.first,
-      theme: nil,
       show_branding: !@site.capabilities(true).remove_branding?,
       closable: false,
       settings: { url: @site.url, url_to_like: @site.url }
