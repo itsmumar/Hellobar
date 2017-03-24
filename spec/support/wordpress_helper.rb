0ca5e38eb412a6328ae7e706cec687dfc5338f7b
Anton Sozontov <a.sozontov@gmail.com>
2017-03-25 03:10:56 +0700

    Use before :suite instead of :all

--- spec/support/wordpress_helper.rb
+++ spec/support/wordpress_helper.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |config|
-  config.before :all do
+  config.before :suite do
     # stub columns hash so that Hello::WordpressUser.respond_to?(:find_by_email) works
     Hello::WordpressUser.instance_variable_set :@columns_hash, 'email' => ''
   end
