b57597c162e36343d9d0d4fdfbc5ef2037ae63fe
Anton Sozontov <a.sozontov@gmail.com>
2017-03-25 23:31:35 +0700

    Remove obsolete gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.coverage_dir 'tmp/coverage'
+SimpleCov.start do
+  # we test seeds in a different way - just by running them
+  add_filter '/lib/seeds/'
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
