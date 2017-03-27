b769beb58d7e199da91ba09f449262c152545786
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-03-27 11:19:26 +0200

    Inline Capistrano tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -200,3 +200,35 @@ namespace :prerequisites do
     end
   end
 end
+
+namespace :ember do
+  desc 'Build Ember Application'
+  task :build do
+    on roles(:web) do
+      execute "cd #{ fetch(:ember_app_path) } && ember build --environment=production >/dev/null"
+    end
+  end
+
+  desc 'Move non digested specific fonts to public'
+  task :move_non_digest_fonts do
+    on roles(:web) do
+      execute "cd #{ fetch(:release_path) } && cp app/assets/fonts/* public/assets/"
+    end
+  end
+end
+
+namespace :node do
+  desc 'Install node modules'
+  task :npm_install do
+    on roles(:web) do
+      execute "cd #{ fetch(:ember_app_path) } && npm install >/dev/null"
+    end
+  end
+
+  desc 'Install bower components'
+  task :bower_install do
+    on roles(:web) do
+      execute "cd #{ fetch(:ember_app_path) } && bower install >/dev/null"
+    end
+  end
+end
