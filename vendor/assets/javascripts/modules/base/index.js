dbd9ab3e33fba3de4927eb42fdd2163298116669
Sergey Shakhov <sergey.shakhov@crossover.com>
2017-03-30 15:35:10 +0300

    Index modules files updated

--- vendor/assets/javascripts/modules/base/index.js
+++ vendor/assets/javascripts/modules/base/index.js
@@ -1,6 +1,18 @@
-//= require ./base.storage
 //= require ./base.ajax
-//= require ./base.dom
-//= require ./base.serialization
+//= require ./base.bus
 //= require ./base.capabilities
+//= require ./base.coloring
+//= require ./base.deferred
+//= require ./base.dom
+//= require ./base.environment
+//= require ./base.format
+//= require ./base.preview
+//= require ./base.sanitizing
+//= require ./base.selfcheck
+//= require ./base.serialization
 //= require ./base.site
+//= require ./base.storage
+//= require ./base.styling
+//= require ./base.templating
+//= require ./base.timezone
+
--- vendor/assets/javascripts/modules/modules.js
+++ vendor/assets/javascripts/modules/modules.js
@@ -1,3 +1,8 @@
 //= require ./base
-//= require ./geolocation
 //= require ./autofills
+//= require ./contentUpgrades
+//= require ./elements
+//= require ./geolocation
+//= require ./tracking
+//= require ./visitor
+
