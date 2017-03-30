22e725d00b781106b047c77d4fe18d5ff4a8b08d
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-03-30 16:22:36 +0200

    Fix more specs broken by adding :success payment_method trait

--- spec/factories/billing_attempts.rb
+++ spec/factories/billing_attempts.rb
@@ -1,13 +1,15 @@
 FactoryGirl.define do
   factory :billing_attempt do
-    payment_method_details factory: :always_successful_billing_details
     status :success
+
+    payment_method_details factory: :always_successful_billing_details
 
     trait :success
 
     trait :failed do
-      payment_method_details factory: :always_fails_payment_method_details
       status :failed
+
+      payment_method_details factory: :always_fails_payment_method_details
     end
   end
 end
