04aac1635f43deeffb72b623865e45a228a7c6e9
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-03-30 11:42:25 +0200

    Fix removal of payment methods in admin panel

--- spec/factories/payment_method_details.rb
+++ spec/factories/payment_method_details.rb
@@ -22,12 +22,39 @@ class AlwaysFailsPaymentMethodDetails < PaymentMethodDetails
   end
 end
 
+class FakeCyberSourceCreditCard < CyberSourceCreditCard
+  def save_to_cybersource
+  end
+end
+
 FactoryGirl.define do
   factory :payment_method_details do
     factory :always_successful_billing_details, class: 'AlwaysSuccessfulPaymentMethodDetails' do
     end
 
     factory :always_fails_payment_method_details, class: 'AlwaysFailsPaymentMethodDetails' do
+    end
+
+    factory :cyber_source_credit_card, class: 'FakeCyberSourceCreditCard' do
+      payment_method
+
+      data do
+        {
+          'number' => '4012 8888 8888 1881',
+          'month' => 12,
+          'year' => Date.current.year + 1,
+          'first_name' => 'John',
+          'last_name' => 'Doe',
+          'address1' => 'Sunset Blv 205',
+          'city' => 'San Francisco',
+          'state' => 'CA',
+          'zip' => '94016',
+          'country' => 'US',
+          'brand' => 'visa',
+          'verification_value' => '777',
+          'token' => 'cc_token'
+        }
+      end
     end
   end
 end
