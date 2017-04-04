d74db56be371dbaf61cb1a67743d7cce9820a904
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-04-04 20:08:00 +0200

    Update sentry config in Rails initializer

---
+++ config/initializers/raven.rb
@@ -0,0 +1,9 @@
+Raven.configure do |config|
+  config.dsn = Hellobar::Settings[:sentry_dsn] if Hellobar::Settings[:sentry_dsn].present?
+
+  # filter sensitive data
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+
+  # enable only on `production` environment
+  config.environments = ['production']
+end
