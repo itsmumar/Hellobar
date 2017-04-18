9e47bc092c878bc1eddcef7063ceef89637d483e
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-04-18 13:17:39 +0200

    Make locally generated site more usable

--- lib/site_generator.rb
+++ lib/site_generator.rb
@@ -15,51 +15,57 @@ class SiteGenerator
   end
 
   def generate_html
-    str = <<-EOS
+    str = <<~EOS.strip
+      <!DOCTYPE html>
       <html>
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <style></style>
+        <title>Hello Bar Test Site</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset="utf-8" />
       </head>
+
       <body style="background-color: #FFFFFF;">
-      <a onclick="console.log('BUTTON PUSHED')">HERE</a>
-      <div style="height:500px; background-color: yellow;">TOP OF PAGE CONTENT<script id="hb-cu-2">window.onload = function() {hellobar('contentUpgrades').show(1);};</script></div>
-      <div style="height:500px;">
-        <h1>Autofills testing playground</h1>
-        <p>In order to test the autofills you need to:</p>
+        <a onclick="console.log('BUTTON PUSHED')" style="cursor: pointer">CLICK ME</a>
 
-        <ul>
-          <li>switch your site to the ProManaged subscription</li>
-          <li>create an email collection bar (any style)</li>
-          <li>create a new autofill rule: <code>listen_selector: `input#f-builtin-email`, populate_selector: `input.email`</code></li>
-          <li>regenerate the site: <code>rake test_site:generate</code></li>
-          <li>visit local testing page (this page)</li>
-          <li>fill in the email address in the bar and click 'Subscripbe'</li>
-          <li>reload the page</li>
-          <li>observe the input below is autofilled with the value from the the bar (from the localStorage value actually)</li>
-        </ul>
+        <div style="height:200px; background-color: yellow;">
+          TOP OF PAGE CONTENT
+          <script id="hb-cu-2">
+            window.onload = function() {hellobar('contentUpgrades').show(1);};
+          </script>
+        </div>
 
-        <p>
-          Autofill: <input type="email" name="email" class="email" style="font-size: 14px; width: 300px; padding: 5px 6px" />
-        </p>
-      </div>
-      <div style="height:500px; background-color: #eee;">Content</div>
-      <div style="height:500px;">Content</div>
-      <div style="height:500px; background-color: #eee;">Content</div>
-      <div style="height:500px;">Content</div>
-      <div style="height:500px; background-color: #eee;">Content</div>
-      <div style="height:500px;">Content</div>
-      <div style="height:500px; background-color: #eee;">Content</div>
-      <div style="height:500px;">Content</div>
-      <div style="height:500px; background-color: #eee;">Content</div>
-      <div style="height:500px;">Content</div>
-      <div style="height:500px; background-color: pink;">BOTTOM OF PAGE CONTENT</div>
-      <script>
+        <div style="height:400px;">
+          <h1>Autofills testing playground</h1>
+          <p>In order to test the autofills you need to:</p>
+
+          <ul>
+            <li>switch your site to the ProManaged subscription</li>
+            <li>create an email collection bar (any style)</li>
+            <li>create a new autofill rule: <code>listen_selector: `input#f-builtin-email`, populate_selector: `input.email`</code></li>
+            <li>regenerate the site: <code>rake test_site:generate</code></li>
+            <li>visit local testing page (this page)</li>
+            <li>fill in the email address in the bar and click 'Subscripbe'</li>
+            <li>reload the page</li>
+            <li>observe the input below is autofilled with the value from the the bar (from the localStorage value actually)</li>
+          </ul>
+
+          <p>
+            Autofill: <input type="email" name="email" class="email" style="font-size: 14px; width: 300px; padding: 5px 6px" />
+          </p>
+        </div>
+
+        <script>
     EOS
 
     str += @site.script_content(@compress)
-    str += "</script><p>Generated on #{ Time.current }</p></body></html>"
-    str
+
+    str += <<~EOS.strip
+        </script>
+
+        <p>Generated on #{ Time.current }</p>
+      </body>
+      </html>
+    EOS
   end
 
   private
