0ef787a3923c1c693b128e9b34d9b0c48548ff10
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-04-19 11:26:04 +0200

    Handle UnknownFormat exceptions gracefully

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -1,14 +1,26 @@
 describe 'Error requests' do
-  context 'Not Found errors' do
-    %i(html js css json text).each do |format|
-      describe "GET #{ format }" do
-        it 'does not raise and returns 404 error' do
+  context 'NotFound errors' do
+    %w(html js css json txt).each do |format|
+      describe "GET *.#{ format }" do
+        it 'does not raise and returns 404 error code' do
           expect {
             get "/nonexisting-page.#{ format }"
           }.not_to raise_exception
 
           expect(response.code).to eq '404'
         end
+      end
+    end
+  end
+
+  context 'UnknownFormat errors' do
+    describe 'GET *.php' do
+      it 'does not raise and returns 422 error code' do
+        expect {
+          get '/wp-login.php'
+        }.not_to raise_exception
+
+        expect(response.code).to eq '422'
       end
     end
   end
