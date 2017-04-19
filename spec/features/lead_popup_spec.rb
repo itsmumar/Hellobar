d1cc31bb0f967b5374db7e2825bf2232a63e9f6a
Anton Sozontov <a.sozontov@gmail.com>
2017-04-19 22:29:28 +0700

    Set countryCode in lead popup spec

--- spec/features/lead_popup_spec.rb
+++ spec/features/lead_popup_spec.rb
@@ -14,8 +14,8 @@ feature 'Lead data popup', :js do
 
   scenario 'new user must fill out questionnaire' do
     OmniAuth.config.add_mock(:google_oauth2, uid: '12345', info: { email: user.email })
-
     visit root_path
+    page.execute_script 'localStorage.setItem("countryCode", "US")'
 
     fill_in 'site[url]', with: 'mewgle.com'
     click_button 'sign-up-button'
