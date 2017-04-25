642907c510dc8ba07c3002366a58d500f5178d61
Anton Sozontov <a.sozontov@gmail.com>
2017-04-25 16:02:10 +0700

    Migrate data from refunded_billing_attempt to refund

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424104821) do
+ActiveRecord::Schema.define(version: 20170425084623) do
 
   create_table "admin_login_attempts", force: true do |t|
     t.string   "email"
