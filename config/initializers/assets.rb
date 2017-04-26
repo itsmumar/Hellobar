5ec5ea1be49866bcffd56b29e7040ef96055d71f
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-04-26 23:07:13 +0200

    Fix assets precompilation directive in initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += /\.(?:svg|eot|woff|ttf)$/
+Rails.application.config.assets.precompile += [/\.(?:svg|eot|woff|ttf)$/]
 Rails.application.config.assets.precompile += %w[admin.js static.js jquery.minicolors.png]
 Rails.application.config.assets.precompile += %w[hellobar.eot hellobar.woff hellobar.ttf hellobar.svg]
 Rails.application.config.assets.precompile += %w[hellobar-icons.eot hellobar-icons.woff hellobar-icons.ttf hellobar-icons.svg]
