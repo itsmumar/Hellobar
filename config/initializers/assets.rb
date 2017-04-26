f2b05e72354a92387643982cb00dacb9a1963833
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-04-26 17:00:54 +0200

    Cleanup environment configs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += /\.(?:svg|eot|woff|ttf)$/
 Rails.application.config.assets.precompile += %w[admin.js static.js jquery.minicolors.png]
 Rails.application.config.assets.precompile += %w[hellobar.eot hellobar.woff hellobar.ttf hellobar.svg]
 Rails.application.config.assets.precompile += %w[hellobar-icons.eot hellobar-icons.woff hellobar-icons.ttf hellobar-icons.svg]
