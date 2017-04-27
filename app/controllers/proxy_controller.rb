c2056a535599fb1654c33432102e460b78c166c6
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-04-27 19:21:08 +0200

    Tweak ProxyController styling

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,11 +1,9 @@
 class ProxyController < ApplicationController
   def proxy
-    if Rails.env.test?
-      render text: 'ok'
-    else
-      body = Net::HTTP.get(proxy_url)
-      send_data body, disposition: :inline, type: 'image/png', filename: 'image.png'
-    end
+    return render(text: 'ok') if Rails.env.test?
+
+    body = Net::HTTP.get(proxy_url)
+    send_data body, disposition: :inline, type: 'image/png', filename: 'image.png'
   end
 
   private
