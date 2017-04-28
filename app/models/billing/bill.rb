316ac45a64b7863417ec2e318abafb6214b6d09f
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-04-28 19:02:13 +0200

    Use Time.current in Bill model

--- app/models/billing/bill.rb
+++ app/models/billing/bill.rb
@@ -25,7 +25,7 @@ class Bill < ActiveRecord::Base
   enum status: %i[pending paid voided]
 
   scope :with_amount, -> { where('bills.amount > 0') }
-  scope :due_now, -> { pending.with_amount.where('? >= bill_at', Time.now) }
+  scope :due_now, -> { pending.with_amount.where('? >= bill_at', Time.current) }
 
   def during_trial_subscription?
     subscription.amount != 0 && subscription.payment_method.nil? && amount == 0 && paid?
