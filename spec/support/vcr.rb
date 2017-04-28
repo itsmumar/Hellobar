ce23a549b790804c14cc9b5520413988f3c8e86f
Anton Sozontov <a.sozontov@gmail.com>
2017-04-28 19:13:24 +0700

    Clean up

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,9 +2,8 @@ VCR.configure do |c|
   c.ignore_localhost = true
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
-  c.debug_logger = Rails.root.join('tmp', 'vcr.log').open('w+')
   c.default_cassette_options = {
-    record: :new_episodes,
+    record: :once,
     match_requests_on: [
       :method,
       VCR.request_matchers.uri_without_param(:oauth_timestamp, :oauth_nonce, :oauth_signature, :oauth_signature_method)
