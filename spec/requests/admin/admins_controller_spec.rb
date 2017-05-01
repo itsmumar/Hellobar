d8e11272800273a51d7320870be1e278d3e89ead
Anton Sozontov <a.sozontov@gmail.com>
2017-05-01 15:26:17 +0700

    Implement request specs for admin/admins

---
+++ spec/requests/admin/admins_controller_spec.rb
@@ -0,0 +1,21 @@
+describe Admin::AdminsController do
+  let!(:admin) { create(:admin) }
+  before { stub_current_admin(admin) }
+
+  describe 'GET admin_admins_path' do
+    it 'allows admins to see all admins' do
+      get admin_admins_path
+      expect(response).to be_success
+    end
+  end
+
+  describe 'PUT unlock_admin_admin_path' do
+    let(:locked_admin) { create(:admin, locked: true) }
+
+    it 'unlocks the admin' do
+      expect {
+        put unlock_admin_admin_path(locked_admin)
+      }.to change { locked_admin.reload.locked }.from(true).to(false)
+    end
+  end
+end
