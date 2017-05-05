18bf1da5f8b253986047b1159589924cda66676c
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-05-05 13:30:38 +0200

    Simplify Settings proxying
    (with an exception in .rubocop.yml)

--- app/settings/settings.rb
+++ app/settings/settings.rb
@@ -1,16 +1,16 @@
 module Settings
   class << self
-    # Proxy Rails.application.secrets
-    def method_missing name, *args, &block
-      if name.to_s != 'new'
-        Rails.application.secrets.public_send name, *args, &block
-      else
-        super
-      end
+    def new
+      raise NoMethodError
     end
 
-    def respond_to_missing? name, _include_private = false
-      name.to_s != 'new' ? true : false
+    # Proxy Rails.application.secrets
+    def method_missing name, *args, &block
+      Rails.application.secrets.public_send name, *args, &block
+    end
+
+    def respond_to_missing? _name, _include_private = false
+      true
     end
   end
 end
--- spec/settings/settings_spec.rb
+++ spec/settings/settings_spec.rb
@@ -3,10 +3,6 @@ describe Settings do
     expect { Settings.new }.to raise_exception NoMethodError
   end
 
-  it 'does not respond to :new' do
-    expect(Settings).not_to respond_to :new
-  end
-
   it 'is a proxy to Rails.application.secrets' do
     expect(Settings.secret_key_base).to eql Rails.application.secrets.secret_key_base
   end
