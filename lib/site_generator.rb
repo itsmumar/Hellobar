e6b4f8e6ca9909d6d631cf6690bfe7f4e1b3329d
Anton Sozontov <a.sozontov@gmail.com>
2017-05-05 07:09:23 +0700

    No need to precompile assets in site generator

--- lib/site_generator.rb
+++ lib/site_generator.rb
@@ -5,7 +5,6 @@ class SiteGenerator
     @site = Site.preload_for_script.find(site_id)
     @full_path = opts[:full_path] || generate_full_path(opts)
     @compress = opts.fetch(:compress, false)
-    StaticScriptAssets.precompile
   end
 
   def generate_file
