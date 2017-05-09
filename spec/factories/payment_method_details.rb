df01bafb0c5bf3f2db54fc49df3609576d7e920e
Anton Sozontov <a.sozontov@gmail.com>
2017-05-10 03:38:47 +0700

    Add more specs for cybersource

--- spec/factories/payment_method_details.rb
+++ spec/factories/payment_method_details.rb
@@ -16,23 +16,43 @@ FactoryGirl.define do
     factory :cyber_source_credit_card, class: 'FakeCyberSourceCreditCard' do
       payment_method
 
-      data do
-        {
-          'number' => '4012 8888 8888 1881',
-          'month' => 12,
-          'year' => Date.current.year + 1,
-          'first_name' => 'John',
-          'last_name' => 'Doe',
-          'address1' => 'Sunset Blv 205',
-          'city' => 'San Francisco',
-          'state' => 'CA',
-          'zip' => '94016',
-          'country' => 'US',
-          'brand' => 'visa',
-          'verification_value' => '777',
-          'token' => 'cc_token'
-        }
-      end
+      data factory: :payment_data
+    end
+  end
+
+  factory :payment_data, class: Hash do
+    skip_create
+
+    number '4012 8888 8888 1881'
+    month 12
+    year { Date.current.year + 1 }
+    first_name 'John'
+    last_name 'Doe'
+    address1 'Sunset Blv 205'
+    city 'San Francisco'
+    state 'CA'
+    zip '94016'
+    country 'US'
+    brand 'visa'
+    verification_value '777'
+    token 'cc_token'
+
+    initialize_with do
+      {
+        'number' => number,
+        'month' => month,
+        'year' => year,
+        'first_name' => first_name,
+        'last_name' => last_name,
+        'address1' => address1,
+        'city' => city,
+        'state' => state,
+        'zip' => zip,
+        'country' => country,
+        'brand' => brand,
+        'verification_value' => verification_value,
+        'token' => token
+      }
     end
   end
 end
