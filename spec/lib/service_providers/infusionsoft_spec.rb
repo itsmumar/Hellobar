fa9c4b43af721dcf1b5710dfa54d01353e6968e2
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-05-15 12:56:19 +0200

    Fix nil names being sent to InfusionSoft

--- spec/lib/service_providers/infusionsoft_spec.rb
+++ spec/lib/service_providers/infusionsoft_spec.rb
@@ -15,27 +15,55 @@ describe ServiceProviders::Infusionsoft do
   end
 
   describe '#subscribe' do
-    it 'should call contact_add_with_dup_check' do
-      data = { Email: 'test@test.com' }
+    let(:email) { 'test@test.com' }
+
+    it 'calls contact_add_with_dup_check' do
+      name = 'Gal Anonymous'
+      first_name, last_name = name.split
+      data = { Email: email, FirstName: first_name, LastName: last_name }
       service_provider.instance_variable_set(:@contact_list, contact_list)
+
       expect(Infusionsoft).to receive(:contact_add_with_dup_check).with(data, :Email)
-      service_provider.subscribe(nil, 'test@test.com')
+
+      service_provider.subscribe(nil, email, name)
     end
 
-    it 'should tag the user with all of the tags when present' do
+    it 'passes empty strings if name is nil to contact_add_with_dup_check' do
+      data = { Email: email, FirstName: '', LastName: '' }
+      service_provider.instance_variable_set(:@contact_list, contact_list)
+
+      expect(Infusionsoft).to receive(:contact_add_with_dup_check).with(data, :Email)
+
+      service_provider.subscribe(nil, email, nil)
+    end
+
+    it 'passes all parts of the name to contact_add_with_dup_check' do
+      name = 'Mr Gal The Third Son Anonymous'
+      first_name = name.split[0..-2].join ' '
+      last_name = name.split[-1]
+      data = { Email: email, FirstName: first_name, LastName: last_name }
+      service_provider.instance_variable_set(:@contact_list, contact_list)
+
+      expect(Infusionsoft).to receive(:contact_add_with_dup_check).with(data, :Email)
+
+      service_provider.subscribe(nil, email, name)
+    end
+
+    it 'tags the user with all of the tags when present' do
       contact_list.data = { 'tags' => %w[1 2 3] }
       service_provider.instance_variable_set(:@contact_list, contact_list)
 
       expect(Infusionsoft).to receive(:contact_add_to_group).exactly(3).times { nil }
 
-      service_provider.subscribe(nil, 'test@test.com')
+      service_provider.subscribe(nil, email)
     end
 
-    it 'should NOT tag the user when no tags are present' do
-      expect(Infusionsoft).to_not receive(:contact_add_to_group)
+    it 'does NOT tag the user when no tags are present' do
       service_provider.instance_variable_set(:@contact_list, contact_list)
 
-      service_provider.subscribe(nil, 'test@test.com')
+      expect(Infusionsoft).to_not receive(:contact_add_to_group)
+
+      service_provider.subscribe(nil, email)
     end
   end
 end
