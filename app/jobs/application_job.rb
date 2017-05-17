6f01097ba8d9ef1144d341d38d6dd6cadb4bb822
Anton Sozontov <a.sozontov@gmail.com>
2017-05-17 23:34:28 +0700

    Send all ActiveJob's exceptions to sentry.io

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,7 @@
 class ApplicationJob < ActiveJob::Base
   queue_as { "hb3_#{ Rails.env }_lowpriority" }
+
+  rescue_from StandardError do |exception|
+    Raven.capture_exception exception
+  end
 end
