cd40206f48c7171f087b7699d86ba78c3e54720d
Anton Sozontov <a.sozontov@gmail.com>
2017-05-19 00:40:00 +0700

    Remove unused methods and cover ContentUpgrade with specs

--- app/models/site_elements/content_upgrade.rb
+++ app/models/site_elements/content_upgrade.rb
@@ -3,19 +3,12 @@ class ContentUpgrade < SiteElement
 
   validates_attachment :content_upgrade_pdf, presence: true, content_type: { content_type: 'application/pdf' }
 
-  def content_upgrade_key
-    "#{ Site.id_to_script_hash(site.id) }/#{ id }.pdf"
-  end
-
   def content_upgrade_download_link
     content_upgrade_pdf.url
   end
 
   def content_upgrade_script_tag
-    '<script id="hb-cu-' + id.to_s + '">window.onload = function() {hellobar("contentUpgrades").show(' + id.to_s + ');};</script>'
-  end
-
-  def content_upgrade_wp_shortcode
-    '[hellobar_content_upgrade id="' + id.to_s + '"]'
+    content = %(window.onload = function() {hellobar("contentUpgrades").show(#{ id });};)
+    %(<script id="hb-cu-#{ id }">#{ content }</script>)
   end
 end
