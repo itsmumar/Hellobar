ed72cb06573897455777560dd18c501bc97a6013
Anton Sozontov <a.sozontov@gmail.com>
2017-05-22 02:47:21 +0700

    Set raven context for jobs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,9 @@ class ApplicationJob < ActiveJob::Base
   rescue_from StandardError do |exception|
     Raven.capture_exception exception
   end
+
+  before_perform do |job|
+    Raven.extra_context arguments: job.arguments, job_id: job.job_id, queue_name: job.queue_name
+    Raven.tags_context job: job.class.to_s
+  end
 end
