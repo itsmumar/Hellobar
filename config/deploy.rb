f1a86ac8d7af7e62a40a26663af2d80f968e2960
Anton Sozontov <a.sozontov@gmail.com>
2017-05-23 02:02:59 +0700

    Hotfix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :shoryuken_role, :worker
 # proper name for the main_queue on the Edge server is `hb3_edge`, however
 # hellobar_backend servers are configured to send SQS messages into `hellobar_edge`,
 # so we need to use this name until we are able to reconfigure it at hellobar_backend.
-set :queue_prefix, -> { fetch(:stage) == 'edge' ? 'hellobar' : 'hb3' }
+set :queue_prefix, -> { fetch(:stage) == :edge ? 'hellobar' : 'hb3' }
 
 # there is shoryuken_queues but it doesn't work because it produce '--queue foo --queue bar' options
 # which doesn't supported by shoryuken anymore
