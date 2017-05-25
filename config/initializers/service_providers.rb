51081d9f2267971fcaf0f33ed7873b1e6e136760
Anton Sozontov <a.sozontov@gmail.com>
2017-05-26 01:47:28 +0700

    Revert initializer

--- config/initializers/service_providers.rb
+++ config/initializers/service_providers.rb
@@ -1,5 +1,5 @@
-# require './lib/service_provider'
-# require './lib/service_providers/email'
-# require './lib/service_providers/embed_code_provider'
-# require './lib/service_providers/webhook'
-# Dir['./lib/service_providers/*.rb'].each { |f| require f }
+require './lib/service_provider'
+require './lib/service_providers/email'
+require './lib/service_providers/embed_code_provider'
+require './lib/service_providers/webhook'
+Dir['./lib/service_providers/**/*.rb'].each { |f| require f }
