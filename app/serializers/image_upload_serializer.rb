147fde07e40b792e880c4c824f78a6f275d349ff
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-05-28 12:38:31 +0200

    Add missing :id attribute to ImageUploadSerializer

--- app/serializers/image_upload_serializer.rb
+++ app/serializers/image_upload_serializer.rb
@@ -1,5 +1,5 @@
 class ImageUploadSerializer < ActiveModel::Serializer
-  attributes :description, :url, :small_url, :medium_url, :large_url,
+  attributes :id, :description, :url, :small_url, :medium_url, :large_url,
     :modal_url, :image_file_name, :image_content_type, :image_file_size,
     :image_updated_at, :created_at, :updated_at, :site_id, :preuploaded_url,
     :theme_id, :version
