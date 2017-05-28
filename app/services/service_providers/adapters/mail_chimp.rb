e5f9a83c8429fb6186595e90b5f972deca4f75ea
Anton Sozontov <a.sozontov@gmail.com>
2017-05-29 00:31:54 +0700

    Cover mail_chimp adapter with simple specs

--- app/services/service_providers/adapters/mail_chimp.rb
+++ app/services/service_providers/adapters/mail_chimp.rb
@@ -3,16 +3,11 @@ module ServiceProviders
     class MailChimp < Base
       register :mailchimp
 
-      def self.oauth?
-        true
-      end
-
       def initialize(identity)
-        client = Gibbon::Request.new(
+        super Gibbon::Request.new(
           api_key: identity.credentials['token'],
           api_endpoint: identity.extra['metadata']['api_endpoint']
         )
-        super client
       end
 
       def lists
@@ -20,7 +15,12 @@ module ServiceProviders
       end
 
       def subscribe(list_id, params)
-        client.lists(list_id).members.create(params)
+        client.lists(list_id).members.create body: prepare_params(params)
+      end
+
+      def batch_subscribe(list_id, subscribers, double_optin = true)
+        operations = prepare_batch_request(list_id, subscribers, double_optin)
+        client.batches.create(body: { operations: operations })
       end
 
       private
@@ -28,6 +28,31 @@ module ServiceProviders
       def raw_lists
         raw_lists = client.lists.retrieve(params: { count: 100 })['lists']
         raw_lists.presence || []
+      end
+
+      def prepare_batch_request(list_id, subscribers, double_optin = true)
+        subscribers.map do |subscriber|
+          {
+            method: 'POST',
+            path: "lists/#{ list_id }/members",
+            body: prepare_params(subscriber, double_optin).to_json
+          }
+        end
+      end
+
+      def prepare_params(subscriber, double_optin = true)
+        email, name = subscriber.values_at(:email, :name)
+
+        { email_address: email }.tap do |body|
+          body[:status] = double_optin ? 'pending' : 'subscribed'
+
+          if name.present?
+            first_name, last_name = name.split(' ', 2)
+            body[:merge_fields] = {}
+            body[:merge_fields][:FNAME] = first_name if first_name.present?
+            body[:merge_fields][:LNAME] = last_name if last_name.present?
+          end
+        end
       end
     end
   end
---
+++ spec/services/service_providers/adapters/mail_chimp_spec.rb
@@ -0,0 +1,59 @@
+describe ServiceProviders::Adapters::MailChimp, :no_vcr do
+  define_urls(
+    lists: 'http://apiendpoint/3.0/lists?count=100',
+    subscribe: 'http://apiendpoint/3.0/lists/57afe96172/members',
+    batch_subscribe: 'http://apiendpoint/3.0/batches'
+  )
+
+  let(:config_source) { double('config_source', extra: { 'metadata' => { 'api_endpoint' => 'http://apiendpoint' } }, credentials: { 'token' => 'api_key' }) }
+  let(:adapter) { described_class.new(config_source) }
+  let(:list_id) { '57afe96172' }
+
+  describe '#initialize' do
+    let(:auth) { { access_token: 'token' } }
+
+    it 'initializes Gibbon::Request' do
+      expect(Gibbon::Request).to receive(:new).with(api_key: 'api_key', api_endpoint: 'http://apiendpoint').and_call_original
+      expect(adapter.client).to be_a Gibbon::Request
+    end
+  end
+
+  describe '#lists' do
+    allow_request :get, :lists
+
+    it 'returns array of id => name' do
+      expect(adapter.lists).to eql [{ 'id' => list_id.to_s, 'name' => 'List1' }]
+    end
+  end
+
+  describe '#subscribe' do
+    allow_request :post, :subscribe, body: '{"email_address":"example@email.com","status":"pending"}' do |stub|
+      let(:subscribe_request) { stub }
+    end
+
+    it 'sends subscribe request' do
+      adapter.subscribe(list_id, email: 'example@email.com')
+      expect(subscribe_request).to have_been_made
+    end
+  end
+
+  describe '#batch_subscribe' do
+    body = {
+      operations: [
+        { method: 'POST', path: 'lists/list_id/members', body: '{"email_address":"example1@email.com","status":"pending"}' },
+        { method: 'POST', path: 'lists/list_id/members', body: '{"email_address":"example2@email.com","status":"pending"}' }
+      ]
+    }
+
+    allow_request :post, :batch_subscribe, body: body do |stub|
+      let(:batch_subscribe_request) { stub }
+    end
+
+    let(:subscribers) { [{ email: 'example1@email.com' }, { email: 'example2@email.com' }] }
+
+    it 'sends post request to /audience_members' do
+      adapter.batch_subscribe 'list_id', subscribers
+      expect(batch_subscribe_request).to have_been_made
+    end
+  end
+end
