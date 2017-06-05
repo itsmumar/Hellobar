08d18c68ca91bec048e9880a6356cd3cd2985e93
Percy Hanna <percy@hellobar.com>
2017-06-05 12:10:07 -0600

    Revert "Remove image_upload_id from theme serialization"
    This reverts commit 9124dc23f56b8a7e6f62b55c0d5419eb53043cf1.

--- app/serializers/theme_serializer.rb
+++ app/serializers/theme_serializer.rb
@@ -1,3 +1,7 @@
 class ThemeSerializer < ActiveModel::Serializer
-  attributes :id, :type, :name, :element_types, :defaults, :fonts, :image
+  attributes :id, :type, :name, :element_types, :defaults, :fonts, :image, :image_upload_id
+
+  def image_upload_id
+    ImageUpload.find_by(theme_id: object.id).try(:id)
+  end
 end
