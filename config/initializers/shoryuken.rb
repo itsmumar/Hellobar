973e9fbca725fe3de01a6a094450f5e8b2b01ab3
Anton Sozontov <a.sozontov@gmail.com>
2017-06-05 16:10:59 +0700

    Remove RavenMiddleware

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,29 +1,3 @@
-Shoryuken.configure_client do |config|
-  config.sqs_client = Aws::SQS::Client.new
-end
-
-Shoryuken.configure_server do |config|
-  config.sqs_client = Aws::SQS::Client.new
-
-  Rails.logger = Shoryuken::Logging.logger
-  Rails.logger.level = Rails.application.config.log_level
-
-  config.server_middleware do |chain|
-    chain.add RavenMiddleware
-  end
-end
-
-class RavenMiddleware
-  include Shoryuken::Util
-
-  def call(worker, queue, sqs_msg, body)
-    yield
-  rescue => e
-    Raven.capture_exception(e, extra: { message: sqs_msg.as_json, worker: worker_name(worker.class, sqs_msg, body), queue: queue })
-    raise e
-  end
-end
-
 # this is needed to avoid parsing errors
 # will be removed when we refactor hellobar_backend
 class CustomWorkerRegistry < Shoryuken::DefaultWorkerRegistry
@@ -34,6 +8,17 @@ class CustomWorkerRegistry < Shoryuken::DefaultWorkerRegistry
     Raven.capture_message('Could not parse sqs message', extra: { message: message.body, sqs_msg: message.as_json })
     Rails.logger.error "Could not parse sqs message: #{ message.body }; #{ message.as_json }"
   end
+end
+
+Shoryuken.configure_client do |config|
+  config.sqs_client = Aws::SQS::Client.new
+end
+
+Shoryuken.configure_server do |config|
+  config.sqs_client = Aws::SQS::Client.new
+
+  Rails.logger = Shoryuken::Logging.logger
+  Rails.logger.level = Rails.application.config.log_level
 end
 
 Shoryuken.worker_registry = CustomWorkerRegistry.new
