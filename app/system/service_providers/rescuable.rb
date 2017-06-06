085bbffaa3d3b4e9f0f27d6d1a2949a4dec32efb
Anton Sozontov <a.sozontov@gmail.com>
2017-06-07 05:39:04 +0700

    Simplify Rescuable

--- app/system/service_providers/rescuable.rb
+++ app/system/service_providers/rescuable.rb
@@ -6,30 +6,26 @@ module ServiceProviders::Rescuable
   def lists
     super
   rescue => exception
-    rescue_with_handler(:lists, exception)
+    rescue_with_handler(exception)
   end
 
   def subscribe(*args)
-    @arguments = args
     super
   rescue => exception
-    rescue_with_handler(:subscribe, exception)
+    rescue_with_handler(exception)
   end
 
   def batch_subscribe(*args)
-    @arguments = args
     super
   rescue => exception
-    rescue_with_handler(:batch_subscribe, exception)
+    rescue_with_handler(exception)
   end
 
   private
 
-  attr_reader :arguments
-
-  # return value of the handler
-  def rescue_with_handler(method, exception)
+  # override ActiveSupport::Rescuable#rescue_with_handler and return value of the handler
+  def rescue_with_handler(exception)
     raise(exception) unless (handler = handler_for_rescue(exception))
-    handler.arity != 0 ? handler.call(method, exception) : handler.call
+    handler.arity != 0 ? handler.call(exception) : handler.call
   end
 end
