ae56c7c4e944fd075f362736493cf1b29bb47c96
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-06-13 17:23:37 +0200

    Bring back changes from PR #1573
    Long running branches...

--- editor/app/components/interstitial/interstitial-email.js
+++ editor/app/components/interstitial/interstitial-email.js
@@ -1,28 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  showEmailVolume: false,
-
-  monthlyPageviews: function () {
-    return this.get('model.site.monthly_pageviews') || 0;
-  }.property(),
-
-  formattedMonthlyPageviews: function () {
-    return this.get('monthlyPageviews').toLocaleString();
-  }.property(),
-
-  hasEnoughSubscribers: function () {
-    return this.get('monthlyPageviews') > 1000;
-  }.property(),
-
-  calculatedSubscribers: function () {
-    return Math.round(this.get('monthlyPageviews') * 0.005);
-  }.property(),
-
-  formattedCalculatedSubscribers: function () {
-    return this.get('calculatedSubscribers').toLocaleString();
-  }.property(),
-
   inputIsInvalid: function () {
     return !!(
       Ember.isEmpty(this.get('model.headline')) ||
