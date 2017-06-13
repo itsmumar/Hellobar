4d96de47fa88d0f918612dfec530e0442de36a37
Anton Sozontov <a.sozontov@gmail.com>
2017-06-14 03:42:20 +0700

    Log error/success to rails logs

--- app/system/service_provider/rails_logger.rb
+++ app/system/service_provider/rails_logger.rb
@@ -1,14 +1,19 @@
 module ServiceProvider::RailsLogger
   def subscribe(*args)
-    log :subscribe, *args
-    super
+    log :subscribe, *args do
+      super
+    end
   end
 
   private
 
   def log(method, *args)
     tags = "[ServiceProvider] [#{ adapter.class.name.demodulize }] [contact_list:#{ @contact_list&.id }:remote:#{ remote_list_id }]"
-    msg = "#{ tags } Performing ##{ method } with arguments #{ args.inspect }"
-    Rails.logger.info msg
+    Rails.logger.info "#{ tags } Performing ##{ method } with arguments #{ args.inspect }"
+    yield
+    Rails.logger.info "#{ tags } Performed ##{ method } successfully with arguments #{ args.inspect }"
+  rescue => e
+    Rails.logger.error "#{ tags } Error on ##{ method } #{ e.inspect }"
+    raise e
   end
 end
