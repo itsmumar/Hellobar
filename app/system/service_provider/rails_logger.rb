ed11777352f80c844f258ba35499d5d3c06f75f3
Anton Sozontov <a.sozontov@gmail.com>
2017-06-14 17:52:02 +0700

    Return original value from log

--- app/system/service_provider/rails_logger.rb
+++ app/system/service_provider/rails_logger.rb
@@ -10,8 +10,9 @@ module ServiceProvider::RailsLogger
   def log(method, *args)
     tags = "[ServiceProvider] [#{ adapter.class.name.demodulize }] [contact_list:#{ @contact_list&.id }:remote:#{ remote_list_id }]"
     Rails.logger.info "#{ tags } Performing ##{ method } with arguments #{ args.inspect }"
-    yield
-    Rails.logger.info "#{ tags } Performed ##{ method } successfully with arguments #{ args.inspect }"
+    yield.tap do
+      Rails.logger.info "#{ tags } Performed ##{ method } successfully with arguments #{ args.inspect }"
+    end
   rescue => e
     Rails.logger.error "#{ tags } Error on ##{ method } #{ e.inspect }"
     raise e
