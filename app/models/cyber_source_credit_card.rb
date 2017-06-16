30b6a0affd9cf2d27b61de8c1c59ce493d65ae23
Anton Sozontov <a.sozontov@gmail.com>
2017-06-17 00:57:35 +0700

    Move CyberSourceCreditCard to app/models

---
+++ app/models/cyber_source_credit_card.rb
@@ -0,0 +1,118 @@
+class CyberSourceCreditCard < PaymentMethodDetails
+  class BillingAddress < OpenStruct
+  end
+
+  CC_FIELDS = %w[number month year first_name last_name brand].freeze
+  ADDRESS_FIELDS = %w[city state zip address country].freeze
+  # Note: any fields not included here will be stripped out when setting
+  FIELDS = CC_FIELDS + ADDRESS_FIELDS + ['token']
+  # These are the required fields to be set
+  REQUIRED_FIELDS = FIELDS - %w[brand token]
+
+  def name
+    "#{ brand&.capitalize || 'Credit Card' } ending in #{ last_digits.presence || '???' }"
+  end
+
+  def grace_period
+    15.days
+  end
+
+  def last_digits
+    ActiveMerchant::Billing::CreditCard.last_digits data['number'].gsub(/[^\d]/, '')
+  end
+
+  def brand
+    data['brand']
+  end
+
+  def address
+    @address ||=
+      begin
+        attributes = ADDRESS_FIELDS.inject({}) { |hash, key| hash.update key.to_sym => data[key] }
+        attributes[:address1] = attributes.delete(:address)
+        BillingAddress.new(attributes)
+      end
+  end
+
+  def data=(new_data)
+    data = new_data.stringify_keys.select { |key| FIELDS.include?(key) }
+    data['number'] = ActiveMerchant::Billing::CreditCard.mask(data['number'])
+    self[:data] = data
+  end
+
+  def charge(amount_in_dollars)
+    raise 'credit card token does not exist' if token.blank?
+    return true, 'Amount was zero' if amount_in_dollars == 0
+
+    if amount_in_dollars.blank? || amount_in_dollars < 0
+      raise ArgumentError, "Invalid amount: #{ amount_in_dollars.inspect }"
+    end
+
+    response = gateway.purchase(amount_in_dollars * 100, formatted_token, order_id: order_id)
+    audit << "Charging #{ amount_in_dollars.inspect }, got response: #{ response.inspect }"
+    return false, response.message unless response.success?
+
+    [true, response.authorization]
+  rescue => e
+    audit << "Error charging #{ amount_in_dollars.inspect }: #{ e.message }"
+    raise
+  end
+
+  def refund(amount_in_dollars, original_transaction_id)
+    raise 'Can not refund money until saved' unless persisted? && token
+    return true, 'Amount was zero' if amount_in_dollars == 0
+
+    if amount_in_dollars.blank? || amount_in_dollars < 0
+      raise ArgumentError, "Invalid amount: #{ amount_in_dollars.inspect }"
+    end
+
+    if original_transaction_id.blank?
+      raise 'Can not refund without original transaction ID'
+    end
+
+    response = gateway.refund(amount_in_dollars * 100, original_transaction_id)
+    audit << "Refunding #{ amount_in_dollars.inspect } to #{ original_transaction_id.inspect }, got response: #{ response.inspect }"
+    return false, response.message unless response.success?
+
+    [true, response.authorization]
+  rescue => e
+    audit << "Error refunding #{ amount_in_dollars.inspect } to #{ original_transaction_id.inspect }: #{ e.message }"
+    raise
+  end
+
+  def delete_token
+    return if token.blank?
+    update_columns data: data.merge('token' => nil)
+  end
+
+  def token
+    data['token']
+  end
+
+  def order_id
+    # The order_id is fairly irrelevant
+    "#{ payment_method&.id || 'NA' }-#{ Time.current.to_i }"
+  end
+
+  protected
+
+  # ActiveMerchant requires the token in this form
+  def formatted_token
+    format_token(token)
+  end
+
+  def format_token(token)
+    ";#{ token };"
+  end
+
+  private
+
+  def gateway
+    @gateway ||=
+      ActiveMerchant::Billing::CyberSourceGateway.new(
+        login: Settings.cybersource_login,
+        password: Settings.cybersource_password,
+        ignore_avs: true
+      )
+  end
+end
---
+++ spec/models/cyber_source_credit_card_spec.rb
@@ -0,0 +1,121 @@
+describe CyberSourceCreditCard do
+  let(:payment_method) { create :payment_method }
+  let(:data) { create :payment_data }
+  let(:credit_card) { CyberSourceCreditCard.new data: data }
+  let(:gateway) { double('CyberSourceGateway') }
+  let(:last_billing_log) { BillingLog.last }
+
+  describe '#delete_token' do
+    let!(:credit_card) { create :cyber_source_credit_card, token: 'token' }
+
+    it 'deletes token when it is present' do
+      expect { credit_card.delete_token }.to change(credit_card, :token).to(nil)
+    end
+  end
+
+  describe '#grace_period' do
+    it 'is 15.days' do
+      expect(credit_card.grace_period).to eql 15.days
+    end
+  end
+
+  describe '#data=' do
+    it 'sanitizes credit card number' do
+      credit_card.data = data.merge(number: '4242424242424242')
+      expect(credit_card.data['number']).to eq 'XXXX-XXXX-XXXX-4242'
+    end
+
+    context 'with extra fields' do
+      it 'removes them from data' do
+        credit_card.data = data.merge('foo' => 'bar')
+        expect(credit_card.data['foo']).to be_nil
+      end
+    end
+  end
+
+  describe '#name' do
+    let(:credit_card) { CyberSourceCreditCard.new data: { number: '4242424242421234' } }
+
+    specify { expect(credit_card.name).to eql 'Credit Card ending in 1234' }
+
+    context 'without number' do
+      let(:credit_card) { CyberSourceCreditCard.new data: { number: '' } }
+
+      specify { expect(credit_card.name).to eql 'Credit Card ending in ???' }
+    end
+
+    context 'with brand' do
+      let(:credit_card) { CyberSourceCreditCard.new data: { brand: 'visa', number: '4242424242421234' } }
+
+      specify { expect(credit_card.name).to eql 'Visa ending in 1234' }
+
+      context 'and without number' do
+        let(:credit_card) { CyberSourceCreditCard.new data: { brand: 'visa', number: '' } }
+
+        specify { expect(credit_card.name).to eql 'Visa ending in ???' }
+      end
+    end
+  end
+
+  describe '#charge', :freeze do
+    let(:credit_card) { create :cyber_source_credit_card, :saved }
+    let(:order_id) { "#{ credit_card.payment_method.id }-#{ Time.current.to_i }" }
+    let(:response) { double(success?: true, authorization: '1') }
+
+    it 'calls gateway.purchase' do
+      expect { credit_card.charge(100) }
+        .to make_gateway_call(:purchase).with(10_000, ';cc_token;', order_id: order_id)
+    end
+
+    context 'when invalid amount' do
+      it 'raises ArgumentError' do
+        expect { credit_card.charge(-1) }.to raise_error(ArgumentError, 'Invalid amount: -1')
+        expect { credit_card.charge(nil) }.to raise_error(ArgumentError, 'Invalid amount: nil')
+      end
+    end
+
+    context 'when error is raise' do
+      it 'creates BillingLog' do
+        expect { credit_card.charge(100) }
+          .to make_gateway_call(:purchase).and_raise_error('an error')
+          .and raise_error('an error').and change(BillingLog, :count).by 1
+
+        expect(last_billing_log.message).to eql 'Error charging 100: an error'
+      end
+    end
+  end
+
+  describe '#refund', :freeze do
+    let(:credit_card) { create :cyber_source_credit_card, :saved }
+    let(:order_id) { "#{ credit_card.payment_method.id }-#{ Time.current.to_i }" }
+
+    it 'calls gateway.refund' do
+      expect { credit_card.refund(100, '1') }
+        .to make_gateway_call(:refund).with(10_000, '1')
+    end
+
+    context 'when invalid amount' do
+      it 'raises ArgumentError' do
+        expect { credit_card.refund(-1, 'id') }.to raise_error(ArgumentError, 'Invalid amount: -1')
+        expect { credit_card.refund(nil, 'id') }.to raise_error(ArgumentError, 'Invalid amount: nil')
+      end
+    end
+
+    context 'when error is raise' do
+      it 'creates BillingLog' do
+        expect { credit_card.refund(100, 'id') }
+          .to make_gateway_call(:refund).and_raise_error('an error')
+          .and raise_error('an error').and change(BillingLog, :count).by 1
+
+        expect(last_billing_log.message).to eql 'Error refunding 100 to "id": an error'
+      end
+    end
+
+    context 'when transaction id is blank' do
+      it 'raises error' do
+        expect { credit_card.refund(100, '') }
+          .to raise_error('Can not refund without original transaction ID')
+      end
+    end
+  end
+end
