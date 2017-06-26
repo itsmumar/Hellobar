0d95fa6403813adc8836c7a6f8b0f6eb8e75faf6
Anton Sozontov <a.sozontov@gmail.com>
2017-06-26 22:15:06 +0700

    Order fonts by name and add Raleway

--- editor/app/services/inline-editing.js
+++ editor/app/services/inline-editing.js
@@ -419,16 +419,17 @@ export default Ember.Service.extend({
           "Arial,Helvetica,sans-serif": 'Arial',
           "Georgia,serif": 'Georgia',
           "Impact, Charcoal, sans-serif": 'Impact',
+          "Lato,sans-serif": 'Lato'
+          "Montserrat,sans-serif": 'Montserrat',
+          "'Open Sans',sans-serif": 'Open Sans',
+          "Oswald,sans-serif": 'Oswald',
+          "'PT Sans',sans-serif": 'PT Sans',
+          "'PT Serif',sans-serif": 'PT Serif',
+          "Raleway, sans-serif": 'Raleway',
+          "Roboto,sans-serif": 'Roboto',
           "Tahoma, Geneva, sans-serif": 'Tahoma',
           "'Times New Roman', Times, serif, -webkit-standard": 'Times New Roman',
           "Verdana, Geneva, sans-serif": 'Verdana',
-          "Roboto,sans-serif": 'Roboto',
-          "Oswald,sans-serif": 'Oswald',
-          "Montserrat,sans-serif": 'Montserrat',
-          "'Open Sans',sans-serif": 'Open Sans',
-          "'PT Sans',sans-serif": 'PT Sans',
-          "'PT Serif',sans-serif": 'PT Serif',
-          "Lato,sans-serif": 'Lato'
         }
       };
       const $textFroala = $(`.hb-editable-block-with-${requestedMode}-formatting`, $iframeBody).froalaEditor($.extend({
