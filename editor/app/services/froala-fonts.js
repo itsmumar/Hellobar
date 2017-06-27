eb982b7c3c95f21756498ce79001ea352f6981d7
Anton Sozontov <a.sozontov@gmail.com>
2017-06-27 15:27:46 +0700

    Extract FroalaFonts service and add all fonts into preview

---
+++ editor/app/services/froala-fonts.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  fontFamily() {
+    return {
+      "Arial,Helvetica,sans-serif": 'Arial',
+      "Georgia,serif": 'Georgia',
+      "Impact, Charcoal, sans-serif": 'Impact',
+      "Lato,sans-serif": 'Lato',
+      "Montserrat,sans-serif": 'Montserrat',
+      "'Open Sans',sans-serif": 'Open Sans',
+      "Oswald,sans-serif": 'Oswald',
+      "'PT Sans',sans-serif": 'PT Sans',
+      "'PT Serif',sans-serif": 'PT Serif',
+      "Raleway, sans-serif": 'Raleway',
+      "Roboto,sans-serif": 'Roboto',
+      "Spectral,serif": 'Spectral',
+      "'Bubbler One', sans-serif": 'Bubbler One',
+      "'Lemon', cursive": 'Lemon',
+      "Tahoma, Geneva, sans-serif": 'Tahoma',
+      "'Times New Roman', Times, serif, -webkit-standard": 'Times New Roman',
+      "Verdana, Geneva, sans-serif": 'Verdana'
+    };
+  },
+
+  googleFonts() {
+    return [
+      'Impact',
+      'Lato',
+      'Montserrat',
+      'Open Sans',
+      'Oswald',
+      'PT Sans',
+      'PT Serif',
+      'Raleway',
+      'Roboto',
+    ]
+  }
+});
