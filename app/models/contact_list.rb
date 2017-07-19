d40d43df9377f439a155c516119526f83ca0b7a4
Anton <a.sozontov@gmail.com>
2017-07-20 03:38:44 +0700

    Add a comment
    We use site.cache_key for caching DynamoDB queries

--- app/models/contact_list.rb
+++ app/models/contact_list.rb
@@ -1,7 +1,7 @@
 class ContactList < ActiveRecord::Base
   attr_accessor :provider_token
 
-  belongs_to :site, touch: true
+  belongs_to :site, touch: true # as we want to invalidate cache
   belongs_to :identity, dependent: :destroy
 
   has_many :site_elements, dependent: :destroy
