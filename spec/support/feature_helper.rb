7d2dbde9a8e887ad29b7ed5aa6a3dad1c84f583c
Anton Sozontov <a.sozontov@gmail.com>
2017-07-27 14:22:53 +0700

    Fix feature specs

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -38,7 +38,7 @@ module FeatureHelper
   end
 
   def finished_all_ajax_requests?
-    page.evaluate_script('jQuery.active').zero?
+    page.evaluate_script('jQuery.active')&.zero?
   end
 
   def bypass_setup_steps(count)
