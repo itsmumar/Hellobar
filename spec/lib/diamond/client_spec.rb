01631ddb7337dc6f236c9b4b36caa95bf2feffad
Percy Hanna <percy@hellobar.com>
2017-07-31 17:24:49 -0600

    Add Diamond analytics implementation

---
+++ spec/lib/diamond/client_spec.rb
@@ -0,0 +1,81 @@
+require 'diamond/client'
+
+describe Diamond::Client do
+  let(:endpoint) { 'http://foobar.com/hbprod' }
+  let(:client) { described_class.new(endpoint: endpoint) }
+  let(:tracking_url) { "#{ endpoint }/t" }
+  let(:headers) { { 'Accept': '*/*', 'Content-Type': 'application/json' } }
+
+  describe '#track' do
+    subject { client.track(params) }
+
+    context 'when identities is nil' do
+      let(:params) { { identities: nil, timestamp: Time.current } }
+
+      it 'raises a TypeError' do
+        expect { subject }.to raise_error(TypeError, 'Must provide identities as a Hash')
+      end
+    end
+
+    context 'when identities is empty' do
+      let(:params) { { identities: {}, timestamp: Time.current } }
+
+      it 'raises an ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError, 'Must provide at least one identity')
+      end
+    end
+
+    context 'when full params are provided' do
+      let(:params) { { event: :foo, identities: { user_id: 123 }, timestamp: Time.current, properties: { foo: :bar } } }
+
+      it 'sends the tracking request' do
+        stub_request(:post, tracking_url).with(
+          headers: headers,
+          body: {
+            event: 'foo',
+            identities: { user_id: 123 },
+            timestamp: params[:timestamp].to_f,
+            properties: { foo: 'bar' }
+          }.to_json
+        )
+
+        subject
+      end
+    end
+
+    context 'when no event is passed in' do
+      let(:params) { { identities: { user_id: 123 }, timestamp: Time.current, properties: { foo: :bar } } }
+
+      it 'tracks the request' do
+        stub_request(:post, tracking_url).with(
+          headers: headers,
+          body: {
+            identities: { user_id: 123 },
+            timestamp: params[:timestamp].to_f,
+            properties: { foo: 'bar' }
+          }.to_json
+        )
+
+        subject
+      end
+    end
+
+    context 'when no properties are passed in' do
+      let(:params) { { event: :foo, identities: { user_id: 123 }, timestamp: Time.current } }
+
+      it 'tracks the request' do
+        stub_request(:post, tracking_url).with(
+          headers: headers,
+          body: {
+            event: 'foo',
+            identities: { user_id: 123 },
+            timestamp: params[:timestamp].to_f,
+            properties: {}
+          }.to_json
+        )
+
+        subject
+      end
+    end
+  end
+end
