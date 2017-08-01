4b6c4be81f9d0a239b361c3b41e41934277889ec
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-08-01 19:33:34 +0200

    Tweak GetResponse adapter specs

--- spec/factories/identities.rb
+++ spec/factories/identities.rb
@@ -42,6 +42,9 @@ FactoryGirl.define do
 
     trait :get_response_api do
       provider 'get_response_api'
+      credentials nil
+      extra nil
+      api_key 'api-key'
     end
 
     trait :icontact do
