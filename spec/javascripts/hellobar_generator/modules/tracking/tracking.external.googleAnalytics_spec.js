02d5276d02cab233f5753d628586ec7961d1b4d3
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-08-09 10:21:50 +0200

    (re-)introduce legacyGoogleAnalytics module
    A little bit of back and forth on this. Now I am certain that having a
    separate module for this is the way to go.

--- spec/javascripts/hellobar_generator/modules/tracking/tracking.external.googleAnalytics_spec.js
+++ spec/javascripts/hellobar_generator/modules/tracking/tracking.external.googleAnalytics_spec.js
@@ -9,7 +9,7 @@ describe('Module tracking.external.googleAnalytics', function () {
     label: 'SiteElement-5'
   }
 
-  describe('Modern Google Analytics', function () {
+  describe('Google Analytics', function () {
     var gaSpy;
     var module;
 
@@ -39,61 +39,9 @@ describe('Module tracking.external.googleAnalytics', function () {
       expect(module.available()).toEqual(true);
     });
 
-    it('returns true for availableModern()', function () {
-      expect(module.availableModern()).toEqual(true);
-    });
-
-    it('returns false for availableLegacy()', function () {
-      expect(module.availableLegacy()).toEqual(false);
-    });
-
     it('sends event to Google Analytics', function () {
       module.send(externalTracking);
       expect(gaSpy).toHaveBeenCalledWith('send', event);
-    });
-  });
-
-  describe('Legacy Google Analytics', function () {
-    var gaSpy;
-    var module;
-
-    var event = [
-      '_trackEvent',
-      externalTracking.category,
-      externalTracking.action,
-      externalTracking.label
-    ]
-
-    beforeEach(function () {
-      hellobar.finalize();
-
-      gaSpy = jasmine.createSpyObj('gaSpy', ['push']);
-
-      module = hellobar('tracking.external.googleAnalytics', {
-        dependencies: {},
-        configurator: function (configuration) {
-          configuration.provider(function () {
-            return gaSpy;
-          });
-        }
-      });
-    });
-
-    it('is available', function () {
-      expect(module.available()).toEqual(true);
-    });
-
-    it('returns false for availableModern()', function () {
-      expect(module.availableModern()).toEqual(false);
-    });
-
-    it('returns false for availableLegacy()', function () {
-      expect(module.availableLegacy()).toEqual(true);
-    });
-
-    it('sends event to Legacy Google Analytics', function () {
-      module.send(externalTracking);
-      expect(gaSpy.push).toHaveBeenCalledWith(event);
     });
   });
 });
