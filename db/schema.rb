9db21d44d952ac08a21e936d7c3798039b8eef6b
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-08-10 22:55:44 +0200

    Update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723190622) do
+ActiveRecord::Schema.define(version: 20170723190737) do
 
   create_table "admin_login_attempts", force: :cascade do |t|
     t.string   "email",         limit: 255
