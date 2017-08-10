945a87510ab03f963e78c4c101a0dad9267efe55
Anton Sozontov <a.sozontov@gmail.com>
2017-08-10 20:55:59 +0700

    Fix feature spec

--- spec/features/billing/past_due_account_spec.rb
+++ spec/features/billing/past_due_account_spec.rb
@@ -4,6 +4,7 @@ feature 'Billing. Past due account', :js do
   given(:user) { create(:user) }
   given(:site) { create(:site, :with_rule, :pro, user: user) }
   given!(:bill) { create(:past_due_bill, subscription: site.current_subscription) }
+  given!(:card) { bill.payment_method_detail }
 
   before do
     allow(Settings).to receive(:fake_data_api).and_return true
@@ -13,7 +14,9 @@ feature 'Billing. Past due account', :js do
 
   scenario 'displays notification about outstanding bill' do
     visit site_path(site)
-    expect(page).to have_content "You have an outstanding bill for $#{ bill.amount.to_i }.00, " \
-                                 "dated #{ bill.bill_at.strftime('%b %d, %Y') }. Fix this."
+
+    expect(page).to have_content "We could not charge $#{ bill.amount.to_i }.00" \
+                                 " on your credit card ending in #{ card.last_digits }." \
+                                 " Update credit card or Try again"
   end
 end
