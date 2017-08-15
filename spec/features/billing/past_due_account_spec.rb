d832d40bc5a424451e82aa19675577f89aed21af
Anton Sozontov <a.sozontov@gmail.com>
2017-08-16 00:50:56 +0700

    Replace payment_method references

--- spec/features/billing/past_due_account_spec.rb
+++ spec/features/billing/past_due_account_spec.rb
@@ -4,7 +4,7 @@ feature 'Billing. Past due account', :js do
   given(:user) { create(:user) }
   given(:site) { create(:site, :with_rule, :pro, user: user) }
   given!(:bill) { create(:past_due_bill, subscription: site.current_subscription) }
-  given!(:card) { bill.payment_method_detail }
+  given!(:credit_card) { bill.credit_card }
 
   before do
     allow(Settings).to receive(:fake_data_api).and_return true
@@ -16,7 +16,7 @@ feature 'Billing. Past due account', :js do
     visit site_path(site)
 
     expect(page).to have_content "We could not charge $#{ bill.amount.to_i }.00" \
-                                 " on your credit card ending in #{ card.last_digits }." \
+                                 " on your credit card ending in #{ credit_card.last_digits }." \
                                  ' Update credit card details or Charge again'
   end
 end
