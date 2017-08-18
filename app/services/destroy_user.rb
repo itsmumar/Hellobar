05898f85f74e93a211532c210e976579b76daf7c
Anton Sozontov <a.sozontov@gmail.com>
2017-08-18 19:28:50 +0700

    Destroy user correctly

---
+++ app/services/destroy_user.rb
@@ -0,0 +1,47 @@
+class DestroyUser
+  def initialize(user)
+    @user = user
+  end
+
+  def call
+    downgrade_subscriptions
+    destroy_credit_cards
+    destroy_own_sites
+    user.destroy!
+  end
+
+  private
+
+  attr_reader :user
+
+  def downgrade_subscriptions
+    subscriptions.each do |subscription|
+      ChangeSubscription.new(subscription.site, subscription: 'free').call
+    end
+  end
+
+  def subscriptions
+    Subscription.where(credit_card_id: user.credit_cards.ids)
+  end
+
+  def destroy_credit_cards
+    user.credit_cards.update_all token: nil
+    user.credit_cards.each &:destroy!
+  end
+
+  def destroy_own_sites
+    SiteMembership.where(user_id: user.id, role: 'owner').each do |membership|
+      if membership.site.users.count > 1
+        promote_first_user_to_owner(membership)
+        membership.destroy!
+      else
+        membership.site.destroy!
+      end
+    end
+  end
+
+  def promote_first_user_to_owner(membership)
+    first_site_user = membership.site.site_memberships.where.not(user_id: user.id).first
+    first_site_user.update role: 'owner'
+  end
+end
