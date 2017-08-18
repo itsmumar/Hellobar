257dde91f606f002e31f8c248b9ced33d92c289d
Percy Hanna <percy@hellobar.com>
2017-08-18 13:27:36 -0600

    Track A/B script uninstalls

--- db/migrate/20170818184546_track_historical_script_installs.rb
+++ db/migrate/20170818184546_track_historical_script_installs.rb
@@ -5,5 +5,11 @@ class TrackHistoricalScriptInstalls < ActiveRecord::Migration
         TrackEvent.new(:installed_script, site: site, user: owner).call
       end
     end
+
+    Site.where('created_at > ? AND script_uninstalled_at IS NOT NULL', Time.gm(2017, 8, 9)).find_each do |site|
+      site.owners.each do |owner|
+        TrackEvent.new(:uninstalled_script, site: site, user: owner).call
+      end
+    end
   end
 end
