5d48a4a843e14f7c45cafd3a7474cc2868f6a5ac
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-09-19 18:14:02 +0200

    Remove ImageUpload versioning

--- spec/features/embed_script_tests/takeover_with_image_spec.rb
+++ spec/features/embed_script_tests/takeover_with_image_spec.rb
@@ -1,24 +1,9 @@
 require 'integration_helper'
 
 feature 'Takeover with image', js: true do
-  scenario 'shows the image' do
+  scenario 'shows large image' do
     element = create(:takeover_element, image_placement: 'bottom')
     image = create(:image_upload, :with_valid_image, site: element.site)
-    element.update(active_image: image)
-
-    visit test_site_path(id: element.site.id)
-
-    # force capybara to wait until iframe is loaded
-    page.has_xpath?('.//iframe[@id="random-container"]')
-
-    within_frame 'random-container-0' do
-      expect(page.find('.uploaded-image')['src']).to have_content(element.image_url)
-    end
-  end
-
-  scenario 'shows the large image for version 2 images' do
-    element = create(:takeover_element, image_placement: 'bottom')
-    image = create(:image_upload, :with_valid_image, site: element.site, version: 2)
     element.update(active_image: image)
 
     visit test_site_path(id: element.site.id)
@@ -32,7 +17,7 @@ feature 'Takeover with image', js: true do
   end
 
   context 'with autodetect theme' do
-    scenario 'shows the image' do
+    scenario 'shows large image' do
       element = create(:takeover_element, image_placement: 'bottom', theme_id: 'autodetect')
       image = create(:image_upload, :with_valid_image, site: element.site)
       element.update(active_image: image)
@@ -43,7 +28,7 @@ feature 'Takeover with image', js: true do
       page.has_xpath?('.//iframe[@id="random-container"]')
 
       within_frame 'random-container-0' do
-        expect(page.find('.uploaded-image')['src']).to have_content(element.image_url)
+        expect(page.find('.uploaded-image')['src']).to have_content(element.image_large_url)
       end
     end
   end
