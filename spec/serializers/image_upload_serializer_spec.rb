de0679bb57a6bafaf37af47d24ce4a82eee7a670
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-09-19 18:37:53 +0200

    Remove `version` from ImageUploadSerializer

--- spec/serializers/image_upload_serializer_spec.rb
+++ spec/serializers/image_upload_serializer_spec.rb
@@ -7,7 +7,7 @@ describe ImageUploadSerializer do
 
     %i[id description url large_url modal_url image_file_name
        image_content_type image_file_size image_updated_at created_at updated_at
-       site_id preuploaded_url theme_id version].each do |attribute|
+       site_id preuploaded_url theme_id].each do |attribute|
       expect(serialized_hash).to have_key attribute
     end
   end
