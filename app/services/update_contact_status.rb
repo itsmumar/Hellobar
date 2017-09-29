35dcede6413dfc21b7235e94efed7e4855fce482
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-09-29 22:24:02 +0200

    Add UpdateContactStatus service object
    It updates subscription status at DynamoDB.

---
+++ app/services/update_contact_status.rb
@@ -0,0 +1,72 @@
+class UpdateContactStatus
+  def initialize contact_list_id, email, status, error: nil
+    @contact_list_id = contact_list_id
+    @email = email
+    @status = status
+    @error = error
+  end
+
+  def call
+    update_status
+  end
+
+  private
+
+  attr_reader :contact_list_id, :email, :status, :error
+
+  def update_status
+    dynamo_db.update_item(
+      key: key,
+      attribute_updates: attribute_updates,
+      return_values: 'NONE',
+      return_consumed_capacity: 'TOTAL',
+      table_name: table_name
+    )
+  end
+
+  def key
+    {
+      lid: contact_list_id,
+      email: email
+    }
+  end
+
+  def attribute_updates
+    error ? erroroneous_attribute_updates : successful_attribute_updates
+  end
+
+  def successful_attribute_updates
+    {
+      status: {
+        value: status,
+        action: 'PUT'
+      }
+    }
+  end
+
+  def erroroneous_attribute_updates
+    successful_attribute_updates.merge(
+      error: {
+        value: error,
+        action: 'PUT'
+      }
+    )
+  end
+
+  def table_name
+    case Rails.env
+    when 'staging'
+      'staging_contacts'
+    when 'production'
+      'contacts'
+    when 'edge'
+      'edge_contacts'
+    else # development / test
+      'development_contacts'
+    end
+  end
+
+  def dynamo_db
+    DynamoDB.new
+  end
+end
