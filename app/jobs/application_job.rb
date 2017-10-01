262364ce38320b7749a45f07d7aade5c518da761
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-10-01 19:39:57 +0200

    Rely on native support for ActiveJob from raven-sentry
    Reference:
    https://github.com/getsentry/raven-ruby/pull/710

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,20 +1,9 @@
 class ApplicationJob < ActiveJob::Base
   queue_as { "hb3_#{ Rails.env }_lowpriority" }
 
-  rescue_from StandardError do |exception|
-    Raven.capture_exception exception
-  end
-
   rescue_from Aws::S3::Errors::InternalError, with: :retry_job
 
   before_perform do |job|
-    Raven.extra_context arguments: job.arguments.inspect, job_id: job.job_id, queue_name: job.queue_name
-    Raven.tags_context job: job.class.to_s
-  end
-
-  def self.perform_later(*args)
-    super
-  rescue => e
-    Raven.capture_exception(e, extra: { arguments: args })
+    Raven.extra_context queue_name: job.queue_name
   end
 end
