e0745b69cfddb51c55b95bcb72cc94ff1b2111b5
Anton Sozontov <a.sozontov@gmail.com>
2017-10-02 13:12:58 +0700

    Fix specs

--- spec/factories/bars.rb
+++ spec/factories/bars.rb
@@ -1,18 +1,24 @@
 FactoryGirl.define do
-  factory :bar, parent: :site_element, class: 'Bar'
+  factory :bar, parent: :site_element, class: 'Bar' do
+    type 'Bar'
+  end
 
   factory :slider, parent: :site_element, class: 'Slider' do
     type 'Slider'
     placement 'top-left'
   end
 
-  factory :alert, parent: :site_element, class: 'Alert'
+  factory :alert, parent: :site_element, class: 'Alert' do
+    type 'Alert'
+  end
 
   factory :modal, parent: :site_element, class: 'Modal' do
+    type 'Modal'
     placement nil
   end
 
   factory :takeover, parent: :site_element, class: 'Takeover' do
+    type 'Takeover'
     placement nil
   end
 end
