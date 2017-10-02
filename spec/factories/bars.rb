cea0dc0bcc9c31e71e67a6c719ccc88b4ee3b922
Anton Sozontov <a.sozontov@gmail.com>
2017-10-02 16:52:17 +0700

    Split factories into their own files

--- spec/factories/bars.rb
+++ spec/factories/bars.rb
@@ -2,23 +2,4 @@ FactoryGirl.define do
   factory :bar, parent: :site_element, class: 'Bar' do
     type 'Bar'
   end
-
-  factory :slider, parent: :site_element, class: 'Slider' do
-    type 'Slider'
-    placement 'top-left'
-  end
-
-  factory :alert, parent: :site_element, class: 'Alert' do
-    type 'Alert'
-  end
-
-  factory :modal, parent: :site_element, class: 'Modal' do
-    type 'Modal'
-    placement nil
-  end
-
-  factory :takeover, parent: :site_element, class: 'Takeover' do
-    type 'Takeover'
-    placement nil
-  end
 end
