5d88d868bc6c0e92f611a5aaf192f082ce2bd0b7
Anton Sozontov <a.sozontov@gmail.com>
2017-10-04 20:19:34 +0700

    Validate url to like

--- editor/app/routes/application.js
+++ editor/app/routes/application.js
@@ -101,7 +101,8 @@ export default Ember.Route.extend({
 
       const allValidation = Ember.RSVP.Promise.all([
         this.get('validation').validate('phone_number', this.currentModel),
-        this.get('validation').validate('url', this.currentModel)
+        this.get('validation').validate('url', this.currentModel),
+        this.get('validation').validate('url_to_like', this.currentModel)
       ]);
 
       allValidation.then((...args) => {
--- editor/app/services/model-validation.js
+++ editor/app/services/model-validation.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 // GLOBALS: isValidNumber function
 const isValidNumber = window.isValidNumber;
+const URL_REGEXP = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
 
 /**
  * @class ModelValidation
@@ -34,9 +35,23 @@ export default Ember.Service.extend({
       validator: (model) => {
         if (Ember.get(model, 'element_subtype') === 'traffic') {
           const url = Ember.get(model, 'settings.url');
-          var regexp = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-          if (!url || !url.match(regexp)) {
+
+          if (!url || !url.match(URL_REGEXP)) {
             return 'link URL is required';
+          }
+        }
+        return null;
+      }
+    }]);
+
+    this.get('validation').add('url_to_like', [{
+      fieldName: 'url',
+      validator: (model) => {
+        if (Ember.get(model, 'element_subtype').match(/social/)) {
+          const url = Ember.get(model, 'settings.url_to_like');
+
+          if (!url || !url.match(URL_REGEXP)) {
+            return 'URL to like is required';
           }
         }
         return null;
