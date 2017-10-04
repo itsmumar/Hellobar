9bbf2920cccd02393d701165239fff04164aa571
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-10-04 12:17:21 +0200

    Fix deserialization exceptions in Raven/Shoryuken
    https://github.com/phstc/shoryuken/wiki/Rails-Integration-Active-Job#active-job-support

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,6 +3,11 @@ class ApplicationJob < ActiveJob::Base
 
   rescue_from Aws::S3::Errors::InternalError, with: :retry_job
 
+  rescue_from ActiveJob::DeserializationError do |exception|
+    Shoryuken.logger.error exception
+    Shoryuken.logger.error exception.backtrace.join("\n")
+  end
+
   before_perform do |job|
     Raven.extra_context queue_name: job.queue_name
   end
