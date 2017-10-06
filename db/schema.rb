cc489bba7697ee35e80f6622d7dfe1998d43ecbc
Anton Sozontov <a.sozontov@gmail.com>
2017-10-06 21:38:24 +0700

    Fix merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005070619) do
+ActiveRecord::Schema.define(version: 20171006104444) do
 
   create_table "admin_login_attempts", force: :cascade do |t|
     t.string   "email",         limit: 255
