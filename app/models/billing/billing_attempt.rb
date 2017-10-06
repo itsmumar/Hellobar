d83d8ab6f3e9a6cbc89857969f804d62b8e27099
Anton Sozontov <a.sozontov@gmail.com>
2017-10-06 11:51:55 +0700

    Add pending status

--- app/models/billing/billing_attempt.rb
+++ app/models/billing/billing_attempt.rb
@@ -1,6 +1,8 @@
 class BillingAttempt < ActiveRecord::Base
   SUCCESSFUL = 'successful'.freeze
   FAILED = 'failed'.freeze
+  PENDING = 'pending'.freeze
+  STATUSES = [FAILED, SUCCESSFUL, PENDING].freeze
 
   # rubocop: disable Rails/HasManyOrHasOneDependent
   belongs_to :bill
@@ -9,16 +11,13 @@ class BillingAttempt < ActiveRecord::Base
   has_one :site, through: :bill
   has_many :refunds, foreign_key: 'refunded_billing_attempt_id', class_name: 'Bill::Refund'
 
-  scope :successful, -> { where(status: SUCCESSFUL) }
-  scope :failed, -> { where(status: FAILED) }
+  STATUSES.each do |status|
+    scope status, -> { where(status: status) }
+  end
 
-  validates :status, presence: true, inclusion: { in: [FAILED, SUCCESSFUL] }
+  validates :status, presence: true, inclusion: { in: STATUSES }
 
   def readonly?
     new_record? ? false : true
-  end
-
-  def status
-    super.to_sym
   end
 end
