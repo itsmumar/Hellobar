8f5c6841cb4f57bc5d617374d9a21485859f2ff9
Anton Sozontov <a.sozontov@gmail.com>
2017-10-11 20:10:56 +0700

    Refactor CreateSite service

--- app/services/create_site.rb
+++ app/services/create_site.rb
@@ -16,23 +16,40 @@ class CreateSite
 
   def call
     check_for_duplicate!
-
-    Site.transaction do
-      site.owners << current_user
-      site.save!
-      site.create_default_rules
-    end
-
-    TrackEvent.new(:created_site, site: site, user: current_user).call
-
-    Referrals::HandleToken.run(user: current_user, token: referral_token)
-    DetectInstallType.new(site).call
-    ChangeSubscription.new(site, subscription: 'free', schedule: 'monthly').call
+    create_site
+    track_site_creation
+    handle_referral_token
+    detect_install_type
+    change_subscription
   end
 
   private
 
   attr_reader :site, :current_user, :referral_token
+
+  def change_subscription
+    ChangeSubscription.new(site, subscription: 'free', schedule: 'monthly').call
+  end
+
+  def detect_install_type
+    DetectInstallType.new(site).call
+  end
+
+  def handle_referral_token
+    Referrals::HandleToken.run(user: current_user, token: referral_token)
+  end
+
+  def track_site_creation
+    TrackEvent.new(:created_site, site: site, user: current_user).call
+  end
+
+  def create_site
+    Site.transaction do
+      site.owners << current_user
+      site.save!
+      site.create_default_rules
+    end
+  end
 
   def check_for_duplicate!
     existing_site = Site.by_url_for(current_user, url: site.url)
