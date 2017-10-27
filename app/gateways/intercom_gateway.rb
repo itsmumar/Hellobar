dd2a57269bb17cd146b44ae9e77768b01032f4a9
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-10-27 11:45:09 +0200

    Rename #intercom to #client in IntercomGateway

--- app/gateways/intercom_gateway.rb
+++ app/gateways/intercom_gateway.rb
@@ -1,22 +1,22 @@
 class IntercomGateway
   def track opts
-    intercom.events.create opts
+    client.events.create opts
   end
 
   def create_user user
-    intercom.users.create(user_id: user.id, email: user.email)
+    client.users.create(user_id: user.id, email: user.email)
   end
 
   def tag_users tag, users
     return if users.blank?
 
-    intercom.tags.tag(name: tag, users: users.map { |u| { user_id: u.id } })
+    client.tags.tag(name: tag, users: users.map { |u| { user_id: u.id } })
   end
 
   private
 
-  def intercom
-    @intercom ||= Intercom::Client.new initialization_opts
+  def client
+    @client ||= Intercom::Client.new initialization_opts
   end
 
   def initialization_opts
