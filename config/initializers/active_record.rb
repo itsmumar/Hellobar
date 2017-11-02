b512091726c2b7f25b5c5d97c1e9d72b2cbd39b4
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-11-02 22:20:03 +0100

    Remove unnecessary ActiveRecord initializer

--- config/initializers/active_record.rb
+++
@@ -1,7 +0,0 @@
-# TODO: Move to ApplicationRecord when upgraded to Rails 5
-class ActiveRecord::Base
-  # Monkey-patch ActiveRecord to always print record ID when doing `#to_s`
-  def to_s
-    "<#{ self.class }##{ format('0x00%x', (object_id << 1)) } id:#{ id.inspect }>"
-  end
-end
