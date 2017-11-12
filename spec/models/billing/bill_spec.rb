a9a6e495493d70e2864cce31a618517becd02ba3
Anton Sozontov <a.sozontov@gmail.com>
2017-11-12 16:18:57 +0700

    Add specs

--- spec/models/billing/bill_spec.rb
+++ spec/models/billing/bill_spec.rb
@@ -3,6 +3,7 @@ describe Bill do
   specify('could be paid') { expect(build(:bill, :paid)).to be_paid }
   specify('could be failed') { expect(build(:bill, :failed)).to be_failed }
   specify('could be voided') { expect(build(:bill, :voided)).to be_voided }
+  specify('could be refunded') { expect(build(:refund_bill, :refunded)).to be_refunded }
 
   describe '#can_pay?' do
     subject { bill.can_pay? }
