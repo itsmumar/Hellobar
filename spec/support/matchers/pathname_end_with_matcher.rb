f9dd47d6577b29f13625ee3459e3b815f3f6280e
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2017-11-28 15:43:51 +0000

    Fix PR comments.

--- spec/support/matchers/pathname_end_with_matcher.rb
+++ spec/support/matchers/pathname_end_with_matcher.rb
@@ -1,4 +1,4 @@
-RSpec::Matchers.define :path_end_with do |expected|
+RSpec::Matchers.define :pathname_end_with do |expected|
   match do |actual|
     expect(actual).to be_a(Pathname)
     expect(actual.to_s).to end_with(expected)
