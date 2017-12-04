fc96cdab06cb2abd264d23ff84f92f5550273cbb
Anton Sozontov <a.sozontov@gmail.com>
2017-12-04 23:20:19 +0700

    Rubocop

--- app/jobs/send_event_to_intercom_job.rb
+++ app/jobs/send_event_to_intercom_job.rb
@@ -1,10 +1,7 @@
 class SendEventToIntercomJob < ApplicationJob
   rescue_from Intercom::ResourceNotFound do |exception|
-    if exception.message == 'User Not Found'
-      handle_user_not_found(*arguments)
-    else
-      raise exception
-    end
+    handle_user_not_found(*arguments) if exception.message == 'User Not Found'
+    raise exception
   end
 
   def perform(event, options = {})
