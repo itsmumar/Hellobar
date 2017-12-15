cbd2c1a296ff155bc2b3d81186ea125db4079c63
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-12-15 20:30:28 +0100

    Fix campaigns factory

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :campaign do
     site
-    contact_list
+
+    contact_list { create :contact_list, site: site }
+
     sequence(:name) { |i| "Campaign #{ i }" }
+
     from_name 'Hello Bar'
     from_email 'dev@hellobar.com'
     subject 'Hello'
