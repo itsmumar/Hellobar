0deb69e238cddb058a7f033079dc86620519ab62
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-01-18 16:59:31 +0100

    Add domain/subdomain validations for Whitelabels

--- app/models/whitelabel.rb
+++ app/models/whitelabel.rb
@@ -3,22 +3,31 @@ class Whitelabel < ApplicationRecord
   VALID = 'valid'.freeze
   STATUSES = [NEW, VALID].freeze
 
-  NAME_REGEXP = /.+\..+/
+  DOMAIN_REGEXP = /.+\..+/
+  SUBDOMAIN_REGEXP = /.+\..+\..+/
 
   belongs_to :site
 
-  validates :domain, presence: true, format: { with: NAME_REGEXP }
+  validates :domain, presence: true, format: { with: DOMAIN_REGEXP }
+  validates :subdomain, presence: true, format: { with: SUBDOMAIN_REGEXP }
   validates :status, presence: true, inclusion: { in: STATUSES }
   validates :site, presence: true
 
-  validate :name_correctness
+  validate :subdomain_correctness
+  validate :domain_correctness
 
   private
 
-  def name_correctness
+  def subdomain_correctness
     # triggers host validation
-    Addressable::URI.new(host: domain)
+    Addressable::URI.new(host: subdomain)
   rescue Addressable::URI::InvalidURIError
-    errors.add :domain, 'Invalid characters in the domain name'
+    errors.add :subdomain, 'Invalid characters used'
+  end
+
+  def domain_correctness
+    return if subdomain.to_s.include? domain.to_s
+
+    errors.add :domain, 'Must be the same host/name as used in the subdomain'
   end
 end
