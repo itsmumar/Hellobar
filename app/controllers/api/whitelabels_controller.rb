1b042bd6e55a0be2be8a91a1be97ccb55f3b2ae7
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-01-22 11:48:40 +0100

    Add #create action to Api::Whitelabels controller

--- app/controllers/api/whitelabels_controller.rb
+++ app/controllers/api/whitelabels_controller.rb
@@ -1,4 +1,9 @@
 class Api::WhitelabelsController < Api::ApplicationController
+  def create
+    whitelabel = Whitelabel.create! whitelabel_params
+    render json: whitelabel, status: :created
+  end
+
   def show
     render json: site.whitelabel
   end
@@ -7,5 +12,9 @@ class Api::WhitelabelsController < Api::ApplicationController
 
   def site
     @site ||= Site.find params[:site_id]
+  end
+
+  def whitelabel_params
+    params.require(:whitelabel).permit :domain, :subdomain, :site_id
   end
 end
--- spec/requests/api/whitelabels_spec.rb
+++ spec/requests/api/whitelabels_spec.rb
@@ -4,6 +4,43 @@ describe 'api/whitelabels requests' do
   let(:headers) { api_headers_for_site_user site, user }
   let(:params) { Hash[format: :json] }
 
+  describe 'POST #create' do
+    let(:domain) { 'hellobar.com' }
+    let(:subdomain) { "email.#{ domain }" }
+    let(:whitelabel_params) do
+      {
+        domain: domain,
+        subdomain: subdomain,
+        site_id: site.id
+      }
+    end
+
+    context 'when created successfully' do
+      it 'returns whitelabel with :created http status' do
+        post api_site_whitelabel_path(site),
+          params.merge(whitelabel: whitelabel_params),
+          headers
+
+        expect(response).to be_successful
+        expect(response.code.to_i).to eql 201
+        expect(json[:domain]).to eql domain
+        expect(json[:subdomain]).to eql subdomain
+      end
+    end
+
+    context 'when there is an error' do
+      it 'returns error code and serialized errors' do
+        post api_site_whitelabel_path(site),
+          params.merge(whitelabel: whitelabel_params.except(:subdomain)),
+          headers
+
+        expect(response).not_to be_successful
+        expect(response.code.to_i).to eql 422
+        expect(json[:errors][:subdomain]).to be_present
+      end
+    end
+  end
+
   describe 'GET #show' do
     context 'when whitelabel exists' do
       let!(:whitelabel) { create :whitelabel, site: site }
