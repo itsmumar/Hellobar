08abb90bc3c2b386cca0c471bfc887350936ad99
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-01-23 21:58:52 +0100

    Fix add 'hellobar101' promo code migration

--- db/migrate/20180122113138_add_hellobar101_promo_code_coupon.rb
+++ db/migrate/20180122113138_add_hellobar101_promo_code_coupon.rb
@@ -3,7 +3,8 @@ class AddHellobar101PromoCodeCoupon < ActiveRecord::Migration
     Coupon.create!(
       label: 'hellobar101',
       trial_period: 30,
-      public: true
+      public: true,
+      amount: Subscription::Pro.defaults[:monthly_amount]
     )
   end
 
