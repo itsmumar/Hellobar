597aff758bd22c482c1ea31fa4dbff5140461326
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-01-24 14:01:47 +0100

    Add ValidateWhitelabel service object

---
+++ app/services/validate_whitelabel.rb
@@ -0,0 +1,63 @@
+class ValidateWhitelabel
+  def initialize whitelabel:
+    @whitelabel = whitelabel
+  end
+
+  def call
+    validate_at_sendgrid
+    validate_sendgrid_response
+    parse_validation_result
+  end
+
+  private
+
+  attr_reader :whitelabel, :sendgrid_response
+
+  delegate :domain_identifier, to: :whitelabel
+
+  def validate_at_sendgrid
+    @sendgrid_response = sendgrid.client.whitelabel.domains._(domain_identifier).validate.post
+  end
+
+  def validate_sendgrid_response
+    return if sendgrid_response.status_code.to_i == 200
+
+    error = sendgrid_response_body['errors'].first['message']
+
+    whitelabel.errors.add :base, error
+    raise_exception
+  end
+
+  def parse_validation_result
+    if sendgrid_response_body['valid']
+      validate_whitelabel
+    else
+      invalidate_whitelabel
+
+      whitelabel.errors.add :base, 'Validation failed'
+      whitelabel.dns = sendgrid_response_body['validation_results']
+
+      raise_exception
+    end
+  end
+
+  def validate_whitelabel
+    whitelabel.valid!
+  end
+
+  def invalidate_whitelabel
+    whitelabel.invalid!
+  end
+
+  def raise_exception
+    raise ActiveRecord::RecordInvalid, whitelabel
+  end
+
+  def sendgrid_response_body
+    JSON.parse sendgrid_response.body
+  end
+
+  def sendgrid
+    @sendgrid ||= SendGrid::API.new api_key: Settings.sendgrid_campaigns_api_key
+  end
+end
---
+++ spec/services/validate_whitelabel_spec.rb
@@ -0,0 +1,86 @@
+describe ValidateWhitelabel do
+  describe '#call' do
+    let(:whitelabel) { create :whitelabel }
+
+    let(:api_url) { 'https://api.sendgrid.com/v3' }
+    let(:url) { "#{ api_url }/whitelabel/domains/#{ whitelabel.domain_identifier }/validate" }
+
+    let(:error_message) { 'Domain whitelabel does not exist.' }
+    let(:erroneous_api_response) { Hash[errors: [{ message: error_message }]] }
+
+    let(:validation_failed_results) do
+      {
+        mail_cname: {
+          valid: false,
+          reason: 'Expected CNAME to match'
+        },
+        dkim1: {
+          valid: false,
+          reason: 'Expected CNAME to match'
+        },
+        dkim2: {
+          valid: false,
+          reason: 'Expected CNAME to match'
+        }
+      }
+    end
+
+    let(:validation_failed_api_response) do
+      Hash[valid: false, validation_results: validation_failed_results]
+    end
+
+    let(:validation_successful_results) do
+      {
+        mail_cname: {
+          valid: true
+        },
+        dkim1: {
+          valid: true
+        },
+        dkim2: {
+          valid: true
+        }
+      }
+    end
+
+    let(:validation_succeeded_api_response) do
+      Hash[valid: true, validation_results: validation_successful_results]
+    end
+
+    it 'returns a general error if SendGrid returns an error' do
+      stub_request(:post, url)
+        .to_return status: 400, body: erroneous_api_response.to_json
+
+      expect {
+        ValidateWhitelabel.new(whitelabel: whitelabel).call
+      }.to raise_exception ActiveRecord::RecordInvalid, /Validation failed/
+    end
+
+    context 'when validation fails' do
+      it 'marks whitelabel as invalid and attaches dns records with error response' do
+        stub_request(:post, url)
+          .to_return status: 200, body: validation_failed_api_response.to_json
+
+        expect {
+          ValidateWhitelabel.new(whitelabel: whitelabel).call
+        }.to raise_exception ActiveRecord::RecordInvalid, /Validation failed/
+
+        expect(whitelabel.status).to eql Whitelabel::INVALID
+        expect(whitelabel.dns).to eql validation_failed_results.deep_stringify_keys
+      end
+    end
+
+    context 'when validation succeeds' do
+      it 'marks whitelabel as valid' do
+        stub_request(:post, url)
+          .to_return status: 200, body: validation_succeeded_api_response.to_json
+
+        expect {
+          ValidateWhitelabel.new(whitelabel: whitelabel).call
+        }.not_to raise_exception
+
+        expect(whitelabel).to be_valid
+      end
+    end
+  end
+end
