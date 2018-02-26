72c8ab5baf01a55adc899f9b77a87f69c0fa393b
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-02-26 19:05:04 +0100

    Fix db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 Coupon.create! label: Coupon::REFERRAL_LABEL, amount: Coupon::REFERRAL_AMOUNT, public: false
-Coupon.create! label: Coupon::PROMOTIONAL_LABEL, public: true, trial_period: 30
+Coupon.create! label: Coupon::PROMOTIONAL_LABEL, amount: Subscription::Pro.defaults[:monthly_amount], public: true, trial_period: 30
