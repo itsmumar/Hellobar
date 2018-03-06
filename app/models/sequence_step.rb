282f06ed8d40b9dcbda6aa16e8adc0fd26c0f3a5
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2018-03-06 21:33:09 +0300

    Add validation on executable type.

--- app/models/sequence_step.rb
+++ app/models/sequence_step.rb
@@ -1,8 +1,12 @@
 class SequenceStep < ApplicationRecord
+  VALID_EXECUTABLE_TYPES = [Campaign.name].freeze
+
   acts_as_paranoid
 
   belongs_to :executable, polymorphic: true
   belongs_to :sequence
 
   validates :delay, presence: true
+  validates :executable, presence: true
+  validates :executable_type, inclusion: { in: VALID_EXECUTABLE_TYPES }
 end
