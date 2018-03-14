a347867e0d9c284c9abd83a690ac2c037e69c0df
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-03-14 21:16:37 +0100

    Fix migration

--- db/migrate/20180302181450_attach_emails_to_sites.rb
+++ db/migrate/20180302181450_attach_emails_to_sites.rb
@@ -1,10 +1,14 @@
 class AttachEmailsToSites < ActiveRecord::Migration
   def up
-    add_column :emails, :site_id, :integer, after: :id, null: false
+    add_column :emails, :site_id, :integer, after: :id
+
+    Campaign.reset_column_information
 
     Campaign.unscoped.each do |campaign|
       campaign.email.update!(site_id: campaign.contact_list.site_id)
     end
+
+    change_column_null :emails, :site_id, false
   end
 
   def down
