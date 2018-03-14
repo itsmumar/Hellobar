bde6f5dc84589a62d20f8a006cb1622d9845fda9
Anton Sozontov <a.sozontov@gmail.com>
2018-03-14 20:42:56 +0700

    Do not serialize email

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -2,7 +2,6 @@ class CampaignSerializer < ActiveModel::Serializer
   attributes :id, :name, :sent_at, :archived_at, :site_id, :contact_list_id, :status, :statistics, :email_id
 
   has_one :contact_list
-  has_one :email
 
   def site_id
     object.site.id
