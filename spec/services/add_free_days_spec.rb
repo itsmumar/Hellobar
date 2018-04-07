af814b1dc338649d4711f49a1d1bad06882f09ce
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2018-04-07 21:56:14 +0300

    Create pending bill for next period after trial.

--- spec/services/add_free_days_spec.rb
+++ spec/services/add_free_days_spec.rb
@@ -2,6 +2,7 @@ describe AddFreeDays, :freeze do
   let(:user) { create :user }
   let(:site) { create :site, :free_subscription, user: user }
   let(:days_number) { 10 }
+  let(:duration) { days_number.days }
   let(:service) { AddFreeDays.new(site, days_number) }
   let(:current_subscription) { site.current_subscription }
   let(:next_bill) { current_subscription.bills.last }
@@ -17,28 +18,54 @@ describe AddFreeDays, :freeze do
       it 'pushes next billing date forward' do
         expect { service.call }
           .to change { next_bill.reload.start_date }
-          .by(10.days) \
+          .by(duration) \
           .and change { next_bill.reload.end_date }
-          .by(10.days) \
+          .by(duration) \
           .and change { next_bill.reload.bill_at }
-          .by(10.days) \
+          .by(duration) \
           .and change { current_bill.reload.end_date }
-          .by(10.days)
+          .by(duration)
       end
     end
   end
 
-  context 'with trail subscription' do
+  context 'with trial subscription' do
     before do
-      AddTrialSubscription.new(site, subscription: 'Pro', trial_period: '10').call
+      AddTrialSubscription.new(site, subscription: 'Pro', trial_period: days_number).call
+    end
+
+    it 'pushes next billing date forward' do
+      expect { service.call }
+        .to change { next_bill.reload.start_date }
+        .by(duration) \
+        .and change { next_bill.reload.end_date }
+        .by(duration) \
+        .and change { next_bill.reload.bill_at }
+        .by(duration) \
+        .and change { current_bill.reload.end_date }
+        .by(duration)
     end
 
     it 'adds free days to the trial' do
       expect { service.call }
-        .to change { current_bill.reload.end_date }
-        .by(10.days) \
-        .and change { current_subscription.reload.trial_end_date }
-        .by(10.days)
+        .to change { current_subscription.reload.trial_end_date }
+        .by(duration)
+    end
+
+    context 'when next bill does not exist' do
+      before do
+        next_bill.destroy
+      end
+
+      it 'creates the bill for next period' do
+        service.call
+
+        new_bill = current_subscription.bills.last
+        expect(new_bill).not_to eq(current_bill)
+        expect(new_bill).to be_pending
+        expect(new_bill.amount).to be > 0
+        expect(new_bill.start_date).not_to eq(duration.from_now)
+      end
     end
   end
 
