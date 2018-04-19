f4fb19579f6091052621bf929ed69f6766f36631
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2018-04-19 14:45:05 +0300

    Clean up registration form.

--- app/models/registration_form.rb
+++ app/models/registration_form.rb
@@ -10,8 +10,6 @@ class RegistrationForm
   def initialize(params)
     super(params[:registration_form])
 
-    self.site_url ||= standardize_url(params[:site_url])
-
     @user = User.new(email: email, password: password)
     @site = Site.new(url: site_url)
   end
@@ -29,15 +27,5 @@ class RegistrationForm
   def validate!
     user.validate!
     site.validate!
-  end
-
-  private
-
-  def standardize_url(url)
-    return if url.blank?
-    parsed_url = Addressable::URI.heuristic_parse(url)
-    "#{ parsed_url.scheme }://#{ parsed_url.normalized_host }"
-  rescue Addressable::URI::InvalidURIError
-    nil
   end
 end
