90c962736ea994d08974cb138572aca07026ebee
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-04-26 16:36:37 +0200

    Increase capybara wait time

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,11 +7,11 @@ MakeMakefile::Logging.instance_variable_set(:@logfile, '/dev/null')
 Selenium::WebDriver::Chrome.driver_path = find_executable('chromedriver')
 
 # Wait a little longer than the default 2 seconds for Ajax requests to finish
-Capybara.default_max_wait_time = 15
+Capybara.default_max_wait_time = 20
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu no-sandbox window-size=1920,1080] }
+    chromeOptions: { args: %w[headless disable-gpu no-sandbox window-size=1600,1200] }
   )
 
   Capybara::Selenium::Driver.new app,
