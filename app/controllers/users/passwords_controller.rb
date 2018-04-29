a343b0a829eb0eba0203c326d10716789c9f1feb
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2018-04-29 18:34:01 +0300

    Disable rubocop warning.

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,5 @@
 class Users::PasswordsController < Devise::PasswordsController
-  skip_before_action :require_no_authentication, only: [:update]
+  skip_before_action :require_no_authentication, only: [:update] # rubocop:disable Rails/LexicallyScopedActionFilter
 
   layout 'static'
 end
