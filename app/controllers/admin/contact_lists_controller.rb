ffb10e6a26bc93a050ff2e79267f3b18ec191670
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-04-30 11:51:28 +0200

    Tweak admin contact list urls/view

--- app/controllers/admin/contact_lists_controller.rb
+++ app/controllers/admin/contact_lists_controller.rb
@@ -1,22 +1,18 @@
 class Admin::ContactListsController < AdminController
-  before_action :load_site
-
   def index
+    @site = Site.find(params[:site_id])
     @contact_lists = @site.contact_lists.with_deleted
     @subscribers_count = FetchSiteContactListTotals.new(@site, @contact_lists.map(&:id)).call
   end
 
   def show
-    @contact_list = @site.contact_lists.with_deleted.find(params[:id])
+    @contact_list = ContactList.with_deleted.find(params[:id])
+    @site = @contact_list.site
     @subscribers = FetchSubscribers.new(@contact_list, pagination_params).call
     @total_subscribers = FetchSiteContactListTotals.new(@site, [@contact_list.id]).call[@contact_list.id]
   end
 
   private
-
-  def load_site
-    @site = Site.find(params[:site_id])
-  end
 
   def pagination_params
     {
