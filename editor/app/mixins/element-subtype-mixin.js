f3616171b584433653ea15a3864eb52f10f52597
Anton Sozontov <a.sozontov@gmail.com>
2018-05-02 17:40:29 +0700

    Fix linting

--- editor/app/mixins/element-subtype-mixin.js
+++ editor/app/mixins/element-subtype-mixin.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
-import _ from 'lodash/lodash';
 
 export default Ember.Mixin.create({
   isEmailSubtype: function () {
-    return this.get('model.element_subtype') === 'email'
+    return this.get('model.element_subtype') === 'email';
   }.property('model.element_subtype')
 });
