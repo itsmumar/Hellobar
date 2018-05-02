b2f58798492ba69a9b01aa956dfb333519a074a0
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2018-05-02 14:02:35 +0300

    Update data migration.

--- db/migrate/20180501104459_remove_refunds_and_chargebacks.rb
+++ db/migrate/20180501104459_remove_refunds_and_chargebacks.rb
@@ -9,97 +9,78 @@ class RemoveRefundsAndChargebacks < ActiveRecord::Migration
   end
 
   def up
-    BillStub.where(type: 'Bill::Refund').find_in_batches do |refunds|
-      bills = BillStub.where(refund_id: refunds.map(&:id)).index_by(&:refund_id)
+    BillStub.where(type: 'Bill::Refund').find_each do |refund|
+      bill = BillStub.find_by(refund_id: refund.id)
+      billing_attempt = BillingAttemptStub.where(bill_id: refund.id).order(:id).last
 
-      refunds.each do |refund|
-        bill = bills[refund.id]
-        next unless bill
+      next unless bill && billing_attempt
 
-        BillStub.transaction do
-          BillingAttemptStub.create!(
-            bill_id: bill.id,
-            response: refund.authorization_code,
-            status: refund.status == 'refunded' ? 'successful' : 'failed',
-            action: 'refund',
-            created_at: refund.created_at
-          )
-
-          bill.update!(status: 'refunded', status_set_at: refund.created_at)
-          refund.destroy
-        end
+      BillStub.transaction do
+        billing_attempt.update!(bill_id: bill.id)
+        bill.update!(status: 'refunded', status_set_at: refund.created_at)
+        refund.destroy
       end
     end
 
-    BillStub.where(type: 'Bill::Chargeback').find_in_batches do |chargebacks|
-      bills = BillStub.where(chargeback_id: chargebacks.map(&:id)).index_by(&:chargeback_id)
+    BillStub.where(type: 'Bill::Chargeback').find_each do |chargeback|
+      bill = BillStub.find_by(chargeback_id: chargeback.id)
+      billing_attempt = BillingAttemptStub.where(bill_id: chargeback.id).order(:id).last
 
-      chargebacks.each do |chargeback|
-        bill = bills[chargeback.id]
-        next unless bill
+      next unless bill && billing_attempt
 
-        BillStub.transaction do
-          BillingAttemptStub.create!(
-            bill_id: bill.id,
-            status: 'successful',
-            action: 'chargeback',
-            created_at: chargeback.created_at
-          )
-
-          bill.update!(status: 'chargedback', status_set_at: chargeback.created_at)
-          chargeback.destroy
-        end
+      BillStub.transaction do
+        billing_attempt.update!(bill_id: bill.id)
+        bill.update!(status: 'chargedback', status_set_at: chargeback.created_at)
+        chargeback.destroy
       end
     end
   end
 
   def down
-    BillStub.where(status: 'refunded').find_in_batches do |bills|
-      billing_attempts = BillingAttemptStub.where(action: 'refund', bill_id: bills.map(&:id)).index_by(&:bill_id)
+    BillStub.where(status: 'refunded').find_each do |bill|
+      billing_attempt = BillingAttemptStub.where(bill_id: bill.id, action: 'refund').order(:id).last
 
-      bills.each do |bill|
-        attempt = billing_attempts[bill.id]
-        next unless attempt
+      next unless billing_attempt
 
+      BillStub.transaction do
         refund = BillStub.create!(
           type: 'Bill::Refund',
           subscription_id: bill.subscription_id,
           amount: bill.amount,
           description: 'Refund due to customer service request',
-          created_at: attempt.created_at,
-          bill_at: attempt.created_at,
-          start_date: attempt.created_at,
+          created_at: billing_attempt.created_at,
+          bill_at: billing_attempt.created_at,
+          start_date: billing_attempt.created_at,
           end_date: bill.end_date,
-          status: attempt.status == 'successful' ? 'refunded' : 'voided',
-          authorization_code: attempt.response
+          status: billing_attempt.status == 'successful' ? 'refunded' : 'voided',
+          authorization_code: billing_attempt.response
         )
 
         bill.update!(status: 'paid', refund_id: refund.id)
-        attempt.destroy
+        billing_attempt.update!(bill_id: refund.id)
       end
     end
 
-    BillStub.where(status: 'chargedback').find_in_batches do |bills|
-      billing_attempts = BillingAttemptStub.where(action: 'chargeback', bill_id: bills.map(&:id)).index_by(&:bill_id)
+    BillStub.where(status: 'chargedback').find_each do |bill|
+      billing_attempt = BillingAttemptStub.where(bill_id: bill.id, action: 'chargeback').order(:id).last
 
-      bills.each do |bill|
-        attempt = billing_attempts[bill.id]
-        next unless attempt
+      next unless billing_attempt
 
+      BillStub.transaction do
         chargeback = BillStub.create!(
           type: 'Bill::Chargeback',
           subscription_id: bill.subscription_id,
           amount: bill.amount,
           description: 'Chargeback',
-          created_at: attempt.created_at,
-          bill_at: attempt.created_at,
-          start_date: attempt.created_at,
+          created_at: billing_attempt.created_at,
+          bill_at: billing_attempt.created_at,
+          start_date: billing_attempt.created_at,
           end_date: bill.end_date,
           status: 'chargedback',
         )
 
         bill.update!(status: 'paid', chargeback_id: chargeback.id)
-        attempt.destroy
+        billing_attempt.update!(bill_id: chargeback.id)
       end
     end
   end
