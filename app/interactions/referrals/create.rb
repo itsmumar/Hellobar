ef6d3b89d796b10ab6db717ef704176b68dacb29
Anton Sozontov <a.sozontov@gmail.com>
2018-05-09 14:45:01 +0700

    Set NUMBER_OF_ALLOWED_REFERRALS to 10

--- app/interactions/referrals/create.rb
+++ app/interactions/referrals/create.rb
@@ -2,7 +2,7 @@ class Referrals::Create < Less::Interaction
   class Error < StandardError
   end
 
-  NUMBER_OF_ALLOWED_REFERRALS = 1
+  NUMBER_OF_ALLOWED_REFERRALS = 10
 
   expects :sender
   expects :params
@@ -24,7 +24,7 @@ class Referrals::Create < Less::Interaction
 
   def validate_referrals_per_day!
     return if sender.sent_referrals.in_last_24_hours.count < NUMBER_OF_ALLOWED_REFERRALS
-    raise "Only #{ NUMBER_OF_ALLOWED_REFERRALS } invitation is allowed per day"
+    raise Error, "Only #{ NUMBER_OF_ALLOWED_REFERRALS } invitation is allowed per day"
   end
 
   def send_initial_email
