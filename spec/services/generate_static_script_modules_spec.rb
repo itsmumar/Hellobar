b565cf59369e5f211bc55114a4bec986f7630a5f
Anton Sozontov <a.sozontov@gmail.com>
2018-05-15 19:44:49 +0700

    Do not allow GenerateStaticScriptModules to override modules.js

--- spec/services/generate_static_script_modules_spec.rb
+++ spec/services/generate_static_script_modules_spec.rb
@@ -2,11 +2,19 @@ describe GenerateStaticScriptModules do
   let(:script_content) { 'script content' }
   let(:compressed_script) { 'compressed script' }
   let(:service) { described_class.new }
+  let(:file) { instance_double(File, 'file') }
+  let(:digest_path) { StaticScriptAssets.digest_path('modules.js') }
 
   before do
     allow(StaticScriptAssets).to receive(:render_compressed).and_return(compressed_script)
     allow(StaticScriptAssets).to receive(:render).and_return(script_content)
     allow(StaticScriptAssets).to receive(:compile_if_missed)
+
+    allow(file).to receive(:puts).with(script_content)
+    allow(File)
+      .to receive(:open)
+      .with(pathname_ending_with("public/generated_scripts/#{ digest_path }"), 'w')
+      .and_yield(file)
   end
 
   shared_examples 'common' do
@@ -17,16 +25,17 @@ describe GenerateStaticScriptModules do
   end
 
   context 'when stored locally' do
-    let(:file) { instance_double(File, 'file') }
-    let(:digest_path) { StaticScriptAssets.digest_path('modules.js') }
-
     before { allow(Settings).to receive(:store_site_scripts_locally).and_return true }
 
     include_examples 'common'
 
     it 'creates file in public/generated_scripts' do
       expect(file).to receive(:puts).with(script_content)
-      expect(File).to receive(:open).with(pathname_ending_with("public/generated_scripts/#{ digest_path }"), 'w').and_yield(file)
+      expect(File)
+        .to receive(:open)
+        .with(pathname_ending_with("public/generated_scripts/#{ digest_path }"), 'w')
+        .and_yield(file)
+
       service.call
     end
   end
