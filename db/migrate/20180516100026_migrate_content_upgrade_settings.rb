fd387bc7e058421bf5ea74c99cc47491fa3f8521
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2018-05-22 22:40:24 +0300

    Handle missing content upgrades files.

--- db/migrate/20180516100026_migrate_content_upgrade_settings.rb
+++ db/migrate/20180516100026_migrate_content_upgrade_settings.rb
@@ -52,7 +52,10 @@ class MigrateContentUpgradeSettings < ActiveRecord::Migration
     ContentUpgradeStub.where(type: 'ContentUpgrade').find_each do |content_upgrade|
       settings = ContentUpgradeSettingsStub.new(content_upgrade_id: content_upgrade.id)
       settings.assign_attributes(content_upgrade.attributes.symbolize_keys.slice(*ATTRIBUTES_TO_MIGRATE))
-      settings.content_upgrade_pdf = content_upgrade.content_upgrade_pdf
+
+      attachment = open(content_upgrade.content_upgrade_pdf) rescue nil
+      settings.content_upgrade_pdf = attachment if attachment
+
       settings.save!
     end
   end
@@ -61,7 +64,10 @@ class MigrateContentUpgradeSettings < ActiveRecord::Migration
     ContentUpgradeSettingsStub.find_each do |settings|
       content_upgrade = ContentUpgradeStub.find(settings.content_upgrade_id)
       content_upgrade.assign_attributes(settings.attributes.symbolize_keys.slice(*ATTRIBUTES_TO_MIGRATE))
-      content_upgrade.content_upgrade_pdf = settings.content_upgrade_pdf
+
+      attachment = open(settings.content_upgrade_pdf) rescue nil
+      content_upgrade.content_upgrade_pdf = attachment if attachment
+
       content_upgrade.save!
     end
   end
