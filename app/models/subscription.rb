3a9af476f67d0a1bd3ff2250412f6e060b77e313
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-06-05 09:39:59 +0200

    Remove Subscription.active scope
    It is wrong, obsolete and unused.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -20,7 +20,6 @@ class Subscription < ApplicationRecord
   after_initialize :set_initial_values
 
   scope :paid, -> { joins(:bills).merge(Bill.paid.active).distinct }
-  scope :active, -> { paid.merge(Bill.without_refunds.without_chargebacks) }
   scope :exclude_ended_trials, -> { where('trial_end_date is null or trial_end_date > ?', Time.current) }
   scope :ended_trial, -> { where('trial_end_date is not null AND trial_end_date < ?', Time.current) }
   scope :trial, -> { where('trial_end_date is not null AND trial_end_date > ?', Time.current) }
