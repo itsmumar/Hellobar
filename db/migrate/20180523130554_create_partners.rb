09089c5c77b6b029d15d70994ea9e319417c72b4
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2018-06-15 10:03:13 +0300

    Update partners fields.

--- db/migrate/20180523130554_create_partners.rb
+++ db/migrate/20180523130554_create_partners.rb
@@ -1,9 +1,12 @@
 class CreatePartners < ActiveRecord::Migration
   def change
     create_table :partners do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.string :email
-      t.string :url
+      t.string :website_url
+      t.string :affiliate_identifier
+      t.string :subscription_type
 
       t.timestamps null: false
     end
