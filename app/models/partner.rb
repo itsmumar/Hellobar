f2f8bb65d7f06b5d101ab279e2bca628fb7d0784
Anton Sozontov <a.sozontov@gmail.com>
2018-06-27 23:01:57 +0300

    Rubocop

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,5 +1,8 @@
 class Partner < ActiveRecord::Base
-  belongs_to :affiliate_information, foreign_key: :affiliate_identifier, primary_key: :affiliate_identifier
+  belongs_to :affiliate_information,
+    foreign_key: :affiliate_identifier,
+    primary_key: :affiliate_identifier,
+    inverse_of: :partner
 
   validates :email, format: { with: Devise.email_regexp, allow_blank: true }
   validates :website_url, url: { allow_blank: true }
