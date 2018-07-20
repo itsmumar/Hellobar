cc5eef73397ecc156ab94ecbc54f359f11b8ab25
Paweł Gościcki <pawel.goscicki@gmail.com>
2018-07-20 14:42:07 +0200

    Migrate cta_height values to database for Sliders

--- db/migrate/20180719125041_migrate_cta_height_to_database_values.rb
+++ db/migrate/20180719125041_migrate_cta_height_to_database_values.rb
@@ -1,5 +1,6 @@
 class MigrateCtaHeightToDatabaseValues < ActiveRecord::Migration
   def up
+    # Bars
     SiteElement
       .where(type: 'Bar')
       .update_all(cta_height: 27)
@@ -7,6 +8,36 @@ class MigrateCtaHeightToDatabaseValues < ActiveRecord::Migration
     SiteElement
       .where(type: 'Bar')
       .where(theme_id: 'smooth-impact')
+      .update_all(cta_height: 34)
+
+    # Sliders
+    SiteElement
+      .where(type: 'Slider')
+      .update_all(cta_height: 30)
+
+    SiteElement
+      .where(type: 'Slider')
+      .where(theme_id: 'french-rose')
+      .update_all(cta_height: 49)
+
+    SiteElement
+      .where(type: 'Slider')
+      .where(theme_id: 'green-timberline')
+      .update_all(cta_height: 33)
+
+    SiteElement
+      .where(type: 'Slider')
+      .where(theme_id: 'marigold')
+      .update_all(cta_height: 46)
+
+    SiteElement
+      .where(type: 'Slider')
+      .where(theme_id: 'smooth-impact')
+      .update_all(cta_height: 35)
+
+    SiteElement
+      .where(type: 'Slider')
+      .where(theme_id: 'marigold')
       .update_all(cta_height: 34)
   end
 end
