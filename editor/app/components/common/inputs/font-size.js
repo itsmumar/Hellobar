cd53ed59238765e7c6bceb1fcdbc6147ed91337f
Anton Sozontov <a.sozontov@gmail.com>
2018-07-27 23:39:03 +0300

    Fix font-size component

--- editor/app/components/common/inputs/font-size.js
+++ editor/app/components/common/inputs/font-size.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
 
   formattedValue: function () {
     return `${ this.get('size') }px`;
-  }.property('value'),
+  }.property('size'),
 
   actions: {
     increment () {
