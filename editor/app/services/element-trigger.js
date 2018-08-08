0a2a845d4c77d79ccf893d6aa5ee34c36745ec37
Seth Siegler <seth@hellobar.com>
2018-08-08 09:20:34 -0700

    different set of options for bars and non-bars

--- editor/app/services/element-trigger.js
+++ editor/app/services/element-trigger.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+
+
   options: [
+    {value: 'immediately', label: 'Immediately'},
+    {value: 'exit-intent', label: 'Exit intent (user begins to leave your site)'},
+    {value: 'wait-5', label: '5 second delay'},
+    {value: 'wait-10', label: '10 second delay'},
+    {value: 'wait-30', label: '30 second delay'},
+    {value: 'wait-60', label: '60 second delay'},
+    {value: 'scroll-some', label: 'After scrolling a little'},
+    {value: 'scroll-middle', label: 'After scrolling to middle'},
+    {value: 'scroll-to-bottom', label: 'After scrolling to bottom'},
+  ],
+  nonBarOptions: [
     {value: 'exit-intent', label: 'Exit intent (user begins to leave your site)'},
     {value: 'wait-5', label: '5 second delay'},
     {value: 'wait-10', label: '10 second delay'},
