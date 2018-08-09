810f5b5423581516ccdee19e0b1c95d5a11fe1b5
Seth Siegler <seth@hellobar.com>
2018-08-09 09:57:02 -0700

    added new placeholder headlines

--- editor/app/routes/interstitial/social.js
+++ editor/app/routes/interstitial/social.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend(InterstitialNestedRouteMixin, {
 
   afterModel(model) {
     Ember.setProperties(model, {
-      'headline': 'Like us on Facebook!',
+      'headline': 'Click “Like” To Learn How To Generate 100+ New Followers a Month Without Spending a Dollar on Ads!',
       'element_subtype': 'social/like_on_facebook'
     });
   }
