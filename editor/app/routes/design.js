383bb6e44068c440662cba6c4753c84e9f2559e0
Anton Sozontov <a.sozontov@gmail.com>
2018-08-17 13:55:34 +0300

    Make sure theme is applied before rendering

--- editor/app/routes/design.js
+++ editor/app/routes/design.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  theming: Ember.inject.service(),
+
   beforeModel(transition) {
     if (!transition.resolvedModels.application.element_subtype) {
       this.transitionTo('goal');
     }
+  },
+
+  afterModel(model) {
+    this.get('theming').applyCurrentTheme();
   }
 });
