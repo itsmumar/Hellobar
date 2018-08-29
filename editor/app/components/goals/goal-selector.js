c3fc8e27f4c107b0e50ccbc4d13430b470f93858
Farooq Farrukh <farooqfarrukh.dev@gmail.com>
2018-08-29 19:03:49 +0500

    Set default value for elements according to goals selected

--- editor/app/components/goals/goal-selector.js
+++ editor/app/components/goals/goal-selector.js
@@ -24,6 +24,30 @@ export default Ember.Component.extend({
       this.set('goal', goal === 'social' ? 'social/like_on_facebook' : goal);
       this.get('theming').resetThemeIfNeeded();
       this.set('model.wiggle_button', false); // turn wiggling off
+      switch(goal) {git
+        case 'call':
+          this.set('model.headline', 'Talk to us to find out more')
+          this.set('model.link_text', 'Call Now')
+          break;
+        case 'email':
+          this.set('model.headline', 'Join our mailing list to stay up to date on our upcoming events')
+          this.set('model.link_text', 'Subscribe')
+          break;
+        case 'announcement':
+          this.set('model.headline', 'Flash Sale: 20% Off Sitewide, Enter Code “20savings')
+          this.set('model.link_text', 'Shop Now')
+          break;
+        case 'social':
+          this.set('model.headline', 'Like us on Facebook!')
+          break;
+        case 'traffic':
+          this.set('model.headline', 'Want To Become An Expert In Hosting Webinars? Join Our Free Webinar Masterclass!' )
+          this.set('model.link_text', 'Save Me A Spot!')
+          break;
+        default:
+          this.set('model.headline', goal )
+          this.set('model.link_text', goal )
+      }
     }
   }
 });
