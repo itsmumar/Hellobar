d80d088d6d39d3cd5cd47245c2517f706670c950
Seth Siegler <seth@hellobar.com>
2018-08-29 11:38:50 -0700

    upsell emailer logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180829223323) do
+=======
+ActiveRecord::Schema.define(version: 20180829153128) do
+>>>>>>> upsell emailer logic
 
   create_table "admin_login_attempts", force: :cascade do |t|
     t.string   "email",         limit: 255
