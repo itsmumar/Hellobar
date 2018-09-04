6d2cf0e62fd2f7e806a8f7eac6a949dc5bb7dd09
Seth Siegler <seth@hellobar.com>
2018-09-04 15:11:43 -0700

    once monthly task setup to run

---
+++ spec/services/reset_email_sent_fields_spec.rb
@@ -0,0 +1,22 @@
+describe ResetEmailSentFields do
+  subject!(:service) { ResetEmailSentFields.new(site) }
+
+  let(:user) { create :user }
+  let(:site) { create :site, user: user,
+                  warning_email_one_sent: true,
+                  warning_email_two_sent: true,
+                  warning_email_three_sent: true,
+                  limit_email_sent: true,
+                  upsell_email_sent: true}
+
+  context 'when warning emails have been sent' do
+    it 'resets warning_email_one_sent to false' do
+      service.call
+      expect(site.warning_email_one_sent).to eql(false)
+      expect(site.warning_email_two_sent).to eql(false)
+      expect(site.warning_email_three_sent).to eql(false)
+      expect(site.limit_email_sent).to eql(false)
+      expect(site.upsell_email_sent).to eql(false)
+    end
+  end
+end
