43df296f63b92c197c9e6fd76fa3601ed9827579
Seth Siegler <seth@hellobar.com>
2018-09-12 12:17:26 -0700

    adding in dummy enterprise STI model so the migration doesn't bug out when querying for enterprise subs

---
+++ app/models/subscription/enterprise.rb
@@ -0,0 +1,23 @@
+class Subscription
+  class Enterprise < Subscription
+    autoload :Capabilities, 'subscription/enterprise/capabilities'
+
+    class << self
+      def defaults
+        {
+          type: 'enterprise',
+          name: 'Enterprise',
+          label: 'ENT',
+          monthly_amount: 99.0,
+          yearly_amount: 799.0,
+          visit_overage: 500_000,
+          visit_warning_one: 400_000,
+          visit_warning_two: ::Float::INFINITY,
+          visit_warning_three: ::Float::INFINITY,
+          visit_overage_amount: nil,
+          upsell_email_trigger: 1_000_000
+        }
+      end
+    end
+  end
+end
