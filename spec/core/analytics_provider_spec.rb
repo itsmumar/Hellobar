babb24a2a9c4eb3518abc006a05c4448cf4f19c3
Seth Siegler <seth@hellobar.com>
2018-09-14 11:40:32 -0700

    WIP

--- spec/core/analytics_provider_spec.rb
+++ spec/core/analytics_provider_spec.rb
@@ -120,7 +120,7 @@ describe AnalyticsProvider do
     it 'tracks "invited-member"' do
       expect(adapter)
         .to receive(:track)
-        .with(event: event, user: user, params: { site_url: site.url })
+        .with(event: event, user: user, params: { site_url: site.url, site_id: site.id })
 
       track(event, user: user, site: site)
     end
@@ -208,14 +208,24 @@ describe AnalyticsProvider do
   end
 
   describe '#created_site' do
-    let(:event) { 'created-site' }
+    # let(:event) { 'created-site' }
+    let(:site) { create :site, :with_user }
+    let(:second_site) { create :site, :with_user }
 
     it 'tracks "created-site"' do
       expect(adapter)
-        .to receive(:track)
-        .with(event: event, user: user, params: { url: site.url })
+        .to receive(:tag_users)
+        .with("#{ (user.sites.count) } Sites", site.owners)
 
-      track(event, user: user, site: site)
+      expect(adapter)
+        .to receive(:untag_users)
+        .with("#{ (user.sites.count - 1) } Sites", site.owners)
+
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'created-site', user: user, params: { url: site.url, site_id: site.id })
+
+      track('created-site', user: user, site: site)
     end
   end
 
@@ -227,6 +237,7 @@ describe AnalyticsProvider do
         .to receive(:track)
         .with(event: event, user: user, params: {
           site_url: site.url,
+          site_id: site.id,
           subscription: 'Free',
           schedule: 'monthly'
         })
@@ -240,8 +251,17 @@ describe AnalyticsProvider do
 
     it 'tracks "installed-script"' do
       expect(adapter)
+        .to receive(:tag_users)
+        .with(site.install_type, anything)
+
+      expect(adapter)
         .to receive(:track)
-        .with(event: event, user: user, params: { url: site.url })
+        .with(event: event, user: user,
+              params: {
+                url: site.url,
+                site_id: site.id,
+                install_type: site.install_type
+              })
 
       track(event, user: user, site: site)
     end
@@ -253,7 +273,7 @@ describe AnalyticsProvider do
     it 'tracks "uninstalled-script"' do
       expect(adapter)
         .to receive(:track)
-        .with(event: event, user: user, params: { url: site.url })
+        .with(event: event, user: user, params: { url: site.url, site_id: site.id })
 
       track(event, user: user, site: site)
     end
@@ -268,7 +288,8 @@ describe AnalyticsProvider do
         .to receive(:track)
         .with(event: event, user: user, params: {
           identity: 'mailchimp',
-          site_url: contact_list.site.url
+          site_url: contact_list.site.url,
+          site_id: contact_list.site.id
         })
 
       track(event, user: user, contact_list: contact_list)
@@ -277,20 +298,254 @@ describe AnalyticsProvider do
 
   describe '#created_bar' do
     let(:event) { 'created-bar' }
-    let(:site_element) { create :site_element }
+    let(:site_element) { create :site_element, type: 'Bar' }
+    let(:takeover_element) { create :site_element, type: 'Takeover' }
+    let(:modal_element) { create :site_element, type: 'Modal' }
+    let(:slider_element) { create :site_element, type: 'Slider' }
+    let(:alert_element) { create :site_element, type: 'Alert' }
 
     it 'tracks "created-bar"' do
       expect(adapter)
         .to receive(:track)
-        .with(event: event, user: user, params: {
-          bar_type: site_element.type,
-          goal: site_element.element_subtype,
-          site_url: site_element.site.url
-        })
+        .with(event: 'created_bar', user: user,
+          params: {
+            goal: site_element.element_subtype,
+            type: site_element.type,
+            theme_id: site_element.theme_id,
+            enable_gdpr: site_element.enable_gdpr,
+            show_branding: site_element.show_branding,
+            headline: site_element.headline,
+            use_default_image: site_element.use_default_image,
+            link_text: site_element.link_text,
+            use_question: site_element.use_question,
+            site_url: site_element.site.url,
+            site_id: site_element.site.id
+          })
 
       track(event,
         user: user,
         site_element: site_element)
+    end
+
+    it 'tracks "created-modal"' do
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'created_modal', user: user,
+          params: {
+            goal: modal_element.element_subtype,
+            type: modal_element.type,
+            theme_id: modal_element.theme_id,
+            enable_gdpr: modal_element.enable_gdpr,
+            show_branding: modal_element.show_branding,
+            headline: modal_element.headline,
+            use_default_image: modal_element.use_default_image,
+            link_text: modal_element.link_text,
+            use_question: modal_element.use_question,
+            site_url: modal_element.site.url,
+            site_id: modal_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: modal_element)
+    end
+
+    it 'tracks "created-slider"' do
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'created_slider', user: user,
+          params: {
+            goal: slider_element.element_subtype,
+            type: slider_element.type,
+            theme_id: slider_element.theme_id,
+            enable_gdpr: slider_element.enable_gdpr,
+            show_branding: slider_element.show_branding,
+            headline: slider_element.headline,
+            use_default_image: slider_element.use_default_image,
+            link_text: slider_element.link_text,
+            use_question: slider_element.use_question,
+            site_url: slider_element.site.url,
+            site_id: slider_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: slider_element)
+    end
+
+    it 'tracks "created-takeover"' do
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'created_page_takeover', user: user,
+          params: {
+            goal: takeover_element.element_subtype,
+            type: takeover_element.type,
+            theme_id: takeover_element.theme_id,
+            enable_gdpr: takeover_element.enable_gdpr,
+            show_branding: takeover_element.show_branding,
+            headline: takeover_element.headline,
+            use_default_image: takeover_element.use_default_image,
+            link_text: takeover_element.link_text,
+            use_question: takeover_element.use_question,
+            site_url: takeover_element.site.url,
+            site_id: takeover_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: takeover_element)
+    end
+
+    it 'tracks "created-alert"' do
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'created_alert', user: user,
+          params: {
+            goal: alert_element.element_subtype,
+            type: alert_element.type,
+            theme_id: alert_element.theme_id,
+            enable_gdpr: alert_element.enable_gdpr,
+            show_branding: alert_element.show_branding,
+            headline: alert_element.headline,
+            use_default_image: alert_element.use_default_image,
+            link_text: alert_element.link_text,
+            use_question: alert_element.use_question,
+            site_url: alert_element.site.url,
+            site_id: alert_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: alert_element)
+    end
+  end
+
+  describe '#updated_bar' do
+    let(:event) { 'updated-bar' }
+    let(:site_element) { create :site_element, type: 'Bar' }
+    let(:takeover_element) { create :site_element, type: 'Takeover' }
+    let(:modal_element) { create :site_element, type: 'Modal' }
+    let(:slider_element) { create :site_element, type: 'Slider' }
+    let(:alert_element) { create :site_element, type: 'Alert' }
+
+    it 'tracks "updated-bar"' do
+      site_element.update(wiggle_button: true)
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'updated_bar', user: user,
+          params: {
+            goal: site_element.element_subtype,
+            type: site_element.type,
+            theme_id: site_element.theme_id,
+            enable_gdpr: site_element.enable_gdpr,
+            show_branding: site_element.show_branding,
+            headline: site_element.headline,
+            use_default_image: site_element.use_default_image,
+            link_text: site_element.link_text,
+            use_question: site_element.use_question,
+            site_url: site_element.site.url,
+            site_id: site_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: site_element)
+    end
+
+    it 'tracks "updated-modal"' do
+      modal_element.update(wiggle_button: true)
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'updated_modal', user: user,
+          params: {
+            goal: modal_element.element_subtype,
+            type: modal_element.type,
+            theme_id: modal_element.theme_id,
+            enable_gdpr: modal_element.enable_gdpr,
+            show_branding: modal_element.show_branding,
+            headline: modal_element.headline,
+            use_default_image: modal_element.use_default_image,
+            link_text: modal_element.link_text,
+            use_question: modal_element.use_question,
+            site_url: modal_element.site.url,
+            site_id: modal_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: modal_element)
+    end
+
+    it 'tracks "updated-slider"' do
+      slider_element.update(wiggle_button: true)
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'updated_slider', user: user,
+          params: {
+            goal: slider_element.element_subtype,
+            type: slider_element.type,
+            theme_id: slider_element.theme_id,
+            enable_gdpr: slider_element.enable_gdpr,
+            show_branding: slider_element.show_branding,
+            headline: slider_element.headline,
+            use_default_image: slider_element.use_default_image,
+            link_text: slider_element.link_text,
+            use_question: slider_element.use_question,
+            site_url: slider_element.site.url,
+            site_id: slider_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: slider_element)
+    end
+
+    it 'tracks "updated-takeover"' do
+      takeover_element.update(wiggle_button: true)
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'updated_page_takeover', user: user,
+          params: {
+            goal: takeover_element.element_subtype,
+            type: takeover_element.type,
+            theme_id: takeover_element.theme_id,
+            enable_gdpr: takeover_element.enable_gdpr,
+            show_branding: takeover_element.show_branding,
+            headline: takeover_element.headline,
+            use_default_image: takeover_element.use_default_image,
+            link_text: takeover_element.link_text,
+            use_question: takeover_element.use_question,
+            site_url: takeover_element.site.url,
+            site_id: takeover_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: takeover_element)
+    end
+
+    it 'tracks "updated-alert"' do
+      alert_element.update(wiggle_button: true)
+      expect(adapter)
+        .to receive(:track)
+        .with(event: 'updated_alert', user: user,
+          params: {
+            goal: alert_element.element_subtype,
+            type: alert_element.type,
+            theme_id: alert_element.theme_id,
+            enable_gdpr: alert_element.enable_gdpr,
+            show_branding: alert_element.show_branding,
+            headline: alert_element.headline,
+            use_default_image: alert_element.use_default_image,
+            link_text: alert_element.link_text,
+            use_question: alert_element.use_question,
+            site_url: alert_element.site.url,
+            site_id: alert_element.site.id
+          })
+
+      track(event,
+        user: user,
+        site_element: alert_element)
     end
   end
 
@@ -303,6 +558,7 @@ describe AnalyticsProvider do
           subscription: subscription.name,
           schedule: subscription.schedule,
           site_url: site.url,
+          site_id: site.id,
           trial_days: 0,
           previous_subscription: previous_subscription.name,
           previous_subscription_amount: previous_subscription.amount,
@@ -439,7 +695,9 @@ describe AnalyticsProvider do
           number_of_views: number_of_views,
           limit: limit,
           subscription: 'Free',
-          schedule: 'monthly'
+          schedule: 'monthly',
+          overage_count: site.overage_count,
+          visit_overage: Subscription::Free.new.visit_overage
         })
 
       track(event, site: site, user: user, limit: limit, number_of_views: number_of_views)
