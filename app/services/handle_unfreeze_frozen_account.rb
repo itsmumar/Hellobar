a8aa12f85aa904e36ad3e3e2845e7a68d580fb79
Farooq Farrukh <farooqfarrukh.dev@gmail.com>
2018-09-20 03:01:05 +0500

    modified unfreeze service

--- app/services/handle_unfreeze_frozen_account.rb
+++ app/services/handle_unfreeze_frozen_account.rb
@@ -1,7 +1,6 @@
 class HandleUnfreezeFrozenAccount
   def initialize(site)
     @site = site
-    @current_subscription = site.current_subscription
   end
 
   def call
@@ -10,17 +9,10 @@ class HandleUnfreezeFrozenAccount
 
   private
 
-  attr_reader :site, :current_subscription
+  attr_reader :site
 
   def handle_unfreeze
-    return unless current_subscription.current_subscription.active_bills.first.paid?
-    current_bill = current_subscription.current_subscription.active_bills.first
-    if current_bill.end_date == Date.current
-      if site.site_elements.deactivated.present?
-        site_element = site.site_elements.deactivated
-        site_element.each(&:activate)
-      end
-      # OveragePaidMailer.unfreeze_email(site).deliver_later
-    end
+    @site.activate_site_element
+    # OveragePaidMailer.unfreeze_email(site).deliver_later
   end
 end
