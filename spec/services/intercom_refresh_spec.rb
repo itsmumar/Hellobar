d0f316273fe0703033c2f3ec1f446b2e13de4271
Seth Siegler <seth@hellobar.com>
2018-09-19 19:10:43 -0700

    added daily intercom refresh task to handle DME tag

--- spec/services/intercom_refresh_spec.rb
+++ spec/services/intercom_refresh_spec.rb
@@ -15,28 +15,28 @@ describe IntercomRefresh do
     let!(:params) { Hash[element_subtype: 'call', rule_id: site.rules.ids.first] }
     let!(:element) { create :slider, site: site}
 
-    it 'calls TrackEvent with :update_site_count event' do
-      expect(service)
-        .to receive_service_call
-      
-
-      service.call
-    end
-
-    it 'updates site count' do
-      site.update(script_installed_at: Time.now)
-
-      service.call
-      # expect { service.call }
-      #   .to have_received(:track_updated_site_counts)
-
-      p user.sites.last.site_elements.count
-      # expect(service).to have_received(:track_updated_site_counts)
-
-      # expect(last_bill.subscription).to eql(site.active_subscription)
-      # expect(last_bill.amount).to eql(10)
-      # expect(last_bill.grace_period_allowed).to be_truthy
-      # expect(last_bill.description).to eql('Monthly View Limit Overage Fee')
-    end
+    # it 'calls TrackEvent with :update_site_count event' do
+    #   expect(service)
+    #     .to receive_service_call
+    #
+    #
+    #   service.call
+    # end
+    #
+    # it 'updates site count' do
+    #   site.update(script_installed_at: Time.now)
+    #
+    #   service.call
+    #   # expect { service.call }
+    #   #   .to have_received(:track_updated_site_counts)
+    #
+    #   p user.sites.last.site_elements.count
+    #   # expect(service).to have_received(:track_updated_site_counts)
+    #
+    #   # expect(last_bill.subscription).to eql(site.active_subscription)
+    #   # expect(last_bill.amount).to eql(10)
+    #   # expect(last_bill.grace_period_allowed).to be_truthy
+    #   # expect(last_bill.description).to eql('Monthly View Limit Overage Fee')
+    # end
   end
 end
