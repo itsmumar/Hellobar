ce0f1836122e6ed7d021bdeb895a9024c3a0db08
Seth Siegler <seth@hellobar.com>
2018-09-19 18:12:41 -0700

    added a test for the reactivate service

--- spec/services/handle_unfreeze_frozen_account_spec.rb
+++ spec/services/handle_unfreeze_frozen_account_spec.rb
@@ -1,23 +1,20 @@
 describe HandleUnfreezeFrozenAccount do
-  # subject!(:service) { HandleUnfreezeFrozenAccount.new(site) }
+  subject!(:service2) { HandleUnfreezeFrozenAccount.new(site) }
 
   let(:user) { create :user }
   let(:site) { create :site, :with_rule, user: user }
   let(:params) { Hash[element_subtype: 'call', rule_id: site.rules.ids.first] }
-  let(:service) { HandleUnfreezeFrozenAccount.new site }
+  let(:service) { CreateSiteElement.new params, site, user }
 
   it 'persists site element' do
     expect { service.call }.to change(SiteElement, :count).by(1)
   end
-  #
-  # context 'when warning emails have been sent' do
-  #   it 'resets warning_email_one_sent to false' do
-  #     service.call
-  #     expect(site.warning_email_one_sent).to eql(false)
-  #     expect(site.warning_email_two_sent).to eql(false)
-  #     expect(site.warning_email_three_sent).to eql(false)
-  #     expect(site.limit_email_sent).to eql(false)
-  #     expect(site.upsell_email_sent).to eql(false)
-  #   end
-  # end
+
+  context 'unfreeze the account' do
+    it 'activates frozen elements' do
+      site.deactivate_site_element
+      service.call
+      expect(site.site_elements.last.deactivated_at).to eql(nil)
+    end
+  end
 end
