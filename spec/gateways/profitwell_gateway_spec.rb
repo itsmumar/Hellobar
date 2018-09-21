cbd84ef57c60003d49334bb2f2b66e7f45b99685
Anton Sozontov <a.sozontov@gmail.com>
2018-09-21 17:20:00 +0300

    Tweak profitwell integration

--- spec/gateways/profitwell_gateway_spec.rb
+++ spec/gateways/profitwell_gateway_spec.rb
@@ -40,43 +40,13 @@ describe ProfitwellGateway do
     end
   end
 
-  describe '#update_subscription' do
-    let(:subscription) { create :subscription }
-
-    let(:request_body) do
-      {
-        'subscription_alias' => subscription.site_id,
-        'plan_id' => subscription.type,
-        'plan_interval' => 'month',
-        'value' => subscription.amount,
-        'effective_date' => subscription.created_at.to_i
-      }
-    end
-
-    let!(:request) do
-      stub_request(:put, "https://api.profitwell.com/v2/subscriptions/#{ subscription.site_id }")
-        .with(
-          body: request_body,
-          headers: {
-            'Authorization' => 'profitwell_api_key',
-            'Content-Type' => 'application/json'
-          }
-        )
-    end
-
-    it 'sends a PUT request to /subscriptions/:site_id' do
-      gateway.update_subscription subscription
-      expect(request).to have_been_requested
-    end
-  end
-
   describe '#churn_subscription' do
     let(:subscription) { create :subscription }
 
     let!(:request) do
       stub_request(
         :delete,
-        "https://api.profitwell.com/v2/subscriptions/#{ subscription.site_id }/" \
+        "https://api.profitwell.com/v2/subscriptions/#{ subscription.id }/" \
         "?effective_date=#{ subscription.created_at.to_i }"
       ).with(
         headers: {
@@ -87,7 +57,7 @@ describe ProfitwellGateway do
     end
 
     it 'sends a DELETE request to /subscriptions/:site_id/?effective_date=' do
-      gateway.churn_subscription subscription.site_id, subscription.created_at
+      gateway.churn_subscription subscription.id, subscription.created_at
       expect(request).to have_been_requested
     end
   end
