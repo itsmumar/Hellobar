f1e16f7444dd1ab4f3e2b627ac4cd9f79b15ee77
Anton Sozontov <a.sozontov@gmail.com>
2018-09-26 16:02:24 +0300

    Redirect to 'design' if editing

--- editor/app/routes/goal.js
+++ editor/app/routes/goal.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
-var firstTime = 0;
 
 export default Ember.Route.extend({
-  beforeModel(transition) {
-    if ((window.location.href.indexOf("/edit") !== -1) && transition.resolvedModels.application.element_subtype && transition.resolvedModels.application.type && firstTime < 1 ) {
-      this.transitionTo('design');
-      firstTime+= 1;
-    }
-  }
 });
