1cfbbdf2afc32a77da0bac8fa7dde58cb63eecd1
Seth Siegler <seth@hellobar.com>
2018-10-12 09:18:19 -0700

    fixing specs

--- spec/features/payment_modal_spec.rb
+++ spec/features/payment_modal_spec.rb
@@ -10,6 +10,7 @@ feature 'Payment modal interaction', :js do
   context 'free subscription' do
     background do
       stub_cyber_source :store, :purchase
+      stub_handle_overage(site, 100, 99)
 
       create :rule, site: site
       create :subscription, :free, site: site, credit_card: credit_card
--- spec/services/change_subscription_spec.rb
+++ spec/services/change_subscription_spec.rb
@@ -9,9 +9,6 @@ describe ChangeSubscription, :freeze do
 
   before do
     stub_handle_overage(site, 100, 99)
-    # stub_request(:get, 'http://es.com:9200/test_over_time/over_time_type/_search')
-      # .to_return(status: 200)
-
     stub_cyber_source :purchase
     change_subscription 'free'
 
