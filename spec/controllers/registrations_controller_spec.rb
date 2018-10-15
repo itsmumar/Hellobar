b21322b70c5c3ad847b5e15d89ad2e85ef5c5434
Razee K <raxeekh@gmail.com>
2018-10-15 21:23:06 +0500

    controller specs added for both credit card and registrations

---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,24 @@
+describe RegistrationsController do
+
+  let(:params) do
+    Hash[email: 'email@example.com', password: 'password', site_url: 'wwww.abcd.com', accept_terms_and_conditions: 'true']
+  end
+
+  describe 'POST #create' do
+    context 'when plan is selected' do
+      before { params[:plan] = 'elite-monthly' }
+      it 'should redirect the user to subscribe and take credit card info' do
+        post :create, registration_form: params, signup_with_email: 'true'
+        expect(response.status).to redirect_to('/subscribe/elite-monthly')
+      end
+    end
+
+    context 'when plan is not selected' do
+      it 'should redirect the user to new_site_element_path' do
+        post :create, registration_form: params, signup_with_email: 'true'
+        expect(response.status).to redirect_to(new_site_site_element_path(controller.current_user.sites.first))
+      end
+    end
+  end
+end
+
