4d1edab185b72b6bcb539fe430919ca7f06b699b
Seth Siegler <seth@hellobar.com>
2018-10-15 15:28:35 -0700

    adding some rubocop fixes

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,20 +1,32 @@
 class StatsController < ApplicationController
   before_action :authenticate_user!
+  before_action :calculate_today, only: [:index]
+  before_action :calculate_tomorrow, only: [:index]
+  before_action :calculate_day_before, only: [:index]
 
   def index
-    date = Date.today
-    @today_stats = Subscription.where(created_at:date.midnight..date.end_of_day).non_free.where(trial_end_date: nil).count
-    @today_trials = Subscription.where(created_at:date.midnight..date.end_of_day).non_free.count - @today_stats
-    @today_elite = Subscription.where(created_at:date.midnight..date.end_of_day).non_free.elite.count
+  end
 
+  private
+
+  def calculate_today
+    date = Time.zone.today
+    @today_stats = Subscription.where(created_at: date.midnight..date.end_of_day).non_free.where(trial_end_date: nil).count
+    @today_trials = Subscription.where(created_at: date.midnight..date.end_of_day).non_free.count - @today_stats
+    @today_elite = Subscription.where(created_at: date.midnight..date.end_of_day).non_free.elite.count
+  end
+
+  def calculate_tomorrow
     yesterday = Date.yesterday
-    @yesterday_stats = Subscription.where(created_at:yesterday.midnight..yesterday.end_of_day).non_free.where(trial_end_date: nil).count
-    @yesterday_trials = Subscription.where(created_at:yesterday.midnight..yesterday.end_of_day).non_free.count - @yesterday_stats
-    @yesterday_elite = Subscription.where(created_at:yesterday.midnight..yesterday.end_of_day).non_free.elite.count
+    @yesterday_stats = Subscription.where(created_at: yesterday.midnight..yesterday.end_of_day).non_free.where(trial_end_date: nil).count
+    @yesterday_trials = Subscription.where(created_at: yesterday.midnight..yesterday.end_of_day).non_free.count - @yesterday_stats
+    @yesterday_elite = Subscription.where(created_at: yesterday.midnight..yesterday.end_of_day).non_free.elite.count
+  end
 
+  def calculate_day_before
     day_before = (Date.yesterday - 1)
-    @day_before_stats = Subscription.where(created_at:day_before.midnight..day_before.end_of_day).non_free.where(trial_end_date: nil).count
-    @day_before_trials = Subscription.where(created_at:day_before.midnight..day_before.end_of_day).non_free.count - @day_before_stats
-    @day_before_elite = Subscription.where(created_at:day_before.midnight..day_before.end_of_day).non_free.elite.count
+    @day_before_stats = Subscription.where(created_at: day_before.midnight..day_before.end_of_day).non_free.where(trial_end_date: nil).count
+    @day_before_trials = Subscription.where(created_at: day_before.midnight..day_before.end_of_day).non_free.count - @day_before_stats
+    @day_before_elite = Subscription.where(created_at: day_before.midnight..day_before.end_of_day).non_free.elite.count
   end
 end
