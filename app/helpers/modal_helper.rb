5b8e06407791d20c41a911bc34b877e1128aac68
Farooq Farrukh <farooqfarrukh.dev@gmail.com>
2018-10-17 16:43:55 +0500

    fixing specs and rubocop

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -5,7 +5,6 @@ module ModalHelper
 
   def rule_country_select name_value
     countries = I18n.t(:country_codes).map { |country| country.fetch_values :name, :code }
-    select('rule', 'conditions_attributes', countries, {priority_countries: ['US']},
-           disabled: "disabled", id: nil, name: name_value, class: "value location-country-select")
+    select('rule', 'conditions_attributes', countries, { priority_countries: ['US'] }, disabled: 'disabled', id: nil, name: name_value, class: 'value location-country-select')
   end
 end
