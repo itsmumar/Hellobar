7a0a36756a31eb76915bddfbfc523ecc406d5587
Farooq Farrukh <farooqfarrukh.dev@gmail.com>
2018-10-17 16:19:16 +0500

    added html options to properly render the countries rule

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -5,6 +5,7 @@ module ModalHelper
 
   def rule_country_select name_value
     countries = I18n.t(:country_codes).map { |country| country.fetch_values :name, :code }
-    select('rule', 'conditions_attributes', countries, {priority_countries: ['US']}, name: name_value)
+    select('rule', 'conditions_attributes', countries, {priority_countries: ['US']},
+           disabled: "disabled", id: nil, name: name_value, class: "value location-country-select")
   end
 end
