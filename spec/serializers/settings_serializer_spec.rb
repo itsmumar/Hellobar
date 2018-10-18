2c020e4543130e918294abdc83b1bcf488daa188
Seth Siegler <seth@hellobar.com>
2018-10-18 15:05:12 -0700

    fixing broken spec

--- spec/serializers/settings_serializer_spec.rb
+++ spec/serializers/settings_serializer_spec.rb
@@ -17,7 +17,7 @@ describe SettingsSerializer do
 
     context 'when site has not got advanced_themes capability' do
       let(:site) { create :site, :pro, user: user }
-      let(:advanced_themes) { %w[arctic-facet subtle-facet mall puesto tocaya gogo cocina tajima lionshare new resteo wooli pulse wayfarer] }
+      let(:advanced_themes) { %w[arctic-facet subtle-facet mall puesto tocaya gatsby gogo cocina tajima lionshare new resteo wooli pulse wayfarer] }
       let(:expected_themes) { Theme.sorted.reject { |theme| theme.id.in? advanced_themes } }
 
       it 'does not return advanced themes' do
