f02e306de609fae5a5d7770fabccdc4129d8601a
Razee K <raxeekh@gmail.com>
2018-10-26 16:53:19 +0500

    migration updated

--- db/migrate/20181024083902_add_font_styling_to_site_elements.rb
+++ db/migrate/20181024083902_add_font_styling_to_site_elements.rb
@@ -2,5 +2,7 @@ class AddFontStylingToSiteElements < ActiveRecord::Migration
   def change
     add_column :site_elements, :text_field_font_family, :string, limit: 255
     add_column :site_elements, :text_field_font_size, :integer, limit: 4, default: 14
+
+    SiteElement.where(type: 'Takeover').update_all(text_field_font_size: 18)
   end
 end
