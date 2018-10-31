36b09108b3a7120f882fad135719036891ec464f
Seth Siegler <seth@hellobar.com>
2018-10-31 14:09:09 -0700

    working on specs

--- spec/features/trial_user_spec.rb
+++ spec/features/trial_user_spec.rb
@@ -18,6 +18,8 @@ feature 'Trial User', :js do
     scenario 'allows users to downgrade' do
       allow_any_instance_of(Subscription::Pro).to receive(:problem_with_payment?).and_return(true)
       allow_any_instance_of(Site).to receive(:script_installed?).and_return(true)
+      visit site_path(site)
+
       expect(page).to have_content('Your subscription has not been renewed')
       find('.show-downgrade-modal').click
 
