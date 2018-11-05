502918767dd43cc3e857dfb1d2844d4eb8559076
Razee K <raxeekh@gmail.com>
2018-11-05 20:27:42 +0500

    added min 8 and max 24px font limit for text fields font size

--- app/models/site_element.rb
+++ app/models/site_element.rb
@@ -58,7 +58,7 @@ class SiteElement < ApplicationRecord
   validates :cta_height, numericality: { only_integer: true, greater_than_or_equal_to: 20, less_than_or_equal_to: 150 }
   validates :text_field_border_color, hex_color: true
   validates :text_field_border_width, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :text_field_font_size, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :text_field_font_size, numericality: { only_integer: true, greater_than_or_equal_to: 8, less_than_or_equal_to: 24}
   validates :text_field_border_radius, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :text_field_text_color, hex_color: true
   validates :text_field_background_color, hex_color: true
