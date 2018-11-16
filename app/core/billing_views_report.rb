73df42ff6187cf11059ec09a61abfec24df9ac0d
Seth Siegler <seth@hellobar.com>
2018-11-16 13:59:06 -0800

    XOHB-4434 black friday / cyber monday sales emails

--- app/core/billing_views_report.rb
+++ app/core/billing_views_report.rb
@@ -52,8 +52,19 @@ class BillingViewsReport
   def send_warning_email(site, number_of_views, limit, warning_level)
     if site.current_subscription&.paid?
       WarningMailer.warning_email(site, number_of_views, limit, warning_level).deliver_later
+
+    # TODO: disable this afer black friday
+    # Begin black friday thing
+    elsif site.warning_email_two == false && !site.current_subscription&.paid? # not warning email two yet so it must be one
+      WarningMailer.black_friday_one(site, number_of_views, limit, warning_level).deliver_later
+    elsif site.warning_email_three == false && !site.current_subscription&.paid?
+      WarningMailer.black_friday_two(site, number_of_views, limit, warning_level).deliver_later
     else
-      WarningMailer.warning_free_email(site, number_of_views, limit, warning_level).deliver_later
+      WarningMailer.black_friday_three(site, number_of_views, limit, warning_level).deliver_later
+      # End of black friday thing
+
+      # else TODO: enable this after black friday
+      # WarningMailer.warning_free_email(site, number_of_views, limit, warning_level).deliver_later
     end
   end
 
--- app/mailers/warning_mailer.rb
+++ app/mailers/warning_mailer.rb
@@ -8,7 +8,7 @@ class WarningMailer < ApplicationMailer
 
     users.each do |user|
       @user = user
-      mail(to: user.email, subject: "You're approaching your Hello Bar monthly view limit!")
+      mail(to: user.email, subject: 'You are approaching your Hello Bar monthly view limit!')
     end
   end
 
@@ -21,7 +21,46 @@ class WarningMailer < ApplicationMailer
 
     users.each do |user|
       @user = user
-      mail(to: user.email, subject: "You're approaching your Hello Bar monthly view limit!")
+      mail(to: user.email, subject: 'You are approaching your Hello Bar monthly view limit!')
+    end
+  end
+
+  def black_friday_one(site, number_of_views, limit, warning_level)
+    @site = site
+    @number_of_views = number_of_views
+    @limit = limit
+    @warning_level = warning_level
+    users = site.owners_and_admins
+
+    users.each do |user|
+      @user = user
+      mail(to: user.email, subject: 'Hello Bar View Limit Approaching: Don’t Miss Out On Your Slice Of Money Pie This Black Friday!')
+    end
+  end
+
+  def black_friday_two(site, number_of_views, limit, warning_level)
+    @site = site
+    @number_of_views = number_of_views
+    @limit = limit
+    @warning_level = warning_level
+    users = site.owners_and_admins
+
+    users.each do |user|
+      @user = user
+      mail(to: user.email, subject: 'Hello Bar 🚨 Warning: Your Site Is In Danger of Missing Out This Black Friday')
+    end
+  end
+
+  def black_friday_three(site, number_of_views, limit, warning_level)
+    @site = site
+    @number_of_views = number_of_views
+    @limit = limit
+    @warning_level = warning_level
+    users = site.owners_and_admins
+
+    users.each do |user|
+      @user = user
+      mail(to: user.email, subject: 'Hello Bar ⚠️ You’re 500 Views Away From Missing Out On The Busiest Sales')
     end
   end
 end
