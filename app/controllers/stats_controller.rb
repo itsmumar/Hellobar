02094633dc92b0c85e2f8794e80a03850ff198b7
Seth Siegler <seth@hellobar.com>
2018-12-03 11:29:05 -0800

    fixing trials status on stats dashboard

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -16,11 +16,13 @@ class StatsController < ApplicationController
     end_of_month = Date.current.end_of_month.end_of_day
     @mtd_paid = Subscription.where(created_at: date..end_of_month).non_free.where(trial_end_date: nil).count
     @mtd_trials = Subscription.where(created_at: date..end_of_month).non_free.count - @mtd_paid
+    @mtd_trials = calculate_trials(date, end_of_month)
+    @mtd_trial_cancels = calculate_trial_cancels(beginning_of_webinars, end_of_month)
     @mtd_elite = Subscription.where(created_at: date..end_of_month).non_free.elite.count
     @mtd_custom = Subscription.where(created_at: date..end_of_month).non_free.custom.count
     @mtd_pro_special = Subscription.where(created_at: date..end_of_month).non_free.pro_special.count
-    @mtd_converted = Subscription.where(trial_end_date: date..end_of_month).paid.count
-    @all_converted = Subscription.where(trial_end_date: beginning_of_webinars..end_of_month).paid.count
+    @all_converted = calculate_trials(beginning_of_webinars, end_of_month)
+    @all_trial_cancels = calculate_trial_cancels(beginning_of_webinars, end_of_month)
   end
 
   def calculate_today
@@ -48,5 +50,23 @@ class StatsController < ApplicationController
     @day_before_elite = Subscription.where(created_at: day_before.midnight..day_before.end_of_day).non_free.elite.count
     @day_before_custom = Subscription.where(created_at: day_before.midnight..day_before.end_of_day).custom.count
     @day_before_pro_special = Subscription.where(created_at: day_before.midnight..day_before.end_of_day).pro_special.count
+  end
+
+  def calculate_trials(start_date, end_date)
+    subs = []
+    Subscription.where(trial_end_date: start_date..end_date).each do |sub|
+      sub.bills.each do |bill|
+        subs << sub.id if bill.amount.to_i != 0 && bill.status == 'paid'
+      end
+    end
+    subs = subs.uniq.count
+  end
+
+  def calculate_trial_cancels(start_date, end_date)
+    subs = []
+    Subscription.where(trial_end_date: start_date..end_date).each do |sub|
+      subs << sub.id if sub.site.current_subscription&.free?
+    end
+    subs = subs.uniq.count
   end
 end
