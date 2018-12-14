2c2215ccca9f63c57726a347ff30ed2a196680b5
Farooq Farrukh <farooqfarrukh.dev@gmail.com>
2018-12-15 00:24:46 +0500

    rubocop....

--- spec/support/stub_service.rb
+++ spec/support/stub_service.rb
@@ -1,9 +1,9 @@
 RSpec.configure do |config|
   config.before do
     # Stub Services
-    allow_any_instance_of(FetchSiteStatisticsFromES).to(receive :call).and_return({'call': 1, 'total': 1, 'social': 1, 'email': 1, 'traffic': 2})
-    allow_any_instance_of(FetchGraphStatisticsFromES).to(receive :call).and_return([{:date=>"1/15", :value=>12},
-                                                                                    {:date=>"7/05", :value=>12},
-                                                                                    {:date=>"9/04", :value=>12}])
+    allow_any_instance_of(FetchSiteStatisticsFromES).to((receive :call)).and_return('call': 1, 'total': 1, 'social': 1, 'email': 1, 'traffic': 2)
+    allow_any_instance_of(FetchGraphStatisticsFromES).to((receive :call)).and_return([{ date: '1/15', value: 12 },
+                                                                                      { date: '7/05', value: 12 },
+                                                                                      { date: '9/04', value: 12 }])
   end
 end
