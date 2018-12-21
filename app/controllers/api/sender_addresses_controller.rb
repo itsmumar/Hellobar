870c6b8397bdc3675fadad7fb68e3b90e76fa35f
Razee Khan <raxeekh@gmail.com>
2018-12-21 18:11:36 +0500

    rafactoring of sender address controller

--- app/controllers/api/sender_addresses_controller.rb
+++ app/controllers/api/sender_addresses_controller.rb
@@ -1,38 +1,32 @@
 class Api::SenderAddressesController < Api::ApplicationController
-  before_action :find_address, except: %i[create index]
-
-  before_action :verify_owner_site_id
+  before_action :set_site
+  before_action :find_sender_address, except: %i[create index]
 
   def create
-    address = SenderAddress.new(address_params.merge(site_id: params[:site_id]))
+    address = @site.build_sender_address(address_params)
     address.save!
 
     render json: address
   end
 
   def index
-    site = current_user.sites.find(params[:site_id])
-
-    render json: site.sender_address
+    render json: @site.sender_address
   end
 
   def update
-    @address.update!(address_params)
+    @sender_address.update!(address_params)
     render json: @address
   end
 
   private
 
-  def verify_owner_site_id
-    if current_user.sites.map(&:id).include?(params[:site_id].to_i)
-      return true
-    else
-      return render json: {} #handle with 401.
-    end
+
+  def set_site
+    @site ||= current_user.sites.find(params[:site_id])
   end
 
-  def find_address
-    @address = SenderAddress.find(params[:id])
+  def find_sender_address
+    @sender_address = @site.sender_address
   end
 
   def address_params
