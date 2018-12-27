4296a397b5711043b75820e965dd85c104a11d10
Razee K <raxeekh@gmail.com>
2018-12-27 17:27:08 +0500

    multiple fixes for email search api and spec fixed which was falsy passing

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -1,6 +1,7 @@
 class Api::EmailsController < Api::ApplicationController
   before_action :find_email, except: %i[create search]
 
+
   def show
     render json: @email
   end
@@ -18,7 +19,7 @@ class Api::EmailsController < Api::ApplicationController
   end
 
   def search
-    results = site.emails.search(params[:query])
+    results = site.emails.send(params[:email][:flag]).search(params[:email][:query])
     render json: results
   end
 
@@ -33,6 +34,6 @@ class Api::EmailsController < Api::ApplicationController
   end
 
   def email_params
-    params.require(:email).permit(:from_name, :from_email, :subject, :body, :query)
+    params.require(:email).permit(:from_name, :from_email, :subject, :body)
   end
 end
