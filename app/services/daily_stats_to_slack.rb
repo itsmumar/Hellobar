45ede76403d7464685d248fca2e0cb5529815bfa
Seth Siegler <seth@hellobar.com>
2019-02-05 13:21:04 -0800

    wip

--- app/services/daily_stats_to_slack.rb
+++ app/services/daily_stats_to_slack.rb
@@ -1,26 +1,28 @@
 class DailyStatsToSlack
   def initialize
-    @new_sites =
-    @churned_sites =
+    yesterday_start = Date.yesterday.midnight
+    yesterday_end = Date.yesterday.end_of_day
+    @new_subs = Subscription.where(created_at: yesterday_start..yesterday_end).non_free.where(trial_end_date: nil)
+    @churned_subs =
   end
 
   def call
-    @net_new_sites =
+    @net_new_subs =
     @total_paid_sites =
     calc_avg_rev_per_site
   end
 
   private
 
-  attr_reader :new_sites, :churned_sites, :net_new_sites, :total_paid_sites
+  attr_reader :new_subs, :churned_subs, :net_new_subs, :total_paid_sites
 
 
   def calc_avg_rev_per_site
     avg_rev =
-    avg_rev_run_rate = 
+    avg_rev_run_rate =
   end
 
-  def build_slack_message(new_sites, churned_sites, net_new_sites, total_paid_sites, avg_rev, avg_rev_run_rate)
+  def build_slack_message(new_subs, churned_subs, net_new_subs, total_paid_sites, avg_rev, avg_rev_run_rate)
     put_to_slack("Attempting to bill #{ bill.id }: #{ site.url } for $#{ amount }... Failed")
   end
 
