cf3850243b2c664c81b35bffefa58f59b72c7388
Seth Siegler <seth@hellobar.com>
2019-02-12 20:09:36 -0800

    wip fixing specs

--- spec/services/daily_stats_to_slack_spec.rb
+++ spec/services/daily_stats_to_slack_spec.rb
@@ -9,7 +9,7 @@ describe DailyStatsToSlack, :freeze do
 
   it 'has a message received by service' do
     Timecop.travel(1.day.from_now) do
-      l = "\n\n\n\n___________________________________________________________\n___________________________________________________________\nSTATS FOR #{ Date.yesterday }:\nNew Subscriptions: 0\nChurned Subscriptions: 0\nNet New Subscriptions: 0\nTotal Paid Sites: 0\n Avg Monthly Rev Per New Site: $0\n Avg Annual Run Rate: $0\nValue of New Subs: $0\nValue Lost to Churn: $0"
+      l = "STATS FOR #{ Date.yesterday }:\nNew Subscriptions: 0\nChurned Subscriptions: 0\nNet New Subscriptions: 0\nTotal Paid Sites: 0\n Avg Monthly Rev Per New Site: $0\n Avg Annual Run Rate: $0\nValue of New Subs: $0\nValue Lost to Churn: $0"
       expect(PostToSlack).to receive_service_call.with(:daily_stats, text: l)
 
       service.call
