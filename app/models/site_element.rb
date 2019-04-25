c31775fd23e4c1901d765f154653ee0205e2cfad
Razee K <raxeekh@gmail.com>
2019-04-25 16:49:40 +0500

    change font bug fixed

--- app/models/site_element.rb
+++ app/models/site_element.rb
@@ -162,7 +162,7 @@ class SiteElement < ApplicationRecord
 
   def fonts
     fonts = [text_field_font_family, conversion_font]
-    inline_fonts = (headline.to_s + caption.to_s + link_text.to_s).scan(/font-family: "?(.*?)"?,/)
+    inline_fonts = (headline.to_s + caption.to_s + link_text.to_s + conversion_cta_text).scan(/font-family: "?(.*?)"?,/)
     (fonts + inline_fonts).flatten.select(&:present?).uniq - SYSTEM_FONTS
   end
 
