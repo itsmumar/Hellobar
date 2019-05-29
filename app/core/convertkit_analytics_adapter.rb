e6076643cd7b26e16ad423083659375d7c5e7de1
Razee K <raxeekh@gmail.com>
2019-05-29 12:07:35 +0500

    added cases

--- app/core/convertkit_analytics_adapter.rb
+++ app/core/convertkit_analytics_adapter.rb
@@ -55,12 +55,15 @@ git
   def create_or_find_tag(event)
     tag = create_tag(event)
     return tag if tag['error'].blank?
+     return find_tag(event)
+  end
+
+  def find_tag(event)
     tags = list_tag
     tags['tags'].find do |t|
       return t if t['name'] == event
     end
   end
-
   def get_subscribber()
 
   end
@@ -74,12 +77,51 @@ git
     }
   end
 
-  def verify_subscription_tag(event,tag,user)
+  def verify_subscription_tag(event, tag, user, params)
     case event
     when 'installed-script'
+      if user.convortkit_subscriber_id.present?
+        r_tag = find_tag('uninstalled-script')
+        remove_tag(r_tag, user) if r_tag.present?
+      end
+      tag_subscriber(tag, user, params)
 
+    when 'uninstalled-script'
+    if user.convortkit_subscriber_id.present?
+      r_tag = find_tag('installed-script')
+      remove_tag(r_tag, user) if r_tag.present?
     end
+    tag_subscriber(tag, user, params)
 
+    when 'created-popup'
+      if user.convortkit_subscriber_id.present?
+        r_tag = find_tag('not-yet-created-popup')
+        remove_tag(r_tag, user) if r_tag.present?
+      end
+      tag_subscriber(tag, user, params)
+
+    when 'inactive'
+      if user.convortkit_subscriber_id.present?
+        r_tag = find_tag('active')
+        remove_tag(r_tag, user) if r_tag.present?
+      end
+      tag_subscriber(tag, user, params)
+
+    when 'active'
+      if user.convortkit_subscriber_id.present?
+        r_tag = find_tag('inactive')
+        remove_tag(r_tag, user) if r_tag.present?
+      end
+      tag_subscriber(tag, user, params)
+
+    when 'email-synced'
+    when 'email-not-synced'
+    when 'ab-test-created'
+    when 'no-ab-test'
+    when 'winning-ab-test'
+    when 'np-accel'
+
+  end
   end
   def base_uri(endpoint)
     "https://api.convertkit.com/v3/#{ endpoint }?api_secret=#{ Settings.convertkit_api_secret }"
